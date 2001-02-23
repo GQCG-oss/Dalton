05e3089a68ada945c01f87b8fc7d66cac97c728e
Kenneth Ruud <kenneth.ruud@uit.no>
2001-02-23 15:34:25 +0000

    Added temperature dependence and centrifugal distortions to the calculation of effective geometries and (indirectly) vibrational property averages, K.Ruud

--- include/cbiwlk.h
+++ include/cbiwlk.h
@@ -1,9 +1,10 @@
 C
-C$Id: cbiwlk.h,v 1.1.1.1 2001-02-08 13:33:27 hjj Exp $
+C$Id: cbiwlk.h,v 1.2 2001-02-23 15:34:25 ruud Exp $
 C
+      PARAMETER (MAXTMP = 10)
       LOGICAL WFPRED, REJECT, KEEPSY, VIBCNV,
      *        START, DOREPW, IMAGE, STRICT, NOORTH, NATCON, V3CAL,
-     *        VIBAVE, NMODIF, ECKART
+     *        VIBAVE, NMODIF, ECKART, DOTEMP, DOCENT
       COMMON /CBIWLK/ TOLST, TRUSTR, TRUSTI, TRUSTD,
      &                REJMIN, REJMAX, RTRMIN, RTRGOD,
      &                XMXNUC, ZERGRD, DISPLC,
@@ -16,4 +17,4 @@ C
      &                WFPRED, REJECT, KEEPSY, VIBCNV,
      &                IMAGE, ISOTPS(MXCENT), STRICT,
      &                NOORTH, NATCON, V3CAL, VIBAVE, NMODIF,
-     &                ECKART
+     &                ECKART, DOTEMP, DOCENT, TEMP(MAXTMP), NTEMP
