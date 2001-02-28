01b79ad60ac85e70f7b16e7cee095ec723b3fa92
Vebjørn Bakken <vebjorn.bakken@kjemi.uio.no>
2001-02-28 10:01:04 +0000

    All tests have been updated to the automatic testing system, except tests 20, 31 and 56 which are multistep tests (2-3 runs). In addition, a small DGEMM dimension-bug in SIRIUS has been removed.

--- test/run.sh
+++ test/run.sh
@@ -2,32 +2,7 @@ TEST all
 ../dalton -D test20 test20
 ../dalton -D test20_1 test20
 ../dalton test20_2 test20
-../dalton test30 test30
 ../dalton -F test31 test31
 ../dalton -f test31_2 test31
-../dalton test53 test53
-../dalton test54 test54
-../dalton test55 test55
 ../dalton -m 6000000 -D test56 test56
 ../dalton -m 6000000 test56_1 test56
-../dalton test57 test57
-../dalton test58 test58
-../dalton test59 test59
-../dalton test60 test60
-../dalton test61 test61
-../dalton test62 test62
-../dalton test63 test63
-../dalton test64 test64
-../dalton test65 test65
-../dalton test66 test66
-../dalton test67 test67
-../dalton test68 test68
-../dalton test69 test69
-../dalton test70 test70
-../dalton test71 test71
-../dalton test72 test72
-../dalton test73 test73
-../dalton test74 test74
-../dalton test75 test75
-../dalton test76 test76
-../dalton test77 test77
