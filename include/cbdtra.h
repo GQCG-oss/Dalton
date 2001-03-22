b3459c2fbdf8835f6864d6b173e0f9a5e44edfd2
Hans Jørgen Aa. Jensen <hjj@ifk.sdu.dk>
2001-03-22 14:44:15 +0000

    removed many unused variables

--- include/cbdtra.h
+++ include/cbdtra.h
@@ -1,11 +1,6 @@
 C
-Ccbdtra.h 14. aug. 1998
+Ccbdtra.h 14. aug. 1998 ; 21 Mar 2001
 C
       LOGICAL SRTCOO, SRTSYM
-      COMMON /CBDTRA/ IPRTRD,NUMSIM,JTER,
-     &                ISTW(MAXORB),ISTX(MAXORB),
-     &                ISYMO(MAXORB),ISYMA(MAXORB),
-     &                LASTAD(700,3),LASTAE(700),
-     &                NPQ,NCD,NUMCHA,LDABUF,LDAMAX,
-     &                LTRI,LPQCD,LDABUG,LPQRS,
+      COMMON /CBDTRA/ IPRTRD,NUMSIM,JTER, LPQRS,
      &                SRTCOO(3), SRTSYM(8)
