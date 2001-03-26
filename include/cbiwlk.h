5cca8d2cac88e4f1d170868ed67e44e1b6c5293c
Kenneth Ruud <kenneth.ruud@uit.no>
2001-03-26 09:37:07 +0000

    Various small bug fixes, among them now allowing for more than 100 geometry perturbations with natural connection, a OPTORB problem is rsp, and inconsistent record length in non-direct response calculations.
    In addition, added temperature dependence to the harmonic vibrational average
    and the possibility of reusing property derivatives. In this connection changed
    the name of some ABACUS interface files. K.Ruud

--- include/cbiwlk.h
+++ include/cbiwlk.h
@@ -1,10 +1,10 @@
 C
-C$Id: cbiwlk.h,v 1.2 2001-02-23 15:34:25 ruud Exp $
+C$Id: cbiwlk.h,v 1.3 2001-03-26 09:37:06 ruud Exp $
 C
       PARAMETER (MAXTMP = 10)
       LOGICAL WFPRED, REJECT, KEEPSY, VIBCNV,
      *        START, DOREPW, IMAGE, STRICT, NOORTH, NATCON, V3CAL,
-     *        VIBAVE, NMODIF, ECKART, DOTEMP, DOCENT
+     *        VIBAVE, NMODIF, ECKART, DOTEMP, DOCENT, REUSED
       COMMON /CBIWLK/ TOLST, TRUSTR, TRUSTI, TRUSTD,
      &                REJMIN, REJMAX, RTRMIN, RTRGOD,
      &                XMXNUC, ZERGRD, DISPLC,
@@ -17,4 +17,5 @@ C
      &                WFPRED, REJECT, KEEPSY, VIBCNV,
      &                IMAGE, ISOTPS(MXCENT), STRICT,
      &                NOORTH, NATCON, V3CAL, VIBAVE, NMODIF,
-     &                ECKART, DOTEMP, DOCENT, TEMP(MAXTMP), NTEMP
+     &                ECKART, DOTEMP, DOCENT, TEMP(MAXTMP), NTEMP,
+     &                REUSED
