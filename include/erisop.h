7cf330b27e57e0afe46e899ea38b1b1403d86465
Trygve Helgaker <t.u.helgaker@kjemi.uio.no>
2001-04-06 07:00:38 +0000

    eri bug fixes. tuh

--- include/erisop.h
+++ include/erisop.h
@@ -1,5 +1,1 @@
-      LOGICAL ACTSOP
-      COMMON /ERISOP/ SGNX(8**3,12), 
-     &                IOFRST, NSOP(8**3,3), 
-     &                ACTSOP(8,3),
-     &                NSOQ(8**3,3)
+      COMMON /ERISOP/ SGNX(8**3,12), IOFRST, NSOP(8**3,3), NSOQ(8**3,3)
