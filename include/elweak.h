6e3d1c2105810919be624dc342b25cd3bdeedbe8
Alf Ch <dalton-wizards@kjemi.uio.no>
2002-01-17 11:48:57 +0000

    Forgot this. PV should work now.

--- include/elweak.h
+++ include/elweak.h
@@ -1,9 +1,10 @@
-C$Id: elweak.h,v 1.2 2001-10-01 13:22:18 vebjornb Exp $
+C$Id: elweak.h,v 1.3 2002-01-17 11:48:57 alfch Exp $
 C
 C
 C     Constants for electroweak interaction
 C
-      LOGICAL BGWEIL, PVFINL
+      LOGICAL BGWEIL, PVFINL, ELWEAK, PVPSO, PVSO
       PARAMETER (WEINBG = 1D0 - 4D0 * 0.2319D0)
       PARAMETER (GFERMI = 5.73416D-17)
-      COMMON /COMEPV/ ISOTPV(MXCENT), BGWEIN, PVFINN, BGWEIL, PVFINL
+      COMMON /COMEPV/ ISOTPV(MXCENT), BGWEIN, PVFINN, BGWEIL, PVFINL, 
+     &       ELWEAK
