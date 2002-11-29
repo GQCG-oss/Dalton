812648a22219ba868469930d89bf02d03738c1f6
Kenneth Ruud <kenneth.ruud@uit.no>
2002-11-29 10:53:51 +0000

    Transfered R12 parameters to the slaves in order to calculate correctly the two-electron integrals. Also reorganized r12int.h for easier transfer of common-block variables\kr\

--- include/r12int.h
+++ include/r12int.h
@@ -1,4 +1,11 @@
       PARAMETER (MXMULB = 2)
+C
+C     The next parameters indicates the number of integer and
+C     logical variables to be transferred in a parallel calculation.
+C     Must be updated if COMR12 is changed. Note: Order of variables in
+C     COMR12 must not be changed.
+C
+      PARAMETER (NR12I = 7, NR12L = 30)
       LOGICAL R12INT, R12TRA, U12INT, U21INT, R12SQR,
      *        R12CAL, R12NOA, R12NOP, R12NOB, R12OLD, NORXR,
      *        R12HYB, COMBSS, R12EIN, R12EOR, R12ECO, R12XXL,
@@ -10,14 +17,13 @@
      *                NORB1(8), NORB2(8), MBSMAX
       CHARACTER*80 MULNAM(MXMULB)
       CHARACTER*8 LABEL
-      COMMON /COMR12/ R12INT, R12TRA, U12INT, U21INT, R12SQR,
-     *        R12CAL, R12NOA, R12NOP, R12NOB, R12OLD, NORXR,
-     *        R12HYB, COMBSS, R12EIN, GAMAC, GAMAD, INTGAC, 
-     *        INTGAD, R12EOR, R12ECO, R12XXL,
-     *        V12INT, NOTR12, NOTU12, NOTV12,
-     *        ANTICO, IADV12, IADR12, IADU12, IADU21, NOPP12,
-     *        DIRSCF, U12DIR, R12DIR, V12DIR, STEPIV, DCCR12,
-     *        CUSP12, DUMR12
+      COMMON /COMR12/ GAMAC, GAMAD, R12INT, R12TRA, U12INT, 
+     *        U21INT, R12SQR, R12CAL, R12NOA, R12NOP, R12NOB, 
+     *        R12OLD, NORXR, R12HYB, COMBSS, R12EIN,
+     *        R12EOR, R12ECO, R12XXL, V12INT, NOTR12, NOTU12, 
+     *        NOTV12, ANTICO, DIRSCF, U12DIR, R12DIR, V12DIR, 
+     *        STEPIV, DCCR12, CUSP12, DUMR12,
+     *        INTGAC, INTGAD, IADV12, IADR12, IADU12, IADU21, NOPP12
       REAL*8 VCLTHR, SVDTHR
       COMMON /COMVCL/ VCLTHR, SVDTHR
       COMMON /CMMBAS/ LMULBS, NMULBS
