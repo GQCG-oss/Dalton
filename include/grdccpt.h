3b2f643398bca15b0df23003b80a026f4976549c
Sonia Coriani <sonia@chem.au.dk>
2003-02-14 12:04:31 +0000

    Added by S. Coriani

---
+++ include/grdccpt.h
@@ -0,0 +1,7 @@
+C
+C Counter to control CCSD(T) gradient
+C Logical to control direct Fock in CCSD(T) kappabar
+C
+      INTEGER*4 IGRDCCPT
+      LOGICAL   LGRDCCPT
+      COMMON /GRDCCPT/ LGRDCCPT, IGRDCCPT
