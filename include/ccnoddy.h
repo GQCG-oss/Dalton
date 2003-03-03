8866b4f51c7d3c525df35aba51aa4beb357ec692
Christoph Haettig <christof.haettig@rub.de>
2003-03-03 09:31:40 +0000

    - fixed CC excited states for degeneracies appearing within the same irrep   (not yet working for CC3...) - introduced CC3 finite difference on excitation energies, FOPs and   polarizabilities - debug CC3 quadratic response noddy code.

--- include/ccnoddy.h
+++ include/ccnoddy.h
@@ -1,9 +1,11 @@
 *----------------------------------------------------------------------*
 * flags for test (noddy) implementation of CC3 response: 
 *----------------------------------------------------------------------*
-      LOGICAL NODDY_XI, NODDY_ETA, NODDY_XI_ALTER, 
-     &        NODDY_ETA_ALTER, NODDY_FMAT, CCSDT_F_ALTER
+      LOGICAL NODDY_XI, NODDY_ETA, NODDY_XI_ALTER, NODDY_OVLP,
+     &        NODDY_ETA_ALTER, NODDY_FMAT, CCSDT_F_ALTER, NODDY_OMEGA,
+     &        NODDY_LHTR, NODDY_RHTR, NODDY_DEN
 
-      COMMON /CC_NODDY/ NODDY_XI, NODDY_ETA, NODDY_XI_ALTER, 
-     &        NODDY_ETA_ALTER, NODDY_FMAT, CCSDT_F_ALTER
+      COMMON /CC_NODDY/ NODDY_XI, NODDY_ETA, NODDY_XI_ALTER, NODDY_OVLP,
+     &        NODDY_ETA_ALTER, NODDY_FMAT, CCSDT_F_ALTER, NODDY_OMEGA,
+     &        NODDY_LHTR, NODDY_RHTR, NODDY_DEN
 *----------------------------------------------------------------------*
