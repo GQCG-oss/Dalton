e3b21f3404e834f779634b73220e6bb9cfb78b70
Hans Jørgen Aa. Jensen <hjj@ifk.sdu.dk>
2003-05-21 11:23:52 +0000

    hjaaj: changed explicit INTEGER*4 to INTEGER        to avoid problems on 64 bit integer machines;        switched order of integer and logical variables to avoid any        alignment problems if 'logical' is shorter than 'integer'.

--- include/grdccpt.h
+++ include/grdccpt.h
@@ -2,6 +2,6 @@ C
 C Counter to control CCSD(T) gradient
 C Logical to control direct Fock in CCSD(T) kappabar
 C
-      INTEGER*4 IGRDCCPT
-      LOGICAL   LGRDCCPT
-      COMMON /GRDCCPT/ LGRDCCPT, IGRDCCPT
+      INTEGER IGRDCCPT
+      LOGICAL LGRDCCPT
+      COMMON /GRDCCPT/ IGRDCCPT, LGRDCCPT
