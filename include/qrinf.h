ef43c9f3351a5a27b0a9ad317844768ec782e288
Hans Jørgen Aa. Jensen <hjj@ifk.sdu.dk>
2003-05-21 10:51:50 +0000

    hjaaj: removed obsolete declaration of MJWOP in qrinf.h   (MJWOP has for some time been removed from this common block;    and the obsolete declaration could maybe cause problems on    e.g. 64bit integer machines).

--- include/qrinf.h
+++ include/qrinf.h
@@ -1,13 +1,4 @@
-#if defined (VAR_STAR2)
-      INTEGER*2 MJWOP
-#endif
-#if defined (VAR_STAR4)
-      INTEGER*4 MJWOP
-#endif
       LOGICAL QRREST
-C     MAXWOP = maximum number of rotations (dimension of JWOP)
-C     MAXWOP IS DEFINED IN INFVAR WHICH HAS TO PRECEDE
-C     THE CALL OF QRINF
       COMMON /QRINF/  MZVAR(8), MZYVAR(8), MZCONF(8),
      *                MZYCON(8), MZWOPT(8), MZYWOP(8), MSYMA ,MSYMB ,
      *                MSYMC, MZYVMX, MZWOPH(8),MZVARH(8),
