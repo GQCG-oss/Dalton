78881bd9869514864fc8a364c7ecd81e958f3ecb
Hans Jørgen Aa. Jensen <hjj@ifk.sdu.dk>
2003-10-22 12:33:34 +0000

    hjaaj: inserted some comments about the purpose of the common blocks;  - in cbihrs.h: removed OLDSUP, reordered variables with double precision first  - in nuclei.h: now NAMDPX*8 instead of NAMDPX*6 for untruncated coordinate names  - in sigma.h: declare DONS before use in COMMON

--- include/cbihrs.h
+++ include/cbihrs.h
@@ -1,2 +1,3 @@
-      LOGICAL RUNSUP, NOSSUP, OLDSUP
-      COMMON /CBIHRS/ RUNSUP, IPRSUP, NOSSUP, OLDSUP, THRSUP
+C     control common block for call of FRMSUP in her2sup.F, used in herdrv.F /hjaaj
+      LOGICAL RUNSUP, NOSSUP
+      COMMON /CBIHRS/ THRSUP, IPRSUP, RUNSUP, NOSSUP
--- include/cbisor.h
+++ include/cbisor.h
@@ -1,3 +1,4 @@
+C     control common block for SORTAO in intsort.F, also used in herdrv.F /hjaaj
       LOGICAL RNINTS, DELAO
       COMMON /CBISOR/ THRQ2,ISKEEP(8),ISPRINT,ISPRFIO,ISNTSYM,
-     &                DELAO,RNINTS
+     &                RNINTS,DELAO
