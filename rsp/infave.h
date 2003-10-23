085a03abe785a8f507ca14f8057fe56275d5640f
Hans Jørgen Aa. Jensen <hjj@ifk.sdu.dk>
2003-10-23 13:20:53 +0000

    hjaaj: expone.F : prepared for 2-electron expectation values:  PVX in parameter list to AVEPRP, renamed PRPAVE to PRP1AVE  new N1AVTOT and N2AVTOT instead of NAVTOT in infave.h etc., + use new PRPGET to find symmetry of property matrices + new .PROP2A keyword + removed trailing blanks (use cvs diff -w)

--- rsp/infave.h
+++ rsp/infave.h
@@ -1,4 +1,9 @@
-      PARAMETER ( MAXAV = 60 )
-      CHARACTER*8 LBLAV
-      COMMON /INFAV/ NAVTOT
-      COMMON /CHRAV/ LBLAV(MAXAV)
+C (notes by HJAaJ Sep 2003)
+C infave.h has information for rsp/expone.F:
+C       N1AVTOT 1-electron expectation values of LBL1AV(1:N1AVTOT)
+C       N2AVTOT 2-electron expectation values of LBL2AV(1:2,1:N2AVTOT)
+C
+      PARAMETER ( MAX1AV = 60, MAX2AV = 10 )
+      CHARACTER*8 LBL1AV, LBL2AV
+      COMMON /INFAV/ N1AVTOT,N2AVTOT
+      COMMON /CHRAV/ LBL1AV(MAX1AV), LBL2AV(2,MAX2AV)
