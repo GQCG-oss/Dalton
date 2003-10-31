9f730490cb2520a34c5a69c32b8ebfd5da484a8f
Hans Jørgen Aa. Jensen <hjj@ifk.sdu.dk>
2003-10-31 15:30:24 +0000

    hjaaj: 1) moved SCF control variables MAXFCK, MXDIIS, MAXEVC from infinp.h to    scbrhf.h (prepared for merging with new DIIS).    --> remember to do a make depend ! 2) inserted comments in infinp.h and scbrhf.h about their purpose 3) changed some 'HF' to 'SCF' as agreed, for code common to HF and DFT 4) sirout.F : print 'Final DFT energy' instead of 'Final HF energy' if DFT

--- include/infinp.h
+++ include/infinp.h
@@ -1,22 +1,25 @@
+C     this common block contains general Sirius input read in sirius/sirinp.F
+C     (specified under **WAVE FUNCTION).  /hjaaj Oct 2003
+C     - SCF specific input is in scbrhf.h
+C     - orbital specifications are in inforb.h
       PARAMETER (NFLAG = 80, MAXRTS = 20, MXFELT = 20)
-      COMMON /INTINP/ ISPIN,ISTATE,LSYM,NACTEL, MCTYPE,
+      COMMON /INTINP/ NFIELD, ISPIN,ISTATE,LSYM,NACTEL, MCTYPE,
      *                LSOLMX,NLMSOL,NELMN1,NELMX1,NELMN3,NELMX3,
      *                LROOTS,NROOTS,IROOT(MAXRTS),
      *                NOROT(MXCORB),IMOORD(MXCORB),
      *                IORTO,ICI0,KDEL,ICHECK,NTIT,
      *                MAXMAC,MAXMIC,MAXJT,MAXCIT,MAXUIT,MAXAPM,MAXABS,
-     *                MAXFCK, ITRLVL,ITRFIN,JCHSYM,JCHORB,
-     *                NROOCI,ISTACI,NFIELD, MXCIMA, ICICNO,IMCCNO,
-     *                MXDIIS,MAXEVC
+     *                ITRLVL,ITRFIN,JCHSYM,JCHORB,
+     *                NROOCI,ISTACI, MXCIMA, ICICNO,IMCCNO
       LOGICAL FLAG,  DORHF, DOMP2, DOCINO,DOCI,  DOMC,  DORSP,FCVORB,
      &        LNOROT,LMOORD,DIRFCK,CORHOL,CORRLX,RESPHP,JOLSEN,ABAIPH,
-     &        INERSI,INERSF,SUPSYM, DODFT,DONEVPT
+     &        INERSI,INERSF,SUPSYM,DODFT, DONEVPT
       COMMON /LOGINP/ FLAG(NFLAG), DORHF,DOMP2,DOCINO,DOCI,DOMC,DORSP,
      &                FCVORB,LNOROT,LMOORD,DIRFCK,CORHOL,CORRLX,RESPHP,
      &                JOLSEN,ABAIPH,INERSI,INERSF,DODFT,DONEVPT
       EQUIVALENCE (SUPSYM,FLAG(17))
       COMMON /RELINP/ SPIN,POTNUC, EPSOL,EPSTAT,EPPN,RSOL(3),
-     &                THRGRD, THRPWF, THRRHF, THRCI, THRMC, THRCGR,
+     &                THRGRD, THRPWF, THRCI, THRMC, THRCGR,
      &                EFIELD(MXFELT), TITMOL(12,2), CMAXMO, THROVL,
      &                THRSSY
       CHARACTER*60 TITLE
--- include/scbrhf.h
+++ include/scbrhf.h
@@ -1,3 +1,24 @@
-      LOGICAL RHFCAN, AUTOCC
-      COMMON /SCBRHF/ NFRRHF(8),NMVO(8),NMVOT,IOPRHF,MXHFMA,MXHFMI,
-     &                NRHFEL,INIOCC, RHFCAN,AUTOCC
+C scbrhf.h: Sirius Common Block for RHF (in fact for SCF - both RHF and DFT)
+C
+C Common block for SCF information, defined in SCFINP input module /hjaaj Oct 2003
+C (called scbrhf.h because it was made before DFT was implemented)
+C
+C THRRHF: convergence threshold for SCF gradient
+C NFRRHF: frozen SCF orbitals
+C NMVO* : see input option .FC MVO
+C IOPRHF: for 1 open shell RHF
+C MXHFMA, MXHFMI: max macro and micro iterations in QC-SCF
+C MAXFCK: max Roothaan Fock interations
+C MXDIIS: max DIIS iterations
+C MAXEVC: max error vectors in DIIS
+C NRHFEL: number of electrons in SCF
+C INIOCC: how is initial orbital occupation determined
+C RHFCAN: generate canonical SCF orbitalsl, diagonalizing Fock matrix
+C AUTOCC: use automatic orbital occupation
+C HSORHF: High spin open-shell RHF
+C
+      LOGICAL RHFCAN, AUTOCC, HSORHF
+      COMMON /SCBRHF/ THRRHF,
+     &                NFRRHF(8),NMVO(8),NMVOT,IOPRHF,MXHFMA,MXHFMI,
+     &                MAXFCK, MXDIIS, MAXEVC, NRHFEL,INIOCC,
+     &                RHFCAN, AUTOCC, HSORHF
