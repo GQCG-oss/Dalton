390691434299b954928e94e5ed4d8993b7591b65
Pawel Salek <pawsa@theochem.kth.se>
2003-11-17 20:04:09 +0000

    * include/sirbkd.h: SBDTAP block data was removed with reference here   left behind. Remove it from here as well./pawsa

--- include/sirbkd.h
+++ include/sirbkd.h
@@ -2,5 +2,5 @@ C
 C     Define block data modules needed for sirius as external
 C     to be sure they are included.
 C
-      EXTERNAL  SBDORB, SBDTAP, SBDGETD, SBDTRA
+      EXTERNAL  SBDORB, SBDGETD, SBDTRA
 C
