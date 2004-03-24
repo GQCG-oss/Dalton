2689c7687eba8e6b27bbd839973d263694da204c
Hans Jørgen Aa. Jensen <hjj@ifk.sdu.dk>
2004-03-24 14:48:18 +0000

    hjaaj, some major changes: 1) Huckel: changed variable ADDSTO to DOHUCKEL for clarity,    implemented Huckel for ECP calculations by skipping ANO orbitals    corresponding to the core orbitals described with ECP    (work done with Johan Henriksson & Patrick Norman) 2) In this connection I implemented a new index vector IZATOM(MXCENT)    in nuclei.h. This now contains the atom number "Z" of each center,    and a code of "-2" for pointcharges and "-1" to tell this is MBSI.    Value of "0" is of course floating orbitals.    A lot of places the use of NINT(CHARGE(I)) is now replaced with IZATOM(I). -> NOTE: This leads to correct handling of isotopes and masses for ECP and    floating orbitals, point charges, and multiple basis sets (DISOTP),    selection of the correct ANO basis set for Huckel, her1pro.F and more.    Only use GAUNUC if IZATOM(i) .gt. 0 3) ATOMBASIS and symmetry detection has been enabled again, it now works.    Also for INTGRL. Checked in herrdn.F if ADDSYM OK. 4) Use IZATOM in hersym.F. Some erroneous output after ADDSYM has now disappeared,    I think the new output in e.g. test energy_symm from READIN is now correct,    both coordinate labels and rotational constants. 5) abarint.F : implemented modification of Lindhs Hessian using atomic    radii. This is p.t. disabled and needs more testing as it gives slower    convergence in some test cases. Removed ALFA and DIST in parameter list    to BLDHES (statically allocate the two 3 by 3 matrices). 6) moved UNCONT for Douglas-Kroll from dalgnr.F to herdrv.F 7) dalton.F: changed reading of environment variables 8) sirinp.F: revised error check for NELMN1 and NELMX1, some errors were not caught.    Corrected check for NHL1MX, NHL1MN. Stop if both .OPEN SHELL and .HSROHF. 9) Revised some print levels and/or some output in    abalnr.F, abaopt.F, dalgnr.F, herrdn.F

--- include/huckel.h
+++ include/huckel.h
@@ -1,7 +1,7 @@
-      LOGICAL ADDSTO
+      LOGICAL DOHUCKEL
 C
 C     Worst case scenario for HUCEXC; a user running STO-3G (Hueckel basis
 C     half the size of the total basis set size
 C
-      COMMON /HUCKEL/ HUCCNT, HUCEXC(MXSHEL), ADDSTO, NHUCAO(8), 
+      COMMON /HUCKEL/ HUCCNT, HUCEXC(MXSHEL), DOHUCKEL, NHUCAO(8),
      &                NHUCBA, IHUCPT(MXSHEL)
