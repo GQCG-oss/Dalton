23a83b7492c81536d21ebdf2ef1c0f6fc3cdd3a9
David John Wilson <dalton-wizards@kjemi.uio.no>
2004-06-16 12:46:42 +0000

    /djw/ DFT magnetizability and mol g-factor merged into main branch code   - symmetry not implemented   - B3LYP test added

--- include/suscpt.h
+++ include/suscpt.h
@@ -1,3 +1,4 @@
 C     used for transfer of magn. susceptibility between routines
       COMMON /SUSCPT/ SUSDIA(3,3), SUS2EL(3,3), SUSFS(3,3), SUSREL(3,3),
-     &                SUSTOT(3,3), SUSFSY(3,3), SUSCOM(3,3), SUSDZD(3,3)
+     &                SUSTOT(3,3), SUSFSY(3,3), SUSCOM(3,3),SUSDZD(3,3),
+     &                SUSDFT(3,3)
