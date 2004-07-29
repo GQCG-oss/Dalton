72ec692cbda3585ddedac4cd42b59090226003ed
Poul Jørgensen <pou@chem.au.dk>
2004-07-29 09:42:52 +0000

    Poul and Filip: changes in CC3 code: 1) remove VVVV integrals from the left transformation; 2) add CC3 Cauchy moments.

--- include/ccsdinp.h
+++ include/ccsdinp.h
@@ -3,7 +3,7 @@
      &        CCSAVE, STOLD,  JACEXP, LHTR,
      &        DEBUG,  CCSTST, ANAAOD,
      &        HERDIR, FREEZE, KEEPAOIN, NOEONL,
-     &        SIRSOP
+     &        SIRSOP, LVVVV
 
       LOGICAL ETACCPT, DIRKAPB
 
@@ -23,7 +23,7 @@
      &                   ICHANG, IPRINT, KEEPAOTWO, HERDIR,
      &                   ETACCPT, DIRKAPB,
      &                   FREEZE, KEEPAOIN, NOEONL,
-     &                   SIRSOP
+     &                   SIRSOP, LVVVV
 
       COMMON /CCMODELS/ CCS,  CIS,  MP2,  CC2,   CC1A,  CC1B,
      &                  MCC2, CCP2, CCD,  CCSD,  CC3,   CCPT,
