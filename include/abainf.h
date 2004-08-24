4d2dde275cdd02696c2869c642a5864e536907e0
Hans Jørgen Aa. Jensen <hjj@ifk.sdu.dk>
2004-08-24 07:12:35 +0000

    hjaaj: added comment about purpose; removed unused NTCSY(8), NSCSY(8); added new DOD2DQ2 option

--- include/abainf.h
+++ include/abainf.h
@@ -1,11 +1,13 @@
 C
-C     Parameters NSYML must be updated after changes (for parallelization)
-C     NOTE: DOSYM are sent to slaves in a parallel calculation and
-C           should not be move from this subroutine
+C     File: abainf.h
+C     Purpose: Control of what to do in ABACUS module
 C
-      INTEGER NSYML, IPRDEF, NWNABA, NTCSY, NSCSY
+C     NOTE: DOSYM(NSYML) is sent to slaves in a parallel calculation and
+C           should not be move from this common block
+C     Parameter NSYML must be updated after changes (for parallelization)
+C
+      INTEGER NSYML, IPRDEF, NWNABA
       PARAMETER (NSYML = 8)
-cLig <> added CTOCD
       LOGICAL MOLGRD, MOLHES, DIPDER, POLAR,  TSTINP,
      &        VIB,    RESTAR, DOWALK, GDALL,  CCSD,
      &        H2MO,   DOSYM,  DOLRES, DOEXCI, SHIELD,
@@ -15,15 +17,15 @@ cLig <> added CTOCD
      &        DARWIN, ABALNR, VROA,   NOCMC,  EXPFCK,
      &        RAMAN,  QUADRU, NQCC,   HYPER,  VERDET,
      &        MCD,    HELFEY, LINCPL, ABASOP, SKIPAB,
-     &        ABA_ALPHA, EXPGRD, CTOCD, NUMHES
-      COMMON /ABAINF/ NTCSY(8), NSCSY(8), IPRDEF, NWNABA, 
+     &        ABA_ALPHA, EXPGRD, CTOCD, NUMHES, DOD2DQ2
+      COMMON /ABAINF/ IPRDEF, NWNABA, 
      &                MOLGRD, MOLHES, DIPDER, POLAR,  TSTINP,
      &                VIB,    RESTAR, DOWALK, GDALL,  CCSD,
-     &                H2MO,   DOSYM(8),DOLRES,DOEXCI, SHIELD,
+     &                H2MO,   DOSYM(NSYML),   DOLRES, DOEXCI, SHIELD,
      &                SPNSPN, MAGSUS, VCD,    NACME,  AAT,
      &                NOLOND, FCKDDR, ECD,    NODIFC, DODRCT,
      &                SUPMAT, MOLGFA, OPTROT, SPINRO, MASSVE,
      &                DARWIN, ABALNR, VROA,   NOCMC,  EXPFCK,
      &                RAMAN,  QUADRU, NQCC,   HYPER,  VERDET,
      &                MCD,    HELFEY, LINCPL, ABASOP, SKIPAB,
-     &                ABA_ALPHA, EXPGRD, CTOCD, NUMHES
+     &                ABA_ALPHA,      EXPGRD, CTOCD,  NUMHES, DOD2DQ2
