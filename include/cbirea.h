7a1a2cc788db3b55aa0c0690ddf4cad3c205b445
Hans Jørgen Aa. Jensen <hjj@ifk.sdu.dk>
2004-12-07 09:13:54 +0000

    hjaaj: moved parameter MAXPRD to cbirea.h from many places

--- include/cbirea.h
+++ include/cbirea.h
@@ -1,7 +1,18 @@
+C
+C    cbirea.h - common block for reading of MOLECULE.INP (herrdn.F)
+C
+C    MAXPRD = default value for MAXPRI
+#if defined (SYS_T3D)
+      PARAMETER ( MAXPRD = 14 )
+#else
+      PARAMETER ( MAXPRD = 25 )
+#endif
+C
       LOGICAL BIGVC,  GENCON, INPTST, DIRAC,  BASIS,  ATOMBA,
      &        UNCONT, OLDNRM, WRTLIN, ANGS
-      COMMON /CBIREA/ ZCMVAL, INPTST, IPREAD, MAXPRI, BIGVC,  GENCON,
-     &                LUMLCL, DIRAC,  BASIS,  LCMMAX, NCMSTR, NCMEND,
-     &                ATOMBA, UNCONT, OLDNRM, WRTLIN, ANGS
+      COMMON /CBIREA/ ZCMVAL,
+     &                IPREAD, MAXPRI, LUMLCL, LCMMAX, NCMSTR, NCMEND,
+     &                BIGVC,  GENCON, INPTST, DIRAC,  BASIS,  ATOMBA,
+     &                UNCONT, OLDNRM, WRTLIN, ANGS
       CHARACTER*80 BASNAM
       COMMON /CBIRET/ BASNAM
