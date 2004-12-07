f28a5c35fd2d76d02c707f2e212fdf7659f45af5
Hans Jørgen Aa. Jensen <hjj@ifk.sdu.dk>
2004-12-07 09:14:43 +0000

    hjaaj: new variable WESTA for writing info for WESTA program

--- include/gnrinf.h
+++ include/gnrinf.h
@@ -1,15 +1,17 @@
 c     -*- mode: fortran; fortran-continuation-string: "&" -*-
+c     File: gnrinf.h -- general information for DALTON
+c
       LOGICAL TESTIN, OPTWLK, RNHERM, RNSIRI, RNABAC, GEOCNV,
      &        HRINPC, SRINPC, RDINPC, RDMLIN, PARCAL, DIRCAL,
      &        WRINDX, WLKREJ, WALKIN, RNRESP, USRIPR, SEGBAS,
      &        DOCCSD, OPTNEW, NEWSYM, NEWBAS, NEWPRP, RELCAL,
-     &        TOTSYM, NMWALK, DKTRAN, GEOALL
+     &        TOTSYM, NMWALK, DKTRAN, GEOALL, WESTA
       COMMON /GNRINF/ GRADML, PANAS,  TESTIN, OPTWLK, RNHERM,
      &                RNSIRI, RNABAC, GEOCNV, HRINPC, SRINPC, RDINPC, 
      &                RDMLIN, PARCAL, DIRCAL, KCHARG, WRINDX, 
      &                WLKREJ, WALKIN, RNRESP, USRIPR, ITERNR,
      &                ITERMX, IPRUSR, SEGBAS, DOCCSD, OPTNEW, NEWSYM,
      &                LENBAS, NEWBAS, NEWPRP, RELCAL, TOTSYM, NMWALK,
-     &                DKTRAN, GEOALL
+     &                DKTRAN, GEOALL, WESTA
       CHARACTER*70 BASDIR
       COMMON /GNRCHR/ BASDIR
