f2818c126291029d914a2c97c259a30c1f384920
Hans Jørgen Aa. Jensen <hjj@ifk.sdu.dk>
2005-02-18 16:47:03 +0000

    hjaaj: Removed 255 limit in inttra.h, instead use dynamical allocation of these pointer vectors. First step towards removing 255 limit in sirtra.F. Also use LBUFAO from AOINT instead of the old hardwired buffer length of 600.

--- sirius/inttra.h
+++ sirius/inttra.h
@@ -2,7 +2,7 @@
       COMMON/INTTRA/ MSYM,MISH(8),MASH(8),MSSH(8),MOCC(8),MORB(8),
      *               MBAS(8),JORB(8),JBAS(8),JCMO(8),
      *               MOCCT,NMOCCX,MSSHT,MORBT,MBAST,NMBASX,NMORBX,
-     *               ITSMO(255),ITSAO(255),ITSW(255),ITSX(255),MUL(8,8),
+     *               MUL(8,8),
      *               NPQ,NCD,LDABUF,LDAMAX,LASTAD(MAXCHA),
-     *               LASTAE(MAXCHA),IT(256),JTER,NMORBT,NMBAST,
+     *               LASTAE(MAXCHA),JTER,NMORBT,NMBAST,
      *               LTRI,LPQCD,LDABUG,MASHT,NMASHX,LPQRS
