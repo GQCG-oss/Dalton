139df70ab4ebcb78f0889451f597d61e99ff434a
Pawel Salek <pawsa@theochem.kth.se>
2005-04-22 11:35:33 +0000

    * dft/fun-kt.c: enable fourth order KT derivatives./pawsa

--- dft/fun-kt.c
+++ dft/fun-kt.c
@@ -32,40 +32,6 @@ C...   For information on how to get a licence see:
 C...      http://www.kjemi.uio.no/software/dalton/dalton.html
 C
 */
-/*
-C...   Copyright (c) 2005 by the authors of Dalton (see below).
-C...   All Rights Reserved.
-C...
-C...   The source code in this file is part of
-C...   "Dalton, a molecular electronic structure program, Release 2.0
-C...   (2005), written by C. Angeli, K. L. Bak,  V. Bakken, 
-C...   O. Christiansen, R. Cimiraglia, S. Coriani, P. Dahle,
-C...   E. K. Dalskov, T. Enevoldsen, B. Fernandez, C. Haettig,
-C...   K. Hald, A. Halkier, H. Heiberg, T. Helgaker, H. Hettema, 
-C...   H. J. Aa. Jensen, D. Jonsson, P. Joergensen, S. Kirpekar, 
-C...   W. Klopper, R.Kobayashi, H. Koch, O. B. Lutnaes, K. V. Mikkelsen, 
-C...   P. Norman, J.Olsen, M. J. Packer, T. B. Pedersen, Z. Rinkevicius,
-C...   T. A. Ruden, K. Ruud, P. Salek, A. Sanchez de Meras, T. Saue, 
-C...   S. P. A. Sauer, B. Schimmelpfennig, K. O. Sylvester-Hvid, 
-C...   P. R. Taylor, O. Vahtras, D. J. Wilson, H. Agren. 
-C...   This source code is provided under a written licence and may be
-C...   used, copied, transmitted, or stored only in accord with that
-C...   written licence.
-C...
-C...   In particular, no part of the source code or compiled modules may
-C...   be distributed outside the research group of the licence holder.
-C...   This means also that persons (e.g. post-docs) leaving the research
-C...   group of the licence holder may not take any part of Dalton,
-C...   including modified files, with him/her, unless that person has
-C...   obtained his/her own licence.
-C...
-C...   For questions concerning this copyright write to:
-C...      dalton-admin@kjemi.uio.no
-C...
-C...   For information on how to get a licence see:
-C...      http://www.kjemi.uio.no/software/dalton/dalton.html
-C
-*/
 /*-*-mode: C; c-indentation-style: "bsd"; c-basic-offset: 4; -*-*/
 /* fun-kt.c:
    implementation of KT functional and its derivatives.
@@ -86,7 +52,7 @@ C
 #define _XOPEN_SOURCE_EXTENDED 1
 #endif
 
-/* #define FOURTH_ORDER_DERIVATIVES */
+#define FOURTH_ORDER_DERIVATIVES
 
 #include <math.h>
 #include <stddef.h>
@@ -100,13 +66,14 @@ static int  kt_isgga(void) { return 1; }
 static int  kt_read(const char* conf_line);
 static real kt_energy(const FunDensProp* dens_prop);
 static void kt_first(FunFirstFuncDrv *ds, real factor, 
-                        const FunDensProp* dens_prop);
+                     const FunDensProp* dens_prop);
 static void kt_second(FunSecondFuncDrv *ds, real factor, 
-                        const FunDensProp* dens_prop);
+                      const FunDensProp* dens_prop);
 static void kt_third(FunThirdFuncDrv *ds, real factor, 
-                        const FunDensProp* dens_prop);
+                     const FunDensProp* dens_prop);
 #ifdef FOURTH_ORDER_DERIVATIVES
-static void kt_fourth(FourthFuncDrv *ds, real factor, const FunDensProp* dens_prop);
+static void kt_fourth(FunFourthFuncDrv *ds, real factor,
+                      const FunDensProp* dens_prop);
 #endif
 
 
@@ -338,7 +305,7 @@ kt_third(FunThirdFuncDrv *ds, real factor, const FunDensProp* dp)
 
 #ifdef FOURTH_ORDER_DERIVATIVES
 static void
-kt_fourth(FourthFuncDrv *ds, real factor, const FunDensProp* dp)
+kt_fourth(FunFourthFuncDrv *ds, real factor, const FunDensProp* dp)
 {       
     real ra, xa, ra43, ra13, ra23, ram1, ram2;
     real rb, xb, rb43, rb13, rb23, rbm1, rbm2;
@@ -384,20 +351,20 @@ kt_fourth(FourthFuncDrv *ds, real factor, const FunDensProp* dp)
         faRRZZ = 1.0/xa*faRZZ;
         faRRRR = -5.0/(3.0*ra*ra43) -20.0*ra/(3.0*denoma) 
                  -96.0*ra13/denoma2 +256.0*ra*ra23/denoma3;
-        ds->df10000 += factor*faR;
-        ds->df00100 += factor*faZ;
-        ds->df10100 += factor*faRZ;
-        ds->df20000 += factor*faRR*t2a;
-        ds->df00200 += factor*faZZ;
-        ds->df30000 += factor*faRRR*t1a;
-        ds->df00300 += factor*faZZZ;
-        ds->df20100 += factor*faRRZ*t2a;
-        ds->df10200 += factor*faRZZ;
-        ds->df40000 += factor*faRRRR*t3a;
-        ds->df00400 += factor*faZZZZ;
-        ds->df30100 += factor*faRRRZ;
-        ds->df10300 += factor*faRZZZ;
-        ds->df20200 += factor*faRRZZ;
+        ds->df1000 += factor*faR;
+        ds->df0010 += factor*faZ;
+        ds->df1010 += factor*faRZ;
+        ds->df2000 += factor*faRR*t2a;
+        ds->df0020 += factor*faZZ;
+        ds->df3000 += factor*faRRR*t1a;
+        ds->df0030 += factor*faZZZ;
+        ds->df2010 += factor*faRRZ*t2a;
+        ds->df1020 += factor*faRZZ;
+        ds->df4000 += factor*faRRRR*t3a;
+        ds->df0040 += factor*faZZZZ;
+        ds->df3010 += factor*faRRRZ;
+        ds->df1030 += factor*faRZZZ;
+        ds->df2020 += factor*faRRZZ;
     }
     if (dp->rhob >KT_THRESHOLD) {
         xb = dp->gradb;
@@ -429,20 +396,20 @@ kt_fourth(FourthFuncDrv *ds, real factor, const FunDensProp* dp)
         fbRRZZ = 1.0/xb*fbRZZ;
         fbRRRR = -5.0/(3.0*rb*rb43) - 20.0*rb/(3.0*denomb) -
                  96.0*rb13/denomb2 + 256.0*rb*rb23/denomb3;
-        ds->df01000 += factor*fbR;
-        ds->df00010 += factor*fbZ;
-        ds->df01010 += factor*fbRZ;
-        ds->df02000 += factor*fbRR*t2b;
-        ds->df00020 += factor*fbZZ;
-        ds->df03000 += factor*fbRRR*t1b;
-        ds->df00030 += factor*fbZZZ;
-        ds->df02010 += factor*fbRRZ*t2b;
-        ds->df01020 += factor*fbRZZ;
-        ds->df04000 += factor*fbRRRR*t3b;
-        ds->df00040 += factor*fbZZZZ;
-        ds->df03010 += factor*fbRRRZ;
-        ds->df01030 += factor*fbRZZZ;
-        ds->df02020 += factor*fbRRZZ;
+        ds->df0100 += factor*fbR;
+        ds->df0001 += factor*fbZ;
+        ds->df0101 += factor*fbRZ;
+        ds->df0200 += factor*fbRR*t2b;
+        ds->df0002 += factor*fbZZ;
+        ds->df0300 += factor*fbRRR*t1b;
+        ds->df0003 += factor*fbZZZ;
+        ds->df0201 += factor*fbRRZ*t2b;
+        ds->df0102 += factor*fbRZZ;
+        ds->df0400 += factor*fbRRRR*t3b;
+        ds->df0004 += factor*fbZZZZ;
+        ds->df0301 += factor*fbRRRZ;
+        ds->df0103 += factor*fbRZZZ;
+        ds->df0202 += factor*fbRRZZ;
     }
 }
 #endif
