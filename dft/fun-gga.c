f7473884b398f202e926bcaaf9664976dbb7f5af
Pawel Salek <pawsa@theochem.kth.se>
2005-04-27 07:50:23 +0000

    * dft/fun-gga.c, dft/functionals.[hc]: add BHandH and BHandHLYP functionals   for Gaussian compatibility. Rename GGAKey to Combine. * Doc/Sirius.tex: update functional doc. * pdpack/scatter-io.c: compile on old DEC machines./pawsa

--- dft/fun-gga.c
+++ dft/fun-gga.c
@@ -32,40 +32,6 @@ C...   For information on how to get a licence see:
 C...      http://www.kjemi.uio.no/software/dalton/dalton.html
 C
 */
-/*
-C...   Copyright (c) 2005 by the authors of Dalton (see below).
-C...   All Rights Reserved.
-C...
-C...   The source code in this file is part of
-C...   "Dalton, a molecular electronic structure program, Release 2.0
-C...   (2005), written by C. Angeli, K. L. Bak,  V. Bakken, 
-C...   O. Christiansen, R. Cimiraglia, S. Coriani, P. Dahle,
-C...   E. K. Dalskov, T. Enevoldsen, B. Fernandez, C. Haettig,
-C...   K. Hald, A. Halkier, H. Heiberg, T. Helgaker, H. Hettema, 
-C...   H. J. Aa. Jensen, D. Jonsson, P. Joergensen, S. Kirpekar, 
-C...   W. Klopper, R.Kobayashi, H. Koch, O. B. Lutnaes, K. V. Mikkelsen, 
-C...   P. Norman, J.Olsen, M. J. Packer, T. B. Pedersen, Z. Rinkevicius,
-C...   T. A. Ruden, K. Ruud, P. Salek, A. Sanchez de Meras, T. Saue, 
-C...   S. P. A. Sauer, B. Schimmelpfennig, K. O. Sylvester-Hvid, 
-C...   P. R. Taylor, O. Vahtras, D. J. Wilson, H. Agren. 
-C...   This source code is provided under a written licence and may be
-C...   used, copied, transmitted, or stored only in accord with that
-C...   written licence.
-C...
-C...   In particular, no part of the source code or compiled modules may
-C...   be distributed outside the research group of the licence holder.
-C...   This means also that persons (e.g. post-docs) leaving the research
-C...   group of the licence holder may not take any part of Dalton,
-C...   including modified files, with him/her, unless that person has
-C...   obtained his/her own licence.
-C...
-C...   For questions concerning this copyright write to:
-C...      dalton-admin@kjemi.uio.no
-C...
-C...   For information on how to get a licence see:
-C...      http://www.kjemi.uio.no/software/dalton/dalton.html
-C
-*/
 /*-*-mode: C; c-indentation-style: "bsd"; c-basic-offset: 4; -*-*/
 /* fun-gga.c:
    implementation of a functional being a linear combination of 
@@ -104,6 +70,8 @@ static int  ldagauss_read(const char* conf_line);
 static int  blyp_read(const char* conf_line);
 static int  b3lyp_read(const char* conf_line);
 static int  b3lypgauss_read(const char* conf_line);
+static int  bhandh_read(const char* conf_line);
+static int  bhandhlyp_read(const char* conf_line);
 static int  bp86_read(const char* conf_line);
 static int  bpw91_read(const char* conf_line);
 static int  b3p86_read(const char* conf_line);
@@ -142,9 +110,12 @@ Functional B3LYPFunctional =  GGA_FUNCTIONAL("B3LYP",   b3lyp_read);
 Functional B3LYPGaussFunctional = GGA_FUNCTIONAL("B3LYP-G", b3lypgauss_read);
 Functional B3P86Functional =  GGA_FUNCTIONAL("B3P86",   b3p86_read);
 Functional B3P86GFunctional = GGA_FUNCTIONAL("B3P86-G", b3p86g_read);
+Functional BHandHFunctional = GGA_FUNCTIONAL("BHandH",  bhandh_read);
+Functional BHandHLYPFunctional = GGA_FUNCTIONAL("BHandHLYP", bhandhlyp_read);
 Functional BLYPFunctional =   GGA_FUNCTIONAL("BLYP",    blyp_read);
 Functional BP86Functional =   GGA_FUNCTIONAL("BP86",    bp86_read);
 Functional BPW91Functional =  GGA_FUNCTIONAL("BPW91",   bpw91_read);
+Functional CombineFunctional =GGA_FUNCTIONAL("Combine", gga_key_read);
 Functional GGAKeyFunctional = GGA_FUNCTIONAL("GGAKey",  gga_key_read);
 Functional KT1Functional =    GGA_FUNCTIONAL("KT1",     kt1_read);
 Functional KT2Functional =    GGA_FUNCTIONAL("KT2",     kt2_read);
@@ -276,6 +247,25 @@ b3lypgauss_read(const char* conf_line)
     gga_fun_list = add_functional(gga_fun_list, &LYPFunctional,    lypw);
     gga_fun_list = add_functional(gga_fun_list, &VWN3Functional,   1-lypw);
     fun_set_hf_weight(1-dirw);
+    return 1;
+}
+
+static int
+bhandh_read(const char* conf_line)
+{
+    gga_fun_list = add_functional(gga_fun_list, &SlaterFunctional, 0.5);
+    gga_fun_list = add_functional(gga_fun_list, &LYPFunctional,    1.0);
+    fun_set_hf_weight(0.5);
+    return 1;
+}
+
+static int
+bhandhlyp_read(const char* conf_line)
+{
+    gga_fun_list = add_functional(gga_fun_list, &SlaterFunctional, 0.5);
+    gga_fun_list = add_functional(gga_fun_list, &BeckeFunctional,  0.5);
+    gga_fun_list = add_functional(gga_fun_list, &LYPFunctional,    1.0);
+    fun_set_hf_weight(0.5);
     return 1;
 }
 
