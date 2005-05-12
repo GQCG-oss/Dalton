534844d046ae6dc9ca7570d5e7b92c6abedbebf6
Hans Jørgen Aa. Jensen <hjj@ifk.sdu.dk>
2005-05-12 11:54:25 +0000

    hjaaj: fixed Fortran errors for new incore code  (correctiosn necessary to compile with g77): 1) her2drv.F, herpar.F:  replaced e.g. illegal AOSAVE .EQ. .FALSE. with .NOT.AOSAVE  (AOSAVE .EQV. .FALSE. would also have been OK, logicals are  compared with .EQV. and .NEQV., not .EQ. and .NEQ.) 2) incore.h:  declare CORE double precision (was REAL*4 in subroutine CLEAR_INCOREMEM() !)  g77 does not allow MAX and MIN in parameter statements  g77 does not allow zero length arrays in common blocks

--- include/incore.h
+++ include/incore.h
@@ -1,15 +1,22 @@
-      PARAMETER (MXTSK=(MXSHEL*(MXSHEL+1)/2), IEIR=0, 
-     &     MXSHL_KEEP=MAX(MXTSK,IEIR), MMWORK=0)
+C     PARAMETER (MXTSK=(MXSHEL*(MXSHEL+1)/2), IEIR=0, MMWORK=0,
+C    &     MXSHL_KEEP=MAX(MXTSK,IEIR), MNSHL_KEEP=MIN(MXTSK,IEIR) )
+Chj 11-May-2005: changes needed for compilation with g77:
+C     MAX and MIN not allowed by "g77"; zero size arrays not allowed
+      PARAMETER (MXTSK=(MXSHEL*(MXSHEL+1)/2), IEIR=1, MMWORK=1,
+     &     MXSHL_KEEP=MXTSK, MNSHL_KEEP=IEIR)
+      DOUBLE PRECISION CORE
       LOGICAL AOSAVE, LINTSV, INITX, LINTMP, MSAVE
       COMMON /INCORE/ CORE(MMWORK), ISCORE, MMCORE, I_SHL, N_SHL,
      &     INDX_SHL(MXSHL_KEEP), INDX_SHL1, INDX_SHL2, INDX_SHL3, 
      &     INDX_SHL4, AOSAVE, LINTSV, INITX, LMCORE, JSCORE,
-     &     INDX_C(3,MIN(MXTSK,IEIR)), ITOTNT, LINTMP, MSAVE,
+     &     INDX_C(3,MNSHL_KEEP), ITOTNT, LINTMP, MSAVE,
      & 	   IPREVA, IPREVB, IPREVC, IPREVD
-      /*#if defined (VAR_MPI)
+/*
+#if defined (VAR_MPI)
       LOGICAL INDMPI
       COMMON /MPISAV/ INDMPI(MXTSK)
-      #endif*/
+#endif
+*/
 /* MMWORK and IEIR must be set to nonzero values here if the 
   AOSAVE-feature of DALTON is to be used. Suggested values: 
   MMWORK=80000000 and IEIR=3000000, 
