088c4a34299a2bb4b2c99c33abc34209062d21aa
Pawel Salek <pawsa@theochem.kth.se>
2005-05-14 09:20:50 +0000

    * include/incore.h: use fortran comment syntax - fix compilation with xlf./pawsa

--- include/incore.h
+++ include/incore.h
@@ -10,14 +10,15 @@ C     MAX and MIN not allowed by "g77"; zero size arrays not allowed
      &     INDX_SHL(MXSHL_KEEP), INDX_SHL1, INDX_SHL2, INDX_SHL3, 
      &     INDX_SHL4, AOSAVE, LINTSV, INITX, LMCORE, JSCORE,
      &     INDX_C(3,MNSHL_KEEP), ITOTNT, LINTMP, MSAVE,
-     & 	   IPREVA, IPREVB, IPREVC, IPREVD
-/*
+     &     IPREVA, IPREVB, IPREVC, IPREVD
+#if 0
 #if defined (VAR_MPI)
       LOGICAL INDMPI
       COMMON /MPISAV/ INDMPI(MXTSK)
 #endif
-*/
+
 /* MMWORK and IEIR must be set to nonzero values here if the 
   AOSAVE-feature of DALTON is to be used. Suggested values: 
   MMWORK=80000000 and IEIR=3000000, 
   if your hardware can take it.*/
+#endif
