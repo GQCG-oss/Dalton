b453bc958e86a508fa676e736e5837005843ae5b
Pawel Salek <pawsa@theochem.kth.se>
2005-05-16 12:33:45 +0000

    * abacus/linux_mem_allo.c: squash a "missing prototype" warning./pawsa

--- abacus/linux_mem_allo.c
+++ abacus/linux_mem_allo.c
@@ -31,73 +31,6 @@ C...
 C...   For information on how to get a licence see:
 C...      http://www.kjemi.uio.no/software/dalton/dalton.html
 C
-*/
-/*
-C...   Copyright (c) 2005 by the authors of Dalton (see below).
-C...   All Rights Reserved.
-C...
-C...   The source code in this file is part of
-C...   "Dalton, a molecular electronic structure program, Release 2.0
-C...   (2005), written by C. Angeli, K. L. Bak,  V. Bakken, 
-C...   O. Christiansen, R. Cimiraglia, S. Coriani, P. Dahle,
-C...   E. K. Dalskov, T. Enevoldsen, B. Fernandez, C. Haettig,
-C...   K. Hald, A. Halkier, H. Heiberg, T. Helgaker, H. Hettema, 
-C...   H. J. Aa. Jensen, D. Jonsson, P. Joergensen, S. Kirpekar, 
-C...   W. Klopper, R.Kobayashi, H. Koch, O. B. Lutnaes, K. V. Mikkelsen, 
-C...   P. Norman, J.Olsen, M. J. Packer, T. B. Pedersen, Z. Rinkevicius,
-C...   T. A. Ruden, K. Ruud, P. Salek, A. Sanchez de Meras, T. Saue, 
-C...   S. P. A. Sauer, B. Schimmelpfennig, K. O. Sylvester-Hvid, 
-C...   P. R. Taylor, O. Vahtras, D. J. Wilson, H. Agren. 
-C...   This source code is provided under a written licence and may be
-C...   used, copied, transmitted, or stored only in accord with that
-C...   written licence.
-C...
-C...   In particular, no part of the source code or compiled modules may
-C...   be distributed outside the research group of the licence holder.
-C...   This means also that persons (e.g. post-docs) leaving the research
-C...   group of the licence holder may not take any part of Dalton,
-C...   including modified files, with him/her, unless that person has
-C...   obtained his/her own licence.
-C...
-C...   For questions concerning this copyright write to:
-C...      dalton-admin@kjemi.uio.no
-C...
-C...   For information on how to get a licence see:
-C...      http://www.kjemi.uio.no/software/dalton/dalton.html
-C
-*/
-/*
-       Copyright (c) 2001 by the authors of Dalton (see below).
-       All Rights Reserved.
-    
-       The source code in this file is part of
-       "Dalton, a molecular electronic structure program, Release 1.2
-       (2001), written by T. Helgaker, H. J. Aa. Jensen, P. Joergensen,
-       J. Olsen, K. Ruud, H. Agren, A.A. Auer, K.L. Bak, V. Bakken,
-       O. Christiansen, S. Coriani, P. Dahle, E. K. Dalskov,
-       T. Enevoldsen, B. Fernandez, C. Haettig, K. Hald, A. Halkier,
-       H. Heiberg, H. Hettema, D. Jonsson, S. Kirpekar, R. Kobayashi,
-       H. Koch, K. V. Mikkelsen, P. Norman, M. J. Packer,
-       T. B. Pedersen, T. A. Ruden, A. Sanchez, T. Saue, S. P. A. Sauer,
-       B. Schimmelpfennig, K. O. Sylvester-Hvid, P. R. Taylor,
-       and O. Vahtras"
-    
-       This source code is provided under a written licence and may be
-       used, copied, transmitted, or stored only in accord with that
-       written licence.
-    
-       In particular, no part of the source code or compiled modules may
-       be distributed outside the research group of the licence holder.
-       This means also that persons (e.g. post-docs) leaving the research
-       group of the licence holder may not take any part of Dalton,
-       including modified files, with him/her, unless that person has
-       obtained his/her own licence.
-    
-       For questions concerning this copyright write to:
-          dalton-admin@kjemi.uio.no
-    
-       For information on how to get a licence see:
-          http://www.kjemi.uio.no/software/dalton/dalton.html
 
  ******************************************************
  * linux_mem_allo: dynamically allocates memory       *
@@ -111,7 +44,8 @@ C
  ******************************************************/
 #include <stdio.h>
 #include <stdlib.h>
-  
+
+void dalton_quit(const char* format, ...);
 typedef void (*DaltonDriver)(double* work, int* lmwork, double* wrkdlm,
                              int* master, int* mynum);
 extern DaltonDriver nodstr_;
