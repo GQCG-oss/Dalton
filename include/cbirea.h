470e7cc9c0a0330f6f171b2d0b436f847cca0a07
Hans Jørgen Aa. Jensen <hjj@ifk.sdu.dk>
2005-09-15 10:46:24 +0000

    hjaaj: increased MAXPRD (default for .MAXPRI) from 25 to 35 so it is        big enough for all elements in the ano-5 basis used for EWMO/EHT.        (For example for Pt in PtH.)

--- include/cbirea.h
+++ include/cbirea.h
@@ -1,18 +1,14 @@
 C
-C    cbirea.h - common block for reading of MOLECULE.INP (herrdn.F)
+C     cbirea.h - common block for reading of MOLECULE.INP (herrdn.F)
 C
-C    MAXPRD = default value for MAXPRI
-#if defined (SYS_T3D)
-      PARAMETER ( MAXPRD = 14 )
-#else
-      PARAMETER ( MAXPRD = 25 )
-#endif
+C     MAXPRD = default value for MAXPRI
+      PARAMETER ( MAXPRD = 35 )
 C
-      LOGICAL BIGVC,  GENCON, INPTST, DIRAC,  BASIS,  ATOMBA,
-     &        UNCONT, OLDNRM, WRTLIN, ANGS, ATOMDF, NODDYDF
+      LOGICAL         BIGVC,  GENCON, INPTST, DIRAC,  BASIS,  ATOMBA,
+     &                UNCONT, OLDNRM, WRTLIN, ANGS,   ATOMDF, NODDYDF
       COMMON /CBIREA/ ZCMVAL,
      &                IPREAD, MAXPRI, LUMLCL, LCMMAX, NCMSTR, NCMEND,
      &                BIGVC,  GENCON, INPTST, DIRAC,  BASIS,  ATOMBA,
-     &                UNCONT, OLDNRM, WRTLIN, ANGS, ATOMDF, NODDYDF
-      CHARACTER*80 BASNAM,AUXNAM
+     &                UNCONT, OLDNRM, WRTLIN, ANGS,   ATOMDF, NODDYDF
+      CHARACTER*80    BASNAM,AUXNAM
       COMMON /CBIRET/ BASNAM,AUXNAM
