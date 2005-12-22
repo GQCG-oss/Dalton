a2dace6b3949d9e5ec7c65f70361ff3dab4f11ec
Hans Jørgen Aa. Jensen <hjj@ifk.sdu.dk>
2005-12-22 08:26:47 +0000

    hjaaj: moved infpop.h from include/ to sirius/;        allocate POPNAB(MXCENT,MXCENT),POPABI(MXCENT,MXCENT)        from work instead of statically in common.

--- sirius/infpop.h
+++ sirius/infpop.h
@@ -1,5 +1,5 @@
+C hjaaj sep 2005: make POPNAB etc. dynamic -- maybe remove completely infpop.h ?
       PARAMETER ( MAXTYP = 20 )
       COMMON /INFPOP/ TITLEM(24),DIPOL(3),QPOL(6),QQFAC(3),
      *                IFXYZ(3),JFXYZ(3),NAME(MXCENT),POPNUC(MXCENT),
-     *                POPNAB(MXCENT,MXCENT),POPABI(MXCENT,MXCENT),
      *                POPTYP(20,MXCENT),POPTY(20,MXCENT)
