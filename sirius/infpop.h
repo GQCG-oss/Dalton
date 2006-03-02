2c729877b747caa277f1ee178898fb5b0c892a6e
Hans Jørgen Aa. Jensen <hjj@ifk.sdu.dk>
2006-03-02 13:45:14 +0000

    hjaaj: increased MAXTYP so sirpop.F also can treat g- and h-orbitals.

--- sirius/infpop.h
+++ sirius/infpop.h
@@ -1,5 +1,6 @@
+C File: infpop.h
 C hjaaj sep 2005: make POPNAB etc. dynamic -- maybe remove completely infpop.h ?
-      PARAMETER ( MAXTYP = 20 )
+      PARAMETER ( MAXTYP = 50 )
       COMMON /INFPOP/ TITLEM(24),DIPOL(3),QPOL(6),QQFAC(3),
-     *                IFXYZ(3),JFXYZ(3),NAME(MXCENT),POPNUC(MXCENT),
-     *                POPTYP(20,MXCENT),POPTY(20,MXCENT)
+     &                IFXYZ(3),JFXYZ(3),NAME(MXCENT),POPNUC(MXCENT),
+     &                POPTYP(MAXTYP,MXCENT), POPTY(MAXTYP,MXCENT)
