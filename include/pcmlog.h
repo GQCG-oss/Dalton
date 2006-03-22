62f585a8e343e21d599b0b8e8e48c5f069446078
Bene <dalton-wizards@kjemi.uio.no>
2006-03-22 17:42:29 +0000

    PCM contribution to the triplet quadratic response. The triplet PCM-QR contribution is computed in rspief2.F. In order to activate it, it is necessary to add the keyword .NEWQR in the PCM section. (default is NEWQR=.false.). rspief2.F can handle SCF response without symmetry (not yet tested) and perform both Singlet and Triplet response calculations.
    Luca and Zilvinas feb 2006

--- include/pcmlog.h
+++ include/pcmlog.h
@@ -4,6 +4,6 @@ C
 C PCM:    True if we perform a PCM calculation
 C OUTFLD: Local field correction for pure liquids
       LOGICAL PCM,OUTFLD,NEWMAT,LOCFLD,NONEQ,NEQRSP,
-     $        NPCMIN,OLDCEN
+     $        NPCMIN,OLDCEN,NEWQR
       COMMON /PCMLOG/ PCM,OUTFLD,NEWMAT,LOCFLD,NONEQ,NEQRSP,
-     $                NPCMIN,OLDCEN
+     $                NPCMIN,OLDCEN,NEWQR
