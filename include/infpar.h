41dc1efe350850f29a8252dcab47a4d963840be6
Hans Jørgen Aa. Jensen <hjj@ifk.sdu.dk>
2006-06-11 21:49:13 +0000

    hjaaj: merged two common blocks to one

--- include/infpar.h
+++ include/infpar.h
@@ -6,13 +6,11 @@
 extern struct common_infpar {
     int nodtot, nodeid[MAXNOD+1], ncode,  iprpar, mtottk, ntask,  
 	nfmat,  ndegdi, master, mynum,  mytid,  timing,   slave,
-	debug;
+	debug, pario;
     char nodnam[MAXNOD][20], myname[20];
 } infpar_;
-extern struct common_parioc {
-    int pario;
-} parioc_;
 #else
+C File: infpar.h
 C
 C     Parameters NPARI must be updated after changes (for parallelization)
 C
@@ -22,13 +20,13 @@ C           Character (NODNAM,...) should NOT be sent to slaves
 C
       PARAMETER (MAXNOD=128, MAXCL2=10000, MAXTSK=MXSHEL*(MXSHEL+1)/2)
       PARAMETER (NPARI = (MAXNOD + 1) + 7)
-      LOGICAL SLAVE, TIMING, DEBUG, PARIO
-      CHARACTER*20 NODNAM, MYNAME
+      LOGICAL        SLAVE, TIMING, DEBUG, PARIO
+      CHARACTER*20   NODNAM, MYNAME
       DIMENSION NODEID(0:MAXNOD), NODNAM(0:MAXNOD)
       COMMON /INFPAR/ NODTOT, NODEID, NCODE, IPRPAR, MTOTTK, NTASK,  
      &                NFMAT,  NDEGDI, 
      &                MASTER, MYNUM,  MYTID,
-     &                TIMING, SLAVE,  DEBUG,
+     &                SLAVE,  TIMING, DEBUG, PARIO,
      &                NODNAM, MYNAME
-      COMMON /PARIOC/ PARIO
+C -- end of infpar.h --
 #endif
