711007a716d4ecf56926af13b2423f7e3678eef4
Hans Jørgen Aa. Jensen <hjj@ifk.sdu.dk>
2006-08-02 12:36:58 +0000

    hjaaj: revoking previous changes by mistake made on trunk

--- include/dfterg.h
+++ include/dfterg.h
@@ -1,6 +1,2 @@
-C File: dfterg.h
-C
-C     ESRDFTY : effective energy correction for SR-DFT,
-C               is calculated in SIRFCK.
-      COMMON /DFTERG/ EDFTX, EDFTC, EDFTY, EDFTK, ESRDFTY,
+      COMMON /DFTERG/ EDFTX, EDFTC, EDFTY, EDFTK,
      &                WDFTX, WDFTC, WDFTL, WDFTB, WDFTK
--- include/infinp.h
+++ include/infinp.h
@@ -1,13 +1,10 @@
-C File : infinp.h
-C
 c  -*- mode:fortran; fortran-continuation-string: "&" -*-
 C     this common block contains general Sirius input read in sirius/sirinp.F
 C     (specified under **WAVE FUNCTION).  /hjaaj Oct 2003
 C     - SCF specific input is in scbrhf.h
 C     - orbital specifications are in inforb.h
-      INTEGER         NFLAG, MAXRTS, MXFELT
+      INTEGER NFLAG, MAXRTS, MXFELT
       PARAMETER (NFLAG = 80, MAXRTS = 20, MXFELT = 20)
-C
       INTEGER         NFIELD, ISPIN,ISTATE,LSYM,NACTEL, MCTYPE,
      *                LSOLMX,NLMSOL,NELMN1,NELMX1,NELMN3,NELMX3,
      *                LROOTS,NROOTS,IROOT,
@@ -24,32 +21,28 @@ C
      *                MAXMAC,MAXMIC,MAXJT,MAXCIT,MAXUIT,MAXAPM,MAXABS,
      *                ITRLVL,ITRFIN,JCHSYM,JCHORB,
      *                NROOCI,ISTACI, MXCIMA, ICICNO,IMCCNO
-C
-      LOGICAL         FLAG,  DOSCF, DOMP2, DOCINO,DOCI,  DOMC,  DORSP,
-     &                FCVORB,LNOROT,LMOORD,DIRFCK,CORHOL,CORRLX,RESPHP,
-     &                JOLSEN,ABAIPH,INERSI,INERSF,DODFT, DONEVPT,HSROHF,
-     &                BOYORB,PIPORB,
-     &                DOCISRDFT,DOHFSRDFT,DOMCSRDFT,ADDSRI,SRHYBR
-      COMMON /LOGINP/ FLAG(NFLAG), DOSCF,DOMP2,DOCINO,DOCI,DOMC,DORSP,
+      LOGICAL FLAG,  DORHF, DOMP2, DOCINO,DOCI,  DOMC,  DORSP,FCVORB,
+     &        LNOROT,LMOORD,DIRFCK,CORHOL,CORRLX,RESPHP,JOLSEN,ABAIPH,
+     &        INERSI,INERSF,SUPSYM,DODFT, DONEVPT,HSROHF ,BOYORB,PIPORB
+      COMMON /LOGINP/ FLAG(NFLAG), DORHF,DOMP2,DOCINO,DOCI,DOMC,DORSP,
      &                FCVORB,LNOROT,LMOORD,DIRFCK,CORHOL,CORRLX,RESPHP,
      &                JOLSEN,ABAIPH,INERSI,INERSF,DODFT,DONEVPT,HSROHF,
-     &                BOYORB,PIPORB,
-     &                DOCISRDFT,DOHFSRDFT,DOMCSRDFT,ADDSRI,SRHYBR
-      LOGICAL         SUPSYM, DORHF
-      EQUIVALENCE (SUPSYM,FLAG(17)), (DOSCF,DORHF)
-C
-      REAL*8          SPIN, POTNUC, EPSOL,EPSTAT,EPPN,RSOL,
-     &                THRGRD, THRPWF, THRCI, THRMC, THRCGR,
+     &                BOYORB,PIPORB
+      EQUIVALENCE (SUPSYM,FLAG(17))
+#if defined (SYS_CRAY)
+      REAL             SPIN,POTNUC, EPSOL,EPSTAT,EPPN,RSOL,
+#else
+      DOUBLE PRECISION SPIN,POTNUC, EPSOL,EPSTAT,EPPN,RSOL,
+#endif
+     &                THRGRD, THRPWF, THRRHF, THRCI, THRMC, THRCGR,
      &                EFIELD, TITMOL, CMAXMO, THROVL,
-     &                THRSSY, DEFLVL, THRCIDFT
-      COMMON /RELINP/ SPIN, POTNUC, EPSOL,EPSTAT,EPPN,RSOL(3),
+     &                THRSSY, DEFLVL
+      COMMON /RELINP/ SPIN,POTNUC, EPSOL,EPSTAT,EPPN,RSOL(3),
      &                THRGRD, THRPWF, THRCI, THRMC, THRCGR,
      &                EFIELD(MXFELT), TITMOL(12,2), CMAXMO, THROVL,
-     &                THRSSY, DEFLVL, THRCIDFT
-C
+     &                THRSSY, DEFLVL
       CHARACTER*60 TITLE
       CHARACTER*4  CENT,   TYPE
       CHARACTER*8  LFIELD
       COMMON /CHRINP/ TITLE(6), CENT(MXCORB), TYPE(MXCORB),
      &                LFIELD(MXFELT)
-C-- end of infinp.h --
