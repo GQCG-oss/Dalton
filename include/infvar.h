bba4ddf833d916daa4bc20ea2bc079628b327bea
Hans Jørgen Aa. Jensen <hjj@ifk.sdu.dk>
2007-08-24 16:29:21 +0000

    hjaaj: removed INTEGER*2 JWOP option, with current memory sizes this is obsolete. Increase MAXWOP from 100k to 200k.

--- include/infvar.h
+++ include/infvar.h
@@ -1,11 +1,10 @@
-#if defined (VAR_STAR2)
-      INTEGER*2 JWOP
-#else
-      INTEGER JWOP
-#endif
-      INTEGER NCONF, NWOPT, NVAR, JWOPSY, NWOP, NWOPH, NVARH, MAXWOP, 
-     *        NCDETS
-C     MAXWOP = maximum number of rotations (dimension of JWOP)
-      PARAMETER ( MAXWOP = 110000 )
+C
+C file: infvar.h (originally from SIRIUS)
+C       contains INFormation on number and symmetry of MCSCF VARiables
+C
+      INTEGER MAXWOP, JWOP,
+     &        NCONF, NWOPT, NVAR, JWOPSY, NWOP, NWOPH, NVARH, NCDETS
+C     MAXWOP = maximum number of orbital rotations (dimension of JWOP)
+      PARAMETER ( MAXWOP = 200000 )
       COMMON /INFVAR/ JWOP(2,MAXWOP),
-     *                NCONF,NWOPT,NVAR,JWOPSY,NWOP(8),NWOPH,NVARH,NCDETS
+     &                NCONF,NWOPT,NVAR,JWOPSY,NWOP(8),NWOPH,NVARH,NCDETS
