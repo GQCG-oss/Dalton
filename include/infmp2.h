4911eb2163b70024a093612dc75cf0eb724c6810
Hans Jørgen Aa. Jensen <hjj@ifk.sdu.dk>
2007-09-18 08:58:56 +0000

    hjaaj: moved IPRMP2 from infpri.h to infmp2.h

--- include/infmp2.h
+++ include/infmp2.h
@@ -1,6 +1,13 @@
-C     Off-set to (bj) block of kappa(ai,bj) is IADR1(B,J)
+C  -- infmp2.h -- Do not change without checking with Hans Joergen Aa. Jensen  /l.r. Sep 2007, hjaaj/
+C
+C     Information for ".MP2" (sirius/sirmp2.F) and ".SOPPA" in RESPONS (rsp/rspsoppa.F)
+C     Also used for ".SOPPA" in abacus/abadrv.F.
+C
+C     Off-set to (bj) block of kappa(ai,bj) is IADR1(B,J), which is calculated when needed.
+C
       PARAMETER (MAXVIR = MXCORB)
-      LOGICAL MP2FRO
-      COMMON /INFMP2/ NFRMP2(8), NPHSYM(8), NPHMAX, NP, NH,
-     &       IPHORD(MXCORB), INDXPH(MXCORB), IFRMP2(MXCORB), NPHTOT(8),
-     &       MP2FRO
+      LOGICAL MP2FRO, SOSMP2, SCSMP2, TDAMP2
+      COMMON /INFMP2/ IPRMP2, NFRMP2(8), NPHSYM(8), NPHMAX, NP, NH,
+     &        IPHORD(MXCORB), INDXPH(MXCORB), IFRMP2(MXCORB), NPHTOT(8),
+     &        MP2FRO, SOSMP2, SCSMP2, TDAMP2
+C  -- end of infmp2.h --
