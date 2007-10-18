18c9321b471b6caa7bc7df0ff8fea4aa21af8f5f
Hans Jørgen Aa. Jensen <hjj@ifk.sdu.dk>
2007-10-18 18:06:25 +0000

    hjaaj bugfix, I missed a comma in previous commit

--- include/magone.h
+++ include/magone.h
@@ -5,6 +5,6 @@ C -- FILE: magone.h --
      &                MNQCC, MELFLD, DISCHA, MSECND, MTHIRD
       COMMON /MAGONE/ MPRINT, MPQUAD,
      &                MSKIP,  MNODC, MNODV, NST, SST, MDIASU,
-     &                TOFILE, MCUT,  MPOINT, MQUADR, ALLCOM
+     &                TOFILE, MCUT,  MPOINT, MQUADR, ALLCOM,
      &                MNQCC, MELFLD, DISCHA, MSECND, MTHIRD
 C -- end of magone.h --
