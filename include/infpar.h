14c0010e1e66a60452836fcc31a532f9ea28dca2
Hans Jørgen Aa. Jensen <hjj@ifk.sdu.dk>
2007-10-23 12:03:12 +0000

    hjaaj: BUGFIX, fixed that parallel tests wouldn't run. Reason: SLAVE (= .true.) was broadcast to slaves,  therefore the slaves all "thought" they were a master  and tried to open MOLECULE.INP#nnn as an old file.  I have reordered variables and changed NPARI such that only  those variables we want to transfer are transferred.

--- include/infpar.h
+++ include/infpar.h
@@ -2,7 +2,7 @@
 ! -- infpar.h --
 !     my_MPI_INTEGER is used both in .c and .F routines in MPI calls
 !        so we can handle "-i8" compilations on 32-bit machines,
-!        using INT_STAR8 /Jan-2007 hjaaj
+!        using VAR_INT64 /Jan-2007 hjaaj
 #endif
 #if defined (VAR_INT64)
 #define my_MPI_INTEGER MPI_INTEGER8
@@ -14,16 +14,16 @@
 #define MAXNOD 200
 #define MAXCL2 10000
 #define MAXTSK (MXSHEL*(MXSHEL+1)/2)
-#define NPARI  ((MAXNOD + 1) + 7)
+#define NPARI  ((MAXNOD + 1) + 6)
 extern struct common_infpar {
 #if defined (VAR_INT64)
-    long iprpar, ntask, ncode, ndegdi, master, mynum, mytid, nodtot;
-    long nodeid[MAXNOD+1], nfmat, mtottk;
-    long parher, slave, timing, debug, pario;
+    long iprpar, ntask, ncode, ndegdi, master, mynum, mytid;
+    long nodtot, nodeid[MAXNOD+1], nfmat, mtottk, parher, debug, pario;
+    long timing, slave;
 #else
-    int  iprpar, ntask, ncode, ndegdi, master, mynum, mytid, nodtot;
-    int  nodeid[MAXNOD+1], nfmat, mtottk;
-    int  parher, slave, timing, debug, pario;
+    int  iprpar, ntask, ncode, ndegdi, master, mynum, mytid;
+    int  nodtot, nodeid[MAXNOD+1], nfmat, mtottk, parher, debug, pario;
+    int  timing, slave;
 #endif
     char nodnam[MAXNOD][20], myname[20];
 } daltoninfpar_;
@@ -32,27 +32,27 @@ C File: infpar.h for Dalton; special information for parallel calculations
 C
 C     Parameters NPARI must be updated after changes (for parallelization)
 C
-C     NOTE: Integers  (MASTER,...)
-C           Logicals  (TIMING,...)
-C           Character (NODNAM,...) should NOT be sent to slaves
+C     NOTE: Integers  (IPRPAR,...,MASTER,...,MYTID)
+C           Logicals  (TIMING,SLAVE)
+C           Character (NODNAM,MYNAME) should NOT be sent to slaves
+C     THUS: NPARI is length from NODTOT,...,PARIO
 C
       INTEGER MAXNOD, MAXCL2
       PARAMETER (MAXNOD = 200, MAXCL2 = 10000)
-      PARAMETER (MAXTSK=MXSHEL*(MXSHEL+1)/2, NPARI = (MAXNOD + 1) + 7)
-      INTEGER IPRPAR, NTASK, NCODE, NDEGDI, MASTER, MYNUM, MYTID, NODTOT
-      INTEGER NODEID(0:MAXNOD), NFMAT, MTOTTK
-      LOGICAL PARHER, SLAVE, TIMING, DEBUG, PARIO
+      PARAMETER (MAXTSK=MXSHEL*(MXSHEL+1)/2, NPARI = (MAXNOD + 1) + 6)
+      INTEGER IPRPAR, NTASK, NCODE, NDEGDI, MASTER, MYNUM, MYTID
+      INTEGER NODTOT, NODEID(0:MAXNOD), NFMAT, MTOTTK
+      LOGICAL PARHER, PARIO, DEBUG,     TIMING, SLAVE
       CHARACTER*20   NODNAM(0:MAXNOD), MYNAME
       COMMON /DALTONINFPAR/                                              &
-     &        IPRPAR, NTASK, NCODE, NDEGDI, MASTER, MYNUM, MYTID, NODTOT &
-     &       ,NODEID, NFMAT, MTOTTK                                      &
-     &       ,PARHER, SLAVE, TIMING, DEBUG, PARIO                        &
-     &       ,NODNAM, MYNAME
+     &        IPRPAR, NTASK, NCODE, NDEGDI, MASTER, MYNUM, MYTID         &
+     &       ,NODTOT, NODEID, NFMAT, MTOTTK, PARHER, DEBUG, PARIO        &
+     &       ,TIMING, SLAVE , NODNAM, MYNAME
 
-#if defined (INT_STAR8)
+#if defined (VAR_INT64)
 !     integer array ISTAT contains MPI_SOURCE information.
 !     Proper use of ISTAT on 64-bit machines in 
-!     combination with INT_STAR8 requires explicit declaration 
+!     combination with VAR_INT64 requires explicit declaration 
 !     as INTEGER*4 /March-2007 sk 
       INTEGER*4 ISTAT
 #endif
