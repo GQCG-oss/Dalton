8e67bdf3f498a88f4e7efeb500b198e45a10fc51
Hans Jørgen Aa. Jensen <hjj@ifk.sdu.dk>
2007-12-09 19:54:31 +0000

    hjaaj: explicitly define variables

--- include/dftinf.h
+++ include/dftinf.h
@@ -1,2 +1,10 @@
-      COMMON /DFTINF/ NDER, NTYPSO, NSO0, NSO1, NSO2, NSOB, NSOB1,
-     &                KSO0, KSO1, KSO2, KSOB, KSOB1
+C --
+C FILE: dftinf.h
+C
+C Purpose: information for DFT SOS matrices from GETSOS
+C
+      INTEGER         NDER, NTYPSO, KSO0, KSO1, KSO2,
+     &                KSOB, KSOB1,  NSOB, NSOB1
+      COMMON /DFTINF/ NDER, NTYPSO, KSO0, KSO1, KSO2,
+     &                KSOB, KSOB1,  NSOB, NSOB1
+C -- end of dftinf.h --
