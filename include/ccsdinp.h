8dfc4859f77bc0c31dc5e497a66ac46c109732d6
Ove Christiansen <ove@chem.au.dk>
2008-04-25 14:09:41 +0000

    Bug fixed for CCSD(T) gradient, moving MCCSD(T) to a separat keyword.

--- include/ccsdinp.h
+++ include/ccsdinp.h
@@ -13,7 +13,7 @@
       LOGICAL CCS,  CIS,  MP2,  CC2,   CC1A,  CC1B,
      &        MCC2, CCP2, CCD,  CCSD,  CC3,   CCPT,
      &        CCP3, CCRT, CCR3, CCR1A, CCR1B, CCT,
-     &        CCSDT,CCR12
+     &        CCSDT,CCR12, MTRIP
 
       COMMON /CCSDGNINP/ SKIP,   DIRECT, DIRGRD, CCRSTR,
      &                   FROIMP, FROEXP, NOCCIT,
@@ -28,5 +28,5 @@
       COMMON /CCMODELS/ CCS,  CIS,  MP2,  CC2,   CC1A,  CC1B,
      &                  MCC2, CCP2, CCD,  CCSD,  CC3,   CCPT,
      &                  CCP3, CCRT, CCR3, CCR1A, CCR1B, CCT,
-     &                  CCSDT,CCR12
+     &                  CCSDT,CCR12,MTRIP
 
