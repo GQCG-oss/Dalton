4a876aaa808f19024d8fc269062b3334a2050f93
Hans Jørgen Aa. Jensen <hjj@ifk.sdu.dk>
2009-10-13 15:23:27 +0000

    hjaaj: renamed some logicals, new SRINTS_SRMP2

--- include/infmp2.h
+++ include/infmp2.h
@@ -1,4 +1,4 @@
-C  -- infmp2.h -- Do not change without checking with Hans Joergen Aa. Jensen  /l.r. Sep 2007, hjaaj/
+C  -- infmp2.h -- Do not change without checking with Hans Joergen Aa. Jensen  /l.r. Oct 2009, hjaaj/
 C
 C     Information for ".MP2" (sirius/sirmp2.F) and ".SOPPA" in RESPONS (rsp/rspsoppa.F)
 C     Also used for ".SOPPA" in abacus/abadrv.F.
@@ -6,8 +6,10 @@ C
 C     Off-set to (bj) block of kappa(ai,bj) is IADR1(B,J), which is calculated when needed.
 C
       PARAMETER (MAXVIR = MXCORB)
-      LOGICAL MP2FRO, SC_SRMP2, SOSMP2, SCSMP2, TDAMP2, MP2_NO_OV
+      LOGICAL MP2FRO, SOS_MP2, SCS_MP2, TDA_MP2, MP2_NO_OV,
+     &        SC_SRMP2, SRINTS_SRMP2
       COMMON /INFMP2/ IPRMP2, NFRMP2(8), NPHSYM(8), NPHMAX, NP, NH,
      &        IPHORD(MXCORB), INDXPH(MXCORB), IFRMP2(MXCORB), NPHTOT(8),
-     &        MP2FRO, SC_SRMP2, SOSMP2, SCSMP2, TDAMP2, MP2_NO_OV
+     &        MP2FRO, SOS_MP2, SCS_MP2, TDA_MP2, MP2_NO_OV,
+     &        SC_SRMP2, SRINTS_SRMP2
 C  -- end of infmp2.h --
