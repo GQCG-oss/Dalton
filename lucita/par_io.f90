132e3cf2ee8db886af0175142e9153f8f05d8a0d
Stefan Knecht <knecht@ifk.sdu.dk>
2010-11-04 22:58:02 +0000

    add a template for module that will contain parallel i/o functionality.

---
+++ lucita/par_io.f90
@@ -0,0 +1,22 @@
+!dalton_copyright_start
+!
+!
+!dalton_copyright_end
+
+module par_mcci_io
+
+! stefan: - this module provides all necessary functionality
+!           to perform disk i/o operations in parallel mcscf/ci 
+!           calculations.
+!           to a two-component framework
+!
+!           written by sknecht, may 2007 for DIRAC MCSCF/KR-CI/LUCITA
+!           adapted for DALTON by sknecht, november 2010.
+contains 
+
+   subroutine dummy_pario
+   end subroutine dummy_pario
+
+!******************************************************************************
+
+end module
