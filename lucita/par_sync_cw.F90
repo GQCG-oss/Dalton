b785732ca96a0fa7b928e1bb73ffe9c721d1f904
Stefan Knecht <knecht@ifk.sdu.dk>
2010-12-13 20:58:25 +0000

    start working on master - coworkers synchronization.

--- lucita/par_sync_cw.F90
+++ lucita/par_sync_cw.F90
@@ -13,6 +13,7 @@ module sync_coworkers
 !           written by sknecht for DALTON, december 2010.
   use mpi
   use dalton_mpi
+  use lucita_cfg
 
 #if defined (VAR_INT64)
 #define my_MPI_INTEGER MPI_INTEGER8
@@ -22,12 +23,20 @@ module sync_coworkers
 
   implicit none
 
+  public sync_coworkers_cfg
+
   private
 
   save
 
-  integer, private                       :: istat(MPI_STATUS_SIZE)
-  integer, private                       :: ierr
+  integer, private :: istat(MPI_STATUS_SIZE)
+  integer, private :: ierr
+
+  logical, public  :: sync_cw_ci_cfg = .true.
+  logical, public  :: sync_cw_mc_cfg = .true.
+  logical, public  :: sync_cw_abcd   = .true.
+  logical, public  :: sync_cw_cvec   = .true.
+  logical, public  :: sync_cw_refvec = .true.
 
 contains 
  
@@ -38,16 +47,35 @@ contains
 !
 !*******************************************************************************
 !-------------------------------------------------------------------------------
+!     case select
   end subroutine sync_coworkers_cfg
 
-  subroutine sync_coworkers_ijkl
+  subroutine sync_coworkers_ci_cfg
+!*******************************************************************************
+!
+!    purpose:  provide co-workers with basic common block/orbital space
+!              knowledge in parallel CI/MCSCF runs.
+!
+!*******************************************************************************
+!-------------------------------------------------------------------------------
+
+!     real(8)
+      call dalton_mpi_bcast(lucita_cfg_accepted_truncation, 0, mpi_comm_world)
+!     logical
+      call dalton_mpi_bcast(lucita_cfg_ras1_set,            0, mpi_comm_world)
+!     integer
+      call dalton_mpi_bcast(ngsh_lucita,                    0, mpi_comm_world)
+
+  end subroutine sync_coworkers_ci_cfg
+!******************************************************************************
+  subroutine sync_coworkers_abcd
 !******************************************************************************
 !
 !    purpose:  provide co-workers with 1-/2-electron integrals in parallel 
 !              CI/MCSCF runs.
 !
 !******************************************************************************
-  end subroutine sync_coworkers_ijkl
+  end subroutine sync_coworkers_abcd
 !******************************************************************************
   
 #else 
