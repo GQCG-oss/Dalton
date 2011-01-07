98738008aca7669cc70b0737d827498840fcbbf1
Bin Gao <bin.gao@uit.no>
2011-01-07 13:02:19 +0000

    implemented different ECPs (when ATOMBASIS is specified) for SIRIUS module (not so sure RSP module)

--- include/gen1int.h
+++ include/gen1int.h
@@ -1,13 +1,13 @@
 C Common block of Gen1Int
 C added by Bin Gao, Dec. 5, 2010
-C     if running Gen1Int
+C...  if running Gen1Int
       logical RUN_GENINT
-C     maximum number of AO-blocks
+C...  maximum number of AO-blocks
       integer MAX_NBLOCK
       parameter (MAX_NBLOCK=200)
-C     number of AO-blocks
+C...  number of AO-blocks
       integer NUM_BLOCK
-C     start index of shell in AO-blocks
+C...  start index of shell in AO-blocks
       integer IDX_SHELL(MAX_NBLOCK)
-C
+C...
       common /GENINT_INFO/ RUN_GENINT, NUM_BLOCK, IDX_SHELL
