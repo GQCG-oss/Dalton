29e0f335b962ea31f1881149c3b23e0437884b44
Hans Jørgen Aa. Jensen <hjj@ifk.sdu.dk>
2011-01-12 19:13:19 +0000

    a simpler sdpre.h

--- include/sdpre.h
+++ include/sdpre.h
@@ -1,5 +1,3 @@
-#if defined (SYS_CRAY) || defined (SYS_T3D) || defined (VAR_REAL) || defined (SYS_T90)
-      SDPRE(I) = FLOAT(I)
-#else
-      SDPRE(I) = DFLOAT(I)
-#endif
+
+      SDPRE(I) = (1.0D0*I)
+
