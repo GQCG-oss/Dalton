f2c690cc277aa4bdf78c3e90a2a5a6ef25bfec09
Bin Gao <bin.gao@uit.no>
2011-02-17 00:07:56 +0000

    save work to reduce the interface to two files and improve the efficiency

--- include/gen1int.h
+++ include/gen1int.h
@@ -1,13 +1,12 @@
-C Common block of Gen1Int
 C added by Bin Gao, Dec. 5, 2010
-C...  if running Gen1Int
-      logical RUN_GENINT
 C...  maximum number of AO-blocks
+C...  here, AO-blocks means the orbitals with the same exponents
+C...  (contraction coefficients may be different)
       integer MAX_NBLOCK
       parameter (MAX_NBLOCK=200)
 C...  number of AO-blocks
       integer NUM_BLOCK
 C...  start index of shell in AO-blocks
       integer IDX_SHELL(MAX_NBLOCK)
-C...
-      common /GENINT_INFO/ RUN_GENINT, NUM_BLOCK, IDX_SHELL
+C...  common block used by gen1int_wrapper.F
+      common /GENINT_INFO/ NUM_BLOCK, IDX_SHELL
