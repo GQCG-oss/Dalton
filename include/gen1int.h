395d86eb242c25ec10dcfad7fb4ddc45b4de13c1
Bin Gao <bin.gao@uit.no>
2011-02-24 23:41:42 +0000

    adds routines of SO basis for Gen1Int (not finished yet)

--- include/gen1int.h
+++ include/gen1int.h
@@ -8,5 +8,9 @@ C...  number of AO-blocks
       integer NUM_BLOCK
 C...  start index of shell in AO-blocks
       integer IDX_SHELL(MAX_NBLOCK)
+C...  symmetry integral pointers for AO-blocks
+      integer INDFA(8,MXAQN,MXCONT), INDFB(8,MXAQN,MXCONT),
+     &        ISOFRA(8), ISOFRB(8)
 C...  common block used by gen1int_wrapper.F
-      common /GENINT_INFO/ NUM_BLOCK, IDX_SHELL
+      common /GENINT_INFO/ NUM_BLOCK, IDX_SHELL, INDFA, INDFB,
+     &                     ISOFRA, ISOFRB
