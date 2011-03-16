2dacd1ba4d8b3d6c0fab0d6e3847a3fd15ae47ef
Hans Jørgen Aa. Jensen <hjj@ifk.sdu.dk>
2011-03-16 11:36:46 +0000

    Last - I hope - major revision before the Dalton2011 release.
    The major change is that now the Mulliken population analysis works
    again (for HF and DFT) - this has been on my TODO list for more than 10
    years, so it was about time. Subroutine MULLIK in sirpop.F has been
    completely rewritten.
    
    -> aosotr.h, herrdn.F, sirpop.F: new IAOINFO(1:NUCDEP,1:3) with
    information for each center, p.t. only used in popana
    -> herrdn.F: renamed a NATOMS to NCENTS, the "real" definition
       NATOMS =NUCDEP -NFLOATS was overwritten!!!
    -> redefined JAOAO(*,2), see herrdn.F and abamolden.F
    -> removed SYS_CRAY for REAL, instead use REAL*8
       (problem was that DOUBLE PRECISION was 128 bits on CRAY)
    -> MBAS1T, MBAS2T in r12int.h, MBAS1T used several places
    -> dalton.F: changed release from Feb. 2011 to Mar. 2011
    -> polish here and there ...

--- include/nuclei.h
+++ include/nuclei.h
@@ -1,25 +1,26 @@
+C
+C     FILE: nuclei.h
+C
+      REAL*8  CHARGE, CORD, GNUEXP
       INTEGER NUCPRE, NUCNUM, NUCDEG, ISTBNU, NCTOT,                    &
      &        NUCIND, NUCDEP, NTRACO, ITRACO, NATOMS, NFLOAT,           &
      &        NBASIS, NLARGE, NSMALL, NPBAS,  NPLRG,  NPSML,            &
-     &        NCHTOT, INCENT, INUNIQ, NDEGNM, ISOTOP, IZATOM,           &
-     &        NBASISAUX, NPBASAUX, NAUX, NPAUX
-#if defined (SYS_CRAY)
-      REAL    CHARGE, CORD, GNUEXP
-#else
-      DOUBLE PRECISION    CHARGE, CORD, GNUEXP
-#endif
-      LOGICAL NOORBT,GAUNUC
+     &        NCHTOT, INCENT, INUNIQ, NDEGNM, ISOTOP, IZATOM,
+     &        NBASISAUX, NPBASAUX,    NAUX,   NPAUX
+      INTEGER MULBSI
+C     MULBSI has been added for multiple basis sets (WK/UniKA/31-10-2002).
+      LOGICAL NOORBT, GAUNUC
       COMMON /NUCLEI/ CHARGE(MXCENT), CORD(3,MXCENT), GNUEXP(MXCENT),   &
-     &                GAUNUC, NOORBT(MXCENT), NUCPRE(MXCENT),           &
-     &                NUCNUM(MXCENT,8), NUCDEG(MXCENT), ISTBNU(MXCENT), &
-     &                NDEGNM(MXCENT), NUCIND, NUCDEP, NTRACO, ITRACO(3),&
+     &                NUCPRE(MXCENT), NUCNUM(MXCENT,8), NUCDEG(MXCENT), &
+     &                ISTBNU(MXCENT), NDEGNM(MXCENT), NUCIND, NUCDEP,   &
+     &                NTRACO, ITRACO(3),                                &
      &                NATOMS, NFLOAT, NBASIS, NLARGE, NSMALL, NPBAS,    &
      &                NPLRG, NPSML, NCHTOT, INCENT(MXCENT), NCTOT,      &
      &                INUNIQ(MXCENT), ISOTOP(MXCENT),IZATOM(MXCENT),    &
-     &                NBASISAUX, NPBASAUX, NAUX, NPAUX
-      CHARACTER NAMEX*6, NAMDEP*6, NAMDPX*8, NAMN*4
-      COMMON /NUCLEC/ NAMEX(MXCOOR), NAMDEP(MXCENT), NAMDPX(MXCOOR),    &
-     &                NAMN(MXCENT)
-      INTEGER MULBSI
-      COMMON /MULBAS/ MULBSI(MXCENT)
-!     MULBAS has been added for multiple basis sets (WK/UniKA/31-10-2002).
+     &                NBASISAUX, NPBASAUX, NAUX, NPAUX,                 &
+     &                MULBSI(MXCENT),                                   &
+     &                GAUNUC, NOORBT(MXCENT)
+      CHARACTER       NAMN*4, NAMEX*6, NAMDEP*6, NAMDPX*8
+      COMMON /NUCLEC/ NAMN(MXCENT),   NAMEX(MXCOOR),                    &
+     &                NAMDEP(MXCENT), NAMDPX(MXCOOR)
+C  -- end of nuclei.h --
