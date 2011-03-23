409c5d1947079e3ae62b6bda6380bdf850f61e95
Arnfinn H. Steindal <arnfinn.steindal@uit.no>
2011-03-23 11:56:17 +0000

    added missing file

---
+++ include/mmtimes.h
@@ -0,0 +1,6 @@
+Cahs: timing of qm/mm routines:
+      COMMON / MMTIMES / TMMSIR, TMMFCK, TMMMULPOL, TMMPOL,
+     *                   TMMGID2, TMMPOL2, TMMF2, TMMITER,
+     *                   TMMRSP, TMMLNO, TMMQRO, TMMCRO,
+     *                   TF2QMMM, TMMFIRST, TMMB2
+
