e961398805f27fb861dc37ffa65d55118f34edaa
Arnfinn H. Steindal <arnfinn.steindal@uit.no>
2011-04-12 13:43:24 +0000

    timer for f2qmmm when called in response now working

--- include/mmtimes.h
+++ include/mmtimes.h
@@ -1,4 +1,10 @@
 Cahs: timing of qm/mm routines:
+
+      DOUBLE PRECISION TMMSIR, TMMFCK, TMMMULPOL, TMMPOL,
+     *                 TMMGID2, TMMPOL2, TMMF2, TMMITER,
+     *                 TMMRSP, TMMLNO, TMMQRO, TMMCRO,
+     *                 TF2QMMM, TMMFIRST, TMMB2
+
       COMMON / MMTIMES / TMMSIR, TMMFCK, TMMMULPOL, TMMPOL,
      *                   TMMGID2, TMMPOL2, TMMF2, TMMITER,
      *                   TMMRSP, TMMLNO, TMMQRO, TMMCRO,
