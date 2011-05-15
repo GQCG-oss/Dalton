693bcf5482cd08e3043888c5ca110f8e87c0db03
Hans Jørgen Aa. Jensen <hjj@ifk.sdu.dk>
2011-05-15 19:33:07 +0000

    changed to standard order in common blocks: first all integers, then logicals

--- include/abainf.h
+++ include/abainf.h
@@ -5,10 +5,7 @@
 !     NOTE: DOSYM(NSYML) is sent to slaves in a parallel calculation and
 !           should not be moved from this common block !
 !
-CSPAS:20/3-2011: allowing for perturbations in only one irrep
-C     INTEGER NSYML, IPRDEF, NWNABA
       INTEGER NSYML, IPRDEF, NWNABA, IRVIBG
-CKeinSPASmehr
       PARAMETER (NSYML = 8)
       LOGICAL MOLGRD, MOLHES, DIPDER, POLAR,  TSTINP,
      &        VIB,    RESTAR, DOWALK, GDALL,  CCSD,
@@ -21,12 +18,9 @@ CKeinSPASmehr
      &        MCD,    HELFEY, LINCPL, ABASOP, SKIPAB,
      &        ABA_ALPHA,      EXPGRD, CTOCD,  NUMHES, DOD2DQ2,
      &        OECD,   MVEOR,  QPGRAD, SECNDM, THIRDM, VIB_G,
-CSPAS:20/3-2011: allowing for perturbations in only one irrep
-C    &        GSDIP,  GSQUAD, GSOCT,  GSDIDI, GSDIQU, GSQUQU
      &        GSDIP,  GSQUAD, GSOCT,  GSDIDI, GSDIQU, GSQUQU,
      &        VIBGIR
-CKeinSPASmehr
-      COMMON /ABAINF/ IPRDEF, NWNABA, 
+      COMMON /ABAINF/ IPRDEF, NWNABA, IRVIBG,
      &        MOLGRD, MOLHES, DIPDER, POLAR,  TSTINP,
      &        VIB,    RESTAR, DOWALK, GDALL,  CCSD,
      &        H2MO,   DOSYM,          DOLRES, DOEXCI, SHIELD,
@@ -38,9 +32,6 @@ CKeinSPASmehr
      &        MCD,    HELFEY, LINCPL, ABASOP, SKIPAB,
      &        ABA_ALPHA,      EXPGRD, CTOCD,  NUMHES, DOD2DQ2,
      &        OECD,   MVEOR,  QPGRAD, SECNDM, THIRDM, VIB_G,
-CSPAS:20/3-2011: allowing for perturbations in only one irrep
-C    &        GSDIP,  GSQUAD, GSOCT,  GSDIDI, GSDIQU, GSQUQU
      &        GSDIP,  GSQUAD, GSOCT,  GSDIDI, GSDIQU, GSQUQU,
-     &        VIBGIR, IRVIBG
-CKeinSPASmehr
+     &        VIBGIR
 ! -- end of abainf.h --
