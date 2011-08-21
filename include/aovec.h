30c6e7c9f9d3cc7f0d475ef6a795afda641c3e71
Radovan Bast <radovan.bast@uit.no>
2011-08-21 15:46:39 +0000

    make file readable in f90

--- include/aovec.h
+++ include/aovec.h
@@ -1,12 +1,12 @@
-C
-C     File: aovec.h
-C
-C     MXAOVC = maximum number of AO input blocks for a given center
-C              (in "MOLECULE.INP" input file)
-C
-C     IF you change MXAOVC you should do a "make depend"
-C     and then rebuild the program using the command "make".
-C
+!
+!     File: aovec.h
+!
+!     MXAOVC = maximum number of AO input blocks for a given center
+!              (in "MOLECULE.INP" input file)
+!
+!     IF you change MXAOVC you should do a "make depend"
+!     and then rebuild the program using the command "make".
+!
       INTEGER MXAOVC, MXCONT
       PARAMETER (MXAOVC = 25, MXCONT = 35)
-C     hjaaj: MXCONT needs to be 35 for ANO-DK3 uncontracted for DKH Hamiltonian
+!     hjaaj: MXCONT needs to be 35 for ANO-DK3 uncontracted for DKH Hamiltonian
