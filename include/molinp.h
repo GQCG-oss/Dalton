1d9af63eac9b1be01cb560eb0d064499978d349f
Hans Jørgen Aa. Jensen <hjj@ifk.sdu.dk>
2011-09-21 12:24:17 +0000

    hjaaj: Fix of problem with long basis set names reported by Frank Jensen
    The problem was caused by that only 80 char of each line in .mol file is used,
    and his input line with the long basis set name became longer than 80 char.
    What I have done:
    1) increase max line length to 100 char
    2) read 120 char, if any text beyond 100 char, then QUIT with message
    3) energy_symm was testing for my previous programming of a warning if
       if line longer than 80 char; as we now quit this test has been removed.

--- include/molinp.h
+++ include/molinp.h
@@ -1,4 +1,16 @@
-      PARAMETER (KMLINE = 2500)
-      CHARACTER*80 MLINE
+!
+! File: molinp.h
+!
+! Contains a copy of the .mol input file in MLINE(1:NMLINE).
+! This is done by RDLINE in "abacus/herrdn.F".
+! NCLINE(i) points to the line defining center no. i
+! NMLAU     points to the line in which it is defined if coordinates
+!           are in AU (bohr) or in Angstroem
+! NONTYP_QM number of QM types, found by Master in abacus/herrdn.F, used by slaves
+!           to only keep the QM centers in their setup (see abacus/herpar.F)
+!
+      PARAMETER (KMLINE = 2500, max_len_MLINE = 100)
+      CHARACTER*(max_len_MLINE) MLINE
       COMMON /MOLINC/ MLINE(KMLINE)
       COMMON /MOLINP/ NMLINE,NCLINE(MXCENT),NMLAU,NONTYP_QM
+! -- End of molinp.h --
