40b6ffa7c61a77a0242e229c50b31a384593c892
manu <fromagere@unistra.fr>
2011-11-25 16:02:32 +0100

    Manu: new file srdftlnr.h added in include. Contains NOSIM_SR and NCSIM_SR (which correspond to NOSIM and NCSIM in the linear response) that are used in sirfck for "many roots" MCSCF-srDFT linear response

---
+++ include/srdftlnr.h
@@ -0,0 +1,7 @@
+! --
+! FILE: srdftlnr.h
+!
+! Purpose: information for linear response srDFT in sirfck 
+!
+      INTEGER         NOSIM_SR, NCSIM_SR                               
+      COMMON /SRDFTLNR/ NOSIM_SR, NCSIM_SR
