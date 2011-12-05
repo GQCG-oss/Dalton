ede5bd80add53efc5df499b8318b1a826bfe72cf
Patricio F. Provasi <patricio@patricio-laptop.(none)>
2011-12-05 19:55:56 -0300

    A, B & C AOSOPPA matrices 	modified:   abacus/abadrv.F 	modified:   abacus/abaexc.F 	modified:   abacus/abarlx.F 	modified:   abacus/abasop.F 	modified:   cc/ccsd_rhs.F 	modified:   cc/ccsd_sortao.F 	modified:   include/soppinf.h
    	modified:   rsp/rspe2c.F
    	modified:   soppa/Makefile
    	modified:   soppa/ccsd_tcmepkx.F
    	modified:   soppa/dc_calc.F
    	modified:   soppa/dc_eres.F
    	modified:   soppa/dc_r1vec.F
    	modified:   soppa/dc_res_o.F
    	modified:   soppa/dc_rsplex.F
    	modified:   soppa/dc_sres.F
    	modified:   soppa/rp_anal.F
    	modified:   soppa/rp_charge.F
    	modified:   soppa/rp_check.F
    	modified:   soppa/rp_diag.F
    	modified:   soppa/rp_eres.F
    	modified:   soppa/rp_getgp.F
    	modified:   soppa/rp_incred.F
    	modified:   soppa/rp_init.F
    	modified:   soppa/rp_lrsolv.F
    	modified:   soppa/rp_newtrial.F
    	modified:   soppa/rp_optvec.F
    	modified:   soppa/rp_orth_trn.F
    	modified:   soppa/rp_redev.F
    	modified:   soppa/rp_res_o.F
    	modified:   soppa/rp_residual.F
    	modified:   soppa/rp_rsplex.F
    	modified:   soppa/rp_sres.F
    	modified:   soppa/rp_trial1.F
    	modified:   soppa/rp_trial2.F
    	modified:   soppa/rp_trmom.F
    	modified:   soppa/so_anal.F
    	modified:   soppa/so_aodens.F
    	modified:   soppa/so_bcktr.F
    	modified:   soppa/so_bextract.F
    	modified:   soppa/so_check.F
    	modified:   soppa/so_close.F
    	modified:   soppa/so_collect_tm.F
    	modified:   soppa/so_dens.F
    	modified:   soppa/so_densai1.F
    	modified:   soppa/so_densai2.F
    	modified:   soppa/so_diag.F
    	modified:   soppa/so_ediag1.F
    	modified:   soppa/so_ediag2.F
    	modified:   soppa/so_eres.F
    	modified:   soppa/so_excit1.F
    	modified:   soppa/so_excout.F
    	modified:   soppa/so_firgp.F
    	modified:   soppa/so_fndpos.F
    	modified:   soppa/so_getgp.F
    	modified:   soppa/so_getmo.F
    	modified:   soppa/so_gett2.F
    	modified:   soppa/so_incred.F
    	modified:   soppa/so_init.F
    	modified:   soppa/so_initial.F
    	modified:   soppa/so_intrp.F
    	modified:   soppa/so_lrsolv.F
    	modified:   soppa/so_memmax.F
    	modified:   soppa/so_moenergy.F
    	modified:   soppa/so_newtrial.F
    	modified:   soppa/so_onefock.F
    	modified:   soppa/so_onep.F
    	modified:   soppa/so_onepmo.F
    	modified:   soppa/so_open.F
    	modified:   soppa/so_optvec.F
    	modified:   soppa/so_ordeig.F
    	modified:   soppa/so_orth_trn.F
    	modified:   soppa/so_prpint.F
    	modified:   soppa/so_read.F
    	modified:   soppa/so_readset.F
    	modified:   soppa/so_redev.F
    	modified:   soppa/so_res_a.F
    	modified:   soppa/so_res_b.F
    	modified:   soppa/so_res_c.F
    	modified:   soppa/so_res_cb.F
    	modified:   soppa/so_res_cd.F
    	modified:   soppa/so_res_cp.F
    	modified:   soppa/so_res_fck.F
    	modified:   soppa/so_res_o.F
    	modified:   soppa/so_res_ovl.F
    	modified:   soppa/so_res_ovlr.F
    	modified:   soppa/so_res_sym.F
    	modified:   soppa/so_res_tcb.F
    	modified:   soppa/so_residual.F
    	modified:   soppa/so_ropt.F
    	modified:   soppa/so_rpprp1.F
    	modified:   soppa/so_rsplex.F
    	modified:   soppa/so_sdiag1.F
    	modified:   soppa/so_secgp.F
    	modified:   soppa/so_sigai.F
    	modified:   soppa/so_sigda.F
    	modified:   soppa/so_sort.F
    	modified:   soppa/so_sqait2.F
    	modified:   soppa/so_sres.F
    	modified:   soppa/so_t2m1.F
    	modified:   soppa/so_t2mp.F
    	modified:   soppa/so_test.F
    	modified:   soppa/so_test1.F
    	modified:   soppa/so_tfset.F
    	modified:   soppa/so_time.F
    	modified:   soppa/so_tmltr.F
    	modified:   soppa/so_tr1new.F
    	modified:   soppa/so_tr1rp.F
    	modified:   soppa/so_trial1.F
    	modified:   soppa/so_trial2.F
    	modified:   soppa/so_trmom.F
    	modified:   soppa/so_twofock.F
    	modified:   soppa/so_write.F
    	modified:   soppa/so_writeset.F
    	modified:   soppa/so_wrtve.F

--- include/cbiexc.h
+++ include/cbiexc.h
@@ -1,8 +1,12 @@
 !
 !     cbiexc.h - Control common block for abacus/abaexc.F
 !
-      LOGICAL         SKIP, CUT, DIPSTR, ROTSTR, ROTVEL, FNAC, EXCTRP,
-     &                SUMRUL, OOTV
+      LOGICAL SKIP, CUT, DIPSTR, ROTSTR, ROTVEL, FNAC, EXCTRP,
+!PFP
+     &        SUMRUL, OOTV
+!     &        SUMRUL, OOTV, DIRECT, MAGPRP
+!     &        SUMRUL, OOTV, MAGPRP
+!end-PFP
       PARAMETER       (MAXPP = 200)
       CHARACTER*8     LABAPP
       COMMON /PPLBL / LABAPP(MAXPP), LABSYM(MAXPP)
@@ -10,5 +14,9 @@
      &                NEXCIT(8), MAXITE, MXNEXI, MXRM,
      &                MXPHP, NABAPP, IPREXC, IPR1IN,
      &                SKIP, CUT, DIPSTR, ROTSTR, ROTVEL, FNAC, EXCTRP,
+!PFP
      &                SUMRUL, OOTV
+!     &                SUMRUL, OOTV, DIRECT, MAGPRP
+!     &                SUMRUL, OOTV, MAGPRP
+!end-PFP
 ! -- end of abaexc.h --
