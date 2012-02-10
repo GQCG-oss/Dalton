0d4aeefbaec858ad22edb076fe471654fb07f28b
Radovan Bast <radovan.bast@uit.no>
2012-02-10 15:25:27 +0100

    organize compiler flags in a better way

--- cmake/compilers/CFlags.cmake
+++ cmake/compilers/CFlags.cmake
@@ -1,7 +1,14 @@
+if(NOT DEFINED DEFAULT_C_COMPILER_FLAGS_SET)
+
 if(CMAKE_C_COMPILER_ID MATCHES GNU)
     set(CMAKE_C_FLAGS         "-std=c99 -DRESTRICT=restrict -DFUNDERSCORE=1")
     set(CMAKE_C_FLAGS_DEBUG   "-O0 -g")
     set(CMAKE_C_FLAGS_RELEASE "-O3 -ffast-math -funroll-loops -ftree-vectorize -Wno-unused")
+    if(ENABLE_STATIC_LINKING)
+        set(CMAKE_C_FLAGS
+            "${CMAKE_C_FLAGS} -static -fpic"
+            )
+    endif()
 endif()
 
 if(CMAKE_C_COMPILER_ID MATCHES Intel)
@@ -21,4 +28,7 @@ if(CMAKE_C_COMPILER_ID MATCHES XL)
     set(CMAKE_C_FLAGS         " ")
     set(CMAKE_C_FLAGS_DEBUG   " ")
     set(CMAKE_C_FLAGS_RELEASE " ")
+endif()
+
+save_compiler_flags(C)
 endif()
