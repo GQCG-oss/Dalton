e7810e219c08c5ed6aaa6ddf87ea1874906752a9
Jógvan Magnus Haugaard Olsen <foeroyingur@gmail.com>
2012-05-15 10:09:57 +0200

    Rename files.

--- cmake/Sources.cmake
+++ cmake/Sources.cmake
@@ -729,10 +729,10 @@ lucita/par_communication_model.F90
 lucita/par_io.F90
 lucita/par_io_fh.F90
 lucita/par_sync_cw.F90
-peqm/dalton_wrappers.F90
-peqm/precision.F90
-peqm/blas_wrappers.F90
-peqm/lapack_wrappers.F90
+peqm/pe_dalton_wrappers.F90
+peqm/pe_precision.F90
+peqm/pe_blas.F90
+peqm/pe_lapack.F90
 peqm/polarizable_embedding.F90
     )
 set(OWN_BLAS_SOURCES
