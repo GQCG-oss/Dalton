0c961785e5178501fa4d69da4cae94c81b3ff10f
Juan Ignacio Melo <jmelo@df.uba.ar>
2012-05-23 16:04:52 -0300

    added lrescinf.h

---
+++ include/lrescinf.h
@@ -0,0 +1,15 @@
+!
+!     File: lrescinf.h
+!     Purpose: Control of what to do in LRESC module
+!
+!     NOTE: 
+!
+cx jim-gesc : RNLRSC login included in abainf.h, jimprt for debugging prints
+!      INTEGER NSYML, IPRDEF, NWNABA, IRVIBG
+!      PARAMETER (NSYML = 8)
+      LOGICAL SIGMAP1S, SIGMAP1T, SIGMAD1S, SIGMAD0S, 
+     &        SIGMAP3S, SIGMAP3T 
+      COMMON /LRESCINF/ SIGMAP1S, SIGMAP1T, SIGMAD1S, SIGMAD0S,
+    &        SIGMAP3S, SIGMAP3T  
+
+! -- end of abainf.h --
