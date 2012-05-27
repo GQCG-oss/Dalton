3632031c34f5dc9bb181a95e1523f6d6bf244d96
Radovan Bast <radovan.bast@irsamc.ups-tlse.fr>
2012-05-27 17:34:32 +0200

    sync compiler cmake code with DIRAC

--- cmake/compilers/ConfigCompilerFlags.cmake
+++ cmake/compilers/ConfigCompilerFlags.cmake
@@ -1,13 +1,13 @@
 include(SaveCompilerFlags)
 
-if (CMAKE_C_COMPILER_WORKS)
+if(CMAKE_C_COMPILER_WORKS)
     include(CFlags)
 endif()
 
-if (CMAKE_CXX_COMPILER_WORKS)
+if(CMAKE_CXX_COMPILER_WORKS)
     include(CXXFlags)
 endif()
 
-if (CMAKE_Fortran_COMPILER_WORKS)
+if(CMAKE_Fortran_COMPILER_WORKS)
     include(FortranFlags)
 endif()
