b19ab6ba16dc631ddf58df19f2ec52f728e9e120
Juan Ignacio Melo <jmelo@df.uba.ar>
2012-05-31 09:37:24 -0300

    New label on Integrals section added ' LRINTS' to set up corresponding integrals to be done on LRESC module.

--- include/lrescinf.h
+++ include/lrescinf.h
@@ -8,10 +8,10 @@ cx jim-gesc : RNLRSC login included in abainf.h, jimprt for debugging prints
       INTEGER NATOM, JIMPRT
 !     PARAMETER (NSYMLasc = 8)
       REAL*8 LRGAUG(3)
-      LOGICAL SIGMAP1S, SIGMAP1T, SIGMAD1S, SIGMAD0S,                   &
-     &        SIGMAP3S, SIGMAP3T, LRESCALL,GAUCHANG
-      COMMON /LRESCINF/ SIGMAP1S, SIGMAP1T, SIGMAD1S, SIGMAD0S,         &
-     &        GAUCHANG, SIGMAP3S, SIGMAP3T, JIMPRT, NATOM, LRGAUG,      &
+      LOGICAL SIGMAP1S, SIGMAP1T, SIGMAD1S, SIGMAD0S,                   
+     &        SIGMAP3S, SIGMAP3T, LRESCALL, GAUCHANG
+      COMMON /LRESCINF/ SIGMAP1S, SIGMAP1T, SIGMAD1S, SIGMAD0S,         
+     &        GAUCHANG, SIGMAP3S, SIGMAP3T, JIMPRT, NATOM, LRGAUG,      
      &        LRESCALL
 
 ! -- end of abainf.h --
