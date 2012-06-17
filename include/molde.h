165b790a6d4f71734fb98163dd646bc85381c83c
Hans Joergen Aagaard Jensen <hjj@ifk.sdu.dk>
2012-06-17 19:19:14 +0200

    Fix for order of AO's to Molden for d,f,g orbitals
    Thanks to Jeppe for discovering that the orbital components were not always
    in correct order for Molden input.

--- include/molde.h
+++ include/molde.h
@@ -1,3 +1,8 @@
+! FILE: include/molde.h
+!
+! Information related to writing input file to Molden program
+!
       LOGICAL MOLDEN,DONEIT,DONEIU,DONEIV,DONEIW,DONEIX
-      COMMON /MOLDEN/ OREN(MXCORB),OROC(MXCORB),EMCEN(MXCORB),
+      COMMON /CBMOLDEN/ OREN(MXCORB),OROC(MXCORB),EMCEN(MXCORB),
      &          IXYZPRINTS,MOLDEN,DONEIT,DONEIU,DONEIV,DONEIW,DONEIX
+! --- end of molde.h --
