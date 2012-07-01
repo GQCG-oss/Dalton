b25fe31774ad76aa58f5c3a05e01cdc4e0dd75a7
Radovan Bast <radovan.bast@irsamc.ups-tlse.fr>
2012-07-01 16:48:18 +0200

    convert more common blocks to f90

--- include/cbilnr.h
+++ include/cbilnr.h
@@ -8,6 +8,8 @@
       PARAMETER       (MXFR  = 101, MAXLN = 80)
       CHARACTER*8     LABALN
       COMMON /LNLBL / LABALN(MAXLN)
+      real(8)         THCLNR, FRVAL
+      integer         NFRVAL, IPRLNR, NABALN
       COMMON /CBILNR/ THCLNR, FRVAL(MXFR), NFRVAL, IPRLNR, NABALN,
      &                ALFA, ROAA, ROAG, STATIC
 ! -- end of cbilnr.h --
