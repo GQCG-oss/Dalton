35252edfbe49bc4bd6f3416b2b35cc5fee32ab9e
Radovan Bast <radovan.bast@irsamc.ups-tlse.fr>
2012-07-01 15:21:31 +0200

    add f90 continuation markers

--- include/cbione.h
+++ include/cbione.h
@@ -1,8 +1,8 @@
 ! File : cbione.h
       integer         IPRINT, MAXDIF, IDATOM, IDCOOR
-      LOGICAL         SKIP, DIFINT, NODC, NODV, DIFDIP, CUT, HFONLY,
+      LOGICAL         SKIP, DIFINT, NODC, NODV, DIFDIP, CUT, HFONLY,    &
      &                NCLONE, DIFQDP
-      COMMON /CBIONE/ IPRINT, SKIP, MAXDIF, IDATOM,
-     &                IDCOOR, DIFINT, NODC, NODV, DIFDIP, CUT, HFONLY,
+      COMMON /CBIONE/ IPRINT, SKIP, MAXDIF, IDATOM,                     &
+     &                IDCOOR, DIFINT, NODC, NODV, DIFDIP, CUT, HFONLY,  &
      &                NCLONE, DIFQDP
 ! -- end of cbione.h --
