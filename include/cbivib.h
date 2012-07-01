028afb9f5271d5249208b7a7916954111d7faf0e
Radovan Bast <radovan.bast@irsamc.ups-tlse.fr>
2012-07-01 16:56:25 +0200

    define integers in headers

--- include/cbivib.h
+++ include/cbivib.h
@@ -2,7 +2,8 @@
       PARAMETER (MAXSUB = 20)
       LOGICAL DIPOL, DOVCD, HESFIL, HESPUN, ROACID
       real(8)         SCALE, COEF
-      integer         NCARD,  NINTCM, IPRINT, MAXDIF, NISOTP, iatoms
+      integer         NCARD,  NINTCM, IPRINT, MAXDIF, NISOTP,           &
+     &                iatoms, isotp
       COMMON /CBIVIB/ SCALE(MXCOOR), COEF(MXCOOR), IATOMS(4,MXCOOR),    &
      &                NCARD,  NINTCM, DIPOL,  IPRINT, MAXDIF, NISOTP,   &
      &                ISOTP(MAXSUB,MXCENT), DOVCD, HESFIL,              &
