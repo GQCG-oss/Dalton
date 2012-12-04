8c5761567afd846fff0ad48f8565c0b59bb06c98
manu <fromagere@unistra.fr>
2012-12-04 18:11:59 +0100

    Manu + hjj: added ESRDFT in the common block (contains the srDFT xc energy)

--- include/dfterg.h
+++ include/dfterg.h
@@ -1,6 +1,9 @@
 C File: dfterg.h
 C
-C     ESRDFTY : effective energy correction for SR-DFT,
-C               is calculated in SIRFCK.
-      COMMON /DFTERG/ EDFTX, EDFTC, EDFTY, EDFTK, ESRDFTY,
+C     ESRDFTY : effective energy correction for SR-DFT
+C     ESRDFT  : srDFT total exchange and correlation energy
+C               (both ESRDFTY and ESRDFT are calculated in SIRFCK)
+C
+      COMMON /DFTERG/ EDFTX, EDFTC, EDFTY, EDFTK, ESRDFTY, ESRDFT,
      &                WDFTX, WDFTC, WDFTL, WDFTB, WDFTK
+C -- end of dfterg.h --
