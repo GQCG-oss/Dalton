GQCG-oss/Dalton

commit d421f901399ffbfc5810147f1304d37e0b8b6d75

hjaaj: more intelligent .MOSTART NEWORB for SCF
Had disabled reading SCF occupation of symmetries in 2008, because you may start
from orbitals from a calculation with another total charge. This meant that
H1DIAG guess was used for initial occupation of symmetries. Now instead, we
check if number of electrons has changed, and if not we use the old occupation.

sirius/sirinp.F
@@ -1154,7 +1154,7 @@ C        ... read old orbitals from file
          NERROR = 0
          NDELT = 0
          IF (IRDMO .EQ. 7 .OR. IRDMO .EQ. 9) THEN
-C           ... read initial MO's from LUIT1
+C           ... read initial MO's from SIRIUS.RST
 C               Find out if any orbitals deleted
             CALL GPOPEN(LUIT1,'SIRIUS.RST','OLD',' ','UNFORMATTED',
      &                  IDUMMY,.FALSE.)
@@ -1162,13 +1162,26 @@ C               Find out if any orbitals deleted
             IF (FNDLAB('BASINFO ',LUIT1)) THEN
 C           ... 921202: old LUIT1 files do not have BASINFO
 C
-               IF (INIOCC .EQ. 0 .OR. IREST .GT. 0) THEN
-C              ... 081205 hjaaj: not for INIOCC .lt. 0, because we
-C                  may restart with old orbitals, but new charge.
+               IF (INIOCC .LE. 0 .OR. IREST .GT. 0) THEN
                   READ (LUIT1) NSYM_i4,NBAS_i4,NORB_i4,NRHF_i4,IOPRHF_i4
-                  INIOCC  = 2
-                  NRHF(:) = NRHF_i4(:)
-                  IOPRHF  = IOPRHF_i4
+                  IF (INIOCC .LT. 0) THEN
+C                 ... 081205 hjaaj: for INIOCC .lt. 0, we
+C                     may restart with old orbitals, but new charge.
+                     NRHFEL_test = 0
+                     DO I = 1, NSYM_i4
+                        NRHFEL_test = NRHFEL_test + NRHF_i4(I)
+                     END DO
+                     IF (IOPRHF_i4 .GT. 0) NRHFEL_test = NRHFEL_test + 1
+                     IF (NRHFEL_test .eq. NRHFEL) THEN
+                        INIOCC  = 2
+                        NRHF(:) = NRHF_i4(:)
+                        IOPRHF  = IOPRHF_i4
+                     END IF
+                  ELSE
+                     INIOCC  = 2
+                     NRHF(:) = NRHF_i4(:)
+                     IOPRHF  = IOPRHF_i4
+                  END IF
                ELSE
                   READ (LUIT1) NSYM_i4,NBAS_i4,NORB_i4
                END IF

commit 0665a6f5d96e567f22847099607d5b15c0133863

Fix SCF restart bug
Electron couting error with shows up in
SCF restart without input occupation
Introduced in commit d421f90

New testcase energy_restart_scf
(based on energy_restart)

/OV

DALTON/sirius/sirinp.F
@@ -1169,7 +1169,7 @@ C                 ... 081205 hjaaj: for INIOCC .lt. 0, we
 C                     may restart with old orbitals, but new charge.
                      NRHFEL_test = 0
                      DO I = 1, NSYM_i4
-                        NRHFEL_test = NRHFEL_test + NRHF_i4(I)
+                        NRHFEL_test = NRHFEL_test + 2*NRHF_i4(I)
                      END DO
                      IF (IOPRHF_i4 .GT. 0) NRHFEL_test = NRHFEL_test + 1
                      IF (NRHFEL_test .eq. NRHFEL) THEN

DALTON/test/TEST
@@ -49,7 +49,7 @@ ESSENTIAL=""
 #
 GEN1INT="gen1int_fluorobenzene_cart gen1int_water_cart gen1int_fluorobenzene_spher \
 gen1int_water_spher"
-SHORT_ENERGY="energy_direct energy_direct_eri energy_douglaskroll energy_nosymm energy_restart \
+SHORT_ENERGY="energy_direct energy_direct_eri energy_douglaskroll energy_nosymm energy_restart energy_restart_scf \
 energy_symm energy_zmat energy_nevpt2_gs energy_nevpt2_ex energy_hsrohf energy_hsrohf_qc \
 energy_lucita_gasci"
 SHORTDFT="dft_hcth120 dft_camb3lyp dft_pbe dft_open_b3lyp dft_qr_sym dft_rspexci_sym dft_rspexci_nosym \

DALTON/test/energy_restart_scf
@@ -1,0 +1,218 @@
+#!/bin/sh
+#
+# This is the script for generating files for a specific Dalton test job.
+#
+# For the .check file ksh or bash is preferred, otherwise use sh
+# (and hope it is not the old Bourne shell, which will not work)
+#
+if [ -x /bin/ksh ]; then
+   CHECK_SHELL='#!/bin/ksh'
+elif [ -x /bin/bash ]; then
+   CHECK_SHELL='#!/bin/bash'
+else
+   CHECK_SHELL='#!/bin/sh'
+fi
+
+
+#######################################################################
+#  TEST DESCRIPTION
+#######################################################################
+cat > energy_restart_scf.info <<'%EOF%'
+   energy_restart_scf
+   --------------
+   Molecule:         H2O
+   Wave Functions:   HF / cc-pVDZ
+   Test Purpose:     Restart an HF wave function optimization
+%EOF%
+
+#######################################################################
+#  MOLECULE INPUT
+#######################################################################
+cat > energy_restart_scf.mol <<'%EOF%'
+BASIS
+cc-pVDZ
+Restarting an HF wave function optimization
+
+    2
+        8.    1
+O     0.0 -0.2249058930 0.0
+        1.    2
+H     1.45235 0.899623 0.0
+H    -1.45235 0.899623 0.0
+%EOF%
+
+#######################################################################
+#  DALTON INPUT
+#######################################################################
+cat > energy_restart_scf__1.dal <<'%EOF%'
+**DALTON INPUT
+.RUN WAVE FUNCTION
+**WAVE FUNCTIONS
+.HF
+*SCF INPUT
+#.DOUBLY OCCUPIED
+# 3 1 1 0
+.MAX DIIS
+ 5
+.NOQCSCF
+**END OF DALTON INPUT
+%EOF%
+
+cat > energy_restart_scf__2.dal <<'%EOF%'
+**DALTON INPUT
+.RUN WAVE FUNCTION
+**WAVE FUNCTIONS
+.RESTART
+.HF
+*SCF INPUT
+#.DOUBLY OCCUPIED
+# 3 1 1 0
+**END OF DALTON INPUT
+%EOF%
+#######################################################################
+
+
+
+#######################################################################
+
+#######################################################################
+#  CHECK SCRIPT
+#######################################################################
+echo $CHECK_SHELL >energy_restart_scf__1.check
+cat >>energy_restart_scf__1.check <<'%EOF%'
+log=$1
+
+if [ `uname` = Linux ]; then
+   GREP="egrep -a"
+else
+   GREP="egrep"
+fi
+
+# Basis set
+CRIT1=`$GREP "Basis set used is \"cc\-pVDZ\" from the basis set library\." $log | wc -l`
+CRIT2=`$GREP "O * 1 * 8\.0000 * 26 * 14 * \[9s4p1d\|3s2p1d\]" $log | wc -l`
+CRIT3=`$GREP "H * 2 * 1\.0000 * 7 * 5 * \[4s1p\|2s1p\]" $log | wc -l`
+CRIT4=`$GREP "total\: * 3 * 10\.0000 * 40 * 24" $log | wc -l`
+CRIT5=`$GREP "Spherical harmonic basis used\." $log | wc -l`
+TEST[1]=`expr	$CRIT1 \+ $CRIT2 \+ $CRIT3 \+ $CRIT4 \+ $CRIT5`
+CTRL[1]=5
+ERROR[1]="BASIS SET NOT READ CORRECTLY"
+
+# Geometry
+CRIT1=`$GREP "Total number of coordinates\: * 9" $log | wc -l`
+CRIT2=`$GREP "1 * x * (0| )\.0000000000" $log | wc -l`
+CRIT3=`$GREP "2 * y * (0| )\.0000000000" $log | wc -l`
+CRIT4=`$GREP "3 * z * (\-0| \-)\.1258515023" $log | wc -l`
+CRIT5=`$GREP "4 * x * (0| )\.0000000000" $log | wc -l`
+CRIT6=`$GREP "5 * y * 1\.4523500000" $log | wc -l`
+CRIT7=`$GREP "6 * z * (0| )\.9986773907" $log | wc -l`
+CRIT8=`$GREP "7 * x * (0| )\.0000000000" $log | wc -l`
+CRIT9=`$GREP "8 * y * \-1\.4523500000" $log | wc -l`
+CRIT10=`$GREP "9 * z * (0| )\.9986773907" $log | wc -l`
+TEST[2]=`expr	$CRIT1 \+ $CRIT2 \+ $CRIT3 \+ $CRIT4 \+ $CRIT5 \+ $CRIT6 \+ \
+		$CRIT7 \+ $CRIT8 \+ $CRIT9 \+ $CRIT10`
+CTRL[2]=10
+ERROR[2]="GEOMETRY NOT READ CORRECTLY"
+
+# Symmetry
+CRIT1=`$GREP "Number of coordinates in each symmetry\: * 3 * 2 * 3 * 1" $log | wc -l`
+CRIT2=`$GREP "Number of orbitals in each symmetry\: * 11 * 4 * 7 * 2" $log | wc -l`
+TEST[3]=`expr	$CRIT1 \+ $CRIT2`
+CTRL[3]=2
+ERROR[3]="SYMMETRY NOT CORRECT"
+
+# Energy
+CRIT1=`$GREP "Final HF energy\: * \-76\.025678.*NOT CONVERGED" $log | wc -l`
+TEST[4]=`expr	$CRIT1`
+CTRL[4]=1
+ERROR[4]="HF ENERGY NOT CORRECT"
+
+PASSED=1
+for i in 1 2 3 4
+do
+   if [ ${TEST[i]} -lt ${CTRL[i]} ]; then
+     echo "${ERROR[i]} ( test = ${TEST[i]}; control = ${CTRL[i]} ); "
+     PASSED=0
+   fi
+done
+
+if [ $PASSED -eq 1 ]
+then
+  echo TEST ENDED AS EXPECTED
+  exit 0
+else
+  echo THERE IS A PROBLEM
+  exit 1
+fi
+
+%EOF%
+#######################################################################
+echo $CHECK_SHELL >energy_restart_scf__2.check
+cat >>energy_restart_scf__2.check <<'%EOF%'
+log=$1
+
+if [ `uname` = Linux ]; then
+   GREP="egrep -a"
+else
+   GREP="egrep"
+fi
+
+# Basis set
+CRIT1=`$GREP "Basis set used is \"cc\-pVDZ\" from the basis set library\." $log | wc -l`
+CRIT2=`$GREP "O * 1 * 8\.0000 * 26 * 14 * \[9s4p1d\|3s2p1d\]" $log | wc -l`
+CRIT3=`$GREP "H * 2 * 1\.0000 * 7 * 5 * \[4s1p\|2s1p\]" $log | wc -l`
+CRIT4=`$GREP "total\: * 3 * 10\.0000 * 40 * 24" $log | wc -l`
+CRIT5=`$GREP "Spherical harmonic basis used\." $log | wc -l`
+TEST[1]=`expr	$CRIT1 \+ $CRIT2 \+ $CRIT3 \+ $CRIT4 \+ $CRIT5`
+CTRL[1]=5
+ERROR[1]="BASIS SET NOT READ CORRECTLY"
+
+# Geometry
+CRIT1=`$GREP "Total number of coordinates\: * 9" $log | wc -l`
+CRIT2=`$GREP "1 * x * (0| )\.0000000000" $log | wc -l`
+CRIT3=`$GREP "2 * y * (0| )\.0000000000" $log | wc -l`
+CRIT4=`$GREP "3 * z * (\-0| \-)\.1258515023" $log | wc -l`
+CRIT5=`$GREP "4 * x * (0| )\.0000000000" $log | wc -l`
+CRIT6=`$GREP "5 * y * 1\.4523500000" $log | wc -l`
+CRIT7=`$GREP "6 * z * (0| )\.9986773907" $log | wc -l`
+CRIT8=`$GREP "7 * x * (0| )\.0000000000" $log | wc -l`
+CRIT9=`$GREP "8 * y * \-1\.4523500000" $log | wc -l`
+CRIT10=`$GREP "9 * z * (0| )\.9986773907" $log | wc -l`
+TEST[2]=`expr	$CRIT1 \+ $CRIT2 \+ $CRIT3 \+ $CRIT4 \+ $CRIT5 \+ $CRIT6 \+ \
+		$CRIT7 \+ $CRIT8 \+ $CRIT9 \+ $CRIT10`
+CTRL[2]=10
+ERROR[2]="GEOMETRY NOT READ CORRECTLY"
+
+# Symmetry
+CRIT1=`$GREP "Number of coordinates in each symmetry\: * 3 * 2 * 3 * 1" $log | wc -l`
+CRIT2=`$GREP "Number of orbitals in each symmetry\: * 11 * 4 * 7 * 2" $log | wc -l`
+TEST[3]=`expr	$CRIT1 \+ $CRIT2`
+CTRL[3]=2
+ERROR[3]="SYMMETRY NOT CORRECT"
+
+# Final energy
+CRIT1=`$GREP "Final HF energy\: * \-76\.02568" $log | wc -l`
+TEST[4]=`expr	$CRIT1`
+CTRL[4]=1
+ERROR[4]="FINAL HF ENERGY NOT CORRECT"
+
+PASSED=1
+for i in 1 2 3 4 
+do
+   if [ ${TEST[i]} -lt ${CTRL[i]} ]; then
+     echo "${ERROR[i]} : ${TEST[i]} .ne. ${CTRL[i]} ; "
+     PASSED=0
+   fi
+done
+
+if [ $PASSED -eq 1 ]
+then
+  echo TEST ENDED PROPERLY
+  exit 0
+else
+  echo THERE IS A PROBLEM
+  exit 1
+fi
+
+%EOF%
+#######################################################################