4914f6c8ba777342c621740bd35ce8b7bcae633c
sonia <sonia@chem.au.dk>
2013-04-29 10:02:31 +0200

    added DRCCD (DRPA), RCCD, RTCCD, SOSEX + related in common blocks. Sonia

--- DALTON/include/ccsdinp.h
+++ DALTON/include/ccsdinp.h
@@ -3,6 +3,7 @@
      &        CCSAVE, STOLD,  JACEXP, LHTR,
      &        DEBUG,  CCSTST, ANAAOD,
      &        HERDIR, FREEZE, KEEPAOIN, NOEONL, NOSORT,
+     &        HURWITZ_CHECK,
 CSPAS 15.11.2009 implementing AO-SOPPA
 C    &        SIRSOP, LVVVV, ONLYMO
      &        SIRSOP, LVVVV, ONLYMO, AOSOPPA
@@ -11,21 +12,24 @@ CKeinSPASmehr
       LOGICAL ETACCPT, DIRKAPB
 
       INTEGER MXDIIS, MXLRV,
+     &        IT2UPD, IT2START,
      &        ICHANG, IPRINT, KEEPAOTWO
 
       LOGICAL CCS,  CIS,  MP2,  CC2,   CC1A,  CC1B,
      &        MCC2, CCP2, CCD,  CCSD,  CC3,   CCPT,
      &        CCP3, CCRT, CCR3, CCR1A, CCR1B, CCT,
+     &        rCCD, drCCD,SOSEX,rTCCD,
      &        CCSDT,CCR12, MTRIP, CHOPT
 
       COMMON /CCSDGNINP/ SKIP,   DIRECT, DIRGRD, CCRSTR,
      &                   FROIMP, FROEXP, NOCCIT,
      &                   CCSAVE, STOLD,  JACEXP, LHTR,
      &                   DEBUG,  CCSTST, ANAAOD,
-     &                   MXDIIS, MXLRV,
+     &                   MXDIIS, MXLRV,IT2UPD, IT2START,
      &                   ICHANG, IPRINT, KEEPAOTWO, HERDIR,
      &                   ETACCPT, DIRKAPB,
      &                   FREEZE, KEEPAOIN, NOEONL, NOSORT,
+     &                   HURWITZ_CHECK,
 CSPAS 15.11.2009 implementing AO-SOPPA
 C    &                   SIRSOP, LVVVV, ONLYMO
      &                   SIRSOP, LVVVV, ONLYMO, AOSOPPA
@@ -34,5 +38,6 @@ CKeinSPASmehr
       COMMON /CCMODELS/ CCS,  CIS,  MP2,  CC2,   CC1A,  CC1B,
      &                  MCC2, CCP2, CCD,  CCSD,  CC3,   CCPT,
      &                  CCP3, CCRT, CCR3, CCR1A, CCR1B, CCT,
+     &                  rCCD, drCCD,SOSEX,rTCCD,
      &                  CCSDT,CCR12,MTRIP, CHOPT
 
