b3e4da86c4b6644b42b5c91209987cc7cfcf3650
Radovan Bast <radovan.bast@irsamc.ups-tlse.fr>
2013-06-05 14:24:16 +0200

    get more info at configure time (flags and libs)

--- cmake/ConfigInfo.cmake
+++ cmake/ConfigInfo.cmake
@@ -1,19 +1,12 @@
 message("-- System: ${CMAKE_SYSTEM_NAME}")
-
-if(cmake_build_type_tolower STREQUAL "debug")
-    message("-- Fortran Compiler Flags: ${CMAKE_Fortran_FLAGS} ${CMAKE_Fortran_FLAGS_DEBUG}")
-    message("-- C Compiler Flags:       ${CMAKE_C_FLAGS} ${CMAKE_C_FLAGS_DEBUG}")
-else()
-    message("-- Fortran Compiler Flags: ${CMAKE_Fortran_FLAGS} ${CMAKE_Fortran_FLAGS_RELEASE}")
-    message("-- C Compiler Flags:       ${CMAKE_C_FLAGS} ${CMAKE_C_FLAGS_RELEASE}")
-endif()
+message("-- Fortran Compiler Flags: ${CMAKE_Fortran_FLAGS} ${CMAKE_Fortran_FLAGS_${cmake_build_type_toupper}}")
+message("-- C Compiler Flags: ${CMAKE_C_FLAGS} ${CMAKE_C_FLAGS_${cmake_build_type_toupper}}")
+message("-- Libraries: ${LIBS}")
 
 # get size of static allocations
-add_custom_target(
-    get_static_dalton
-    COMMAND ${CMAKE_SOURCE_DIR}/../cmake/binary-info/get_static_size.py dalton.x
-    )
-add_custom_target(
-    get_static_lsdalton
-    COMMAND ${CMAKE_SOURCE_DIR}/../cmake/binary-info/get_static_size.py lsdalton.x
-    )
+foreach(_binary ${STATIC_MEM_INFO_BINARIES})
+    add_custom_target(
+        static_mem_${_binary}
+            COMMAND ${CMAKE_SOURCE_DIR}/../cmake/binary-info/get_static_size.py ${_binary}.x
+        )
+endforeach()
