6848afdb2ac1eaa8bae14411ee466b0bbda2c580
Patrick Ettenhuber <pett@chem.au.dk>
2013-06-18 13:33:33 +0200

    added labels for dec testcases
    called "dec"
    
    and reverted the VAR_LSDEBUG stuff for the manual_*F90 files

--- cmake/LibsLSDALTON.cmake
+++ cmake/LibsLSDALTON.cmake
@@ -58,7 +58,7 @@ target_link_libraries(pdpacklib matrixulib)
 # automatially generate the manual_reorderdings.F90
 SET_SOURCE_FILES_PROPERTIES(${CMAKE_BINARY_DIR}/manual_reorderings.F90 PROPERTIES GENERATED 1)
 SET_SOURCE_FILES_PROPERTIES(${CMAKE_BINARY_DIR}/manual_utils.F90 PROPERTIES GENERATED 1)
-execute_process(COMMAND python ${CMAKE_SOURCE_DIR}/LSDALTON/lsutil/autogen/generate_man_reord.py nocollapse VAR_LSDEBUG CMAKE_BUILD=${CMAKE_BINARY_DIR} ${DirDefs})
+execute_process(COMMAND python ${CMAKE_SOURCE_DIR}/LSDALTON/lsutil/autogen/generate_man_reord.py nocollapse CMAKE_BUILD=${CMAKE_BINARY_DIR} ${LIST_OF_DEFINITIONS})
 
 add_library(
     lsutiltypelib_common
