129b0eec99fbd607c7f1a8342d5bfb76e7eecf85
Thomas Kjaergaard <tkjaergaardchem@gmail.com>
2013-08-01 19:55:19 +0200

    fixed issue with Fortran2003 variable. TK

--- cmake/fortran-standard/CheckFortranStandardCompatibility.cmake
+++ cmake/fortran-standard/CheckFortranStandardCompatibility.cmake
@@ -4,10 +4,10 @@ include(CheckFortranSourceCompiles)
 file(READ "${CMAKE_SOURCE_DIR}/cmake/fortran-standard/test-fortran-2003-compatibility.F90" _source)
 check_fortran_source_compiles(
     ${_source}
-    COMPILER_UNDERSTANDS_FORTRAN_2003
+    COMPILER_UNDERSTANDS_FORTRAN03
     )
 
-if(COMPILER_UNDERSTANDS_FORTRAN_2003)
+if(COMPILER_UNDERSTANDS_FORTRAN03)
     add_definitions(-DCOMPILER_UNDERSTANDS_FORTRAN_2003)
 else()
     message(FATAL_ERROR "Your compiler is not compatible with some Fortran 2003 features that we need.")
