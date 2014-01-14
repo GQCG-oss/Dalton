2d2fa3731919920965179b90fd8752ea61a1a982
Ignat Harczuk <harczuk@kth.se>
2014-01-14 16:48:42 +0000

    EX3VEC and X2VEC implemented, EX3VEC writing works at this point

--- DALTON/include/inftap.h
+++ DALTON/include/inftap.h
@@ -11,7 +11,8 @@
      &                LUINTR, LUMOLDEN,       LUMOLDEN_MOS,             &
      &                LUR12,  LUORDA, LUMINT, LUSRINT
       INTEGER         LUAHSO, LUCRV1, LUCRV2, LUXYVE, LUCRVE,           &
-     &                LURSP3, LURSP4, LURSP5, LUMHSO, LURSP
+     &                LURSP3, LURSP4, LURSP5, LUMHSO, LURSP,            &
+     &		      LUE3VEC,LUX2VEC
       INTEGER         LUINTM, LUIT1,  LUIT2,  LUIT3,  LUIT5,  LUINF,    &
      &                LUH2AC, LUSIFC, LBINTM, LBINTD, LBONEL, LBINFO
       INTEGER         LUPMOM, LUMOM,  LUEIND, LUENUC, LUESITE,LUEOBAR,  &
