c0ba39bf8f86c157e8b209a1ed9adb15a6e5a464
Hans Joergen Aagaard Jensen <hjj@sdu.dk>
2014-01-16 15:35:47 +0100

    hjaaj: fix of errors made in merge of hjaaj-srdft with master-before-rename

--- input/input_reader_sections.F90
+++ input/input_reader_sections.F90
@@ -184,10 +184,6 @@
       gasci_input_fci_dump = .true.
     end if
 
-    if (kw_matches(word, '.FCIDUM')) then
-      lucita_cfg_fci_dump = .true.
-    end if
-
     if (kw_matches(word, '.MAXITR')) then
       call kw_read(word, gasci_input_max_nr_dav_ci_iter)
     end if
