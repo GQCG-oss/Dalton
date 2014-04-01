37ec7b63e2bcb94b3533e835d46361608f472830
Frederik Beyer <beyerkjaer@gmail.com>
2014-04-01 14:31:25 +0200

    Updated common blocks for easy MPI data transfer
    Common blocks were updated for the <name>LAST scheme of transferrring
    data from master to slaves in a parallel calculation.

--- DALTON/include/ccpack.h
+++ DALTON/include/ccpack.h
@@ -9,7 +9,10 @@ C
       INTEGER IOFFINT(MXCORB)
       INTEGER NPCKINT(MXCORB)
       INTEGER NTOTINT, NTOTPCK
-
+      INTEGER CCPACKLAST
+C
+C
+C
 #if defined (SYS_CRAY)
       REAL THRPCKINT, PCKRATIO, PCKTIME
 #else
@@ -18,4 +21,14 @@ C
       COMMON /CCPACK/ THRPCKINT, PCKRATIO, PCKTIME,
      &                IPCKTABINT, IOFFINT, NPCKINT,
      &                NTOTINT, NTOTPCK, LPACKINT
-
+C
+      COMMON /CCPACK/ CCPACKLAST
+      !   Very important !!!
+      !   Always keep this variable as the last variable in the common block. 
+      !   If you add more variables to the block add them before <name>last.
+      !   This variable is used to synchronize slaves for parallel
+      !   calculations. Other than acting as a target to calculate the size of a common
+      !   block, they have no use.
+      !   Use CALL GETBYTESPAN(firstvar, <name>last, SizeInBytes) from all processes 
+      !   to get the number of bytes needed to transfer the common block.
+      !   Then transfer the block with mpi_bcast(firstvar, SizeInBytes, mpi_byte, 0, mpi_comm_world, ierr)
