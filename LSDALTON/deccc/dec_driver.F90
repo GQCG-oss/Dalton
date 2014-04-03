a10550a341fa44419bc941fbf26dc64a20314b5d
Patrick Ettenhuber <pett@chem.au.dk>
2014-04-03 11:34:56 +0200

    print if backing up
    this is not intended for the master branch, remove the print before
    merging

--- LSDALTON/deccc/dec_driver.F90
+++ LSDALTON/deccc/dec_driver.F90
@@ -862,7 +862,7 @@ subroutine print_dec_info()
        counter=counter+1
 
        if(k<=jobs%njobs) then
-          if(jobs%jobsdone(k)) cycle  ! job is already done
+          if(jobs%jobsdone(k)) cycle JobLoop ! job is already done
        end if
 
        ! *********************************************
@@ -1101,9 +1101,16 @@ subroutine print_dec_info()
           call LSTIMER('START',t2cpu,t2wall,DECinfo%output)
           dt = t2wall - t1wall
 
-          backup_files =  (((float(jobdone) < 1.0/4.0*float(jobs%njobs)) .or. &
+          backup_files =  (((float(jobdone) < 0.25*float(jobs%njobs)) .or. &
               &(dt > DECinfo%TimeBackup) .or. all(jobs%jobsdone) ) .and. & 
               & (.not. all(jobs%dofragopt))) .or. (DECinfo%only_one_frag_job) 
+
+           print *,"BACKUP",backup_files
+           print *,"crit 1:",float(jobdone) ,0.25*float(jobs%njobs)
+           print *,"crit 2:",dt , DECinfo%TimeBackup
+           print *,"crit 3:",all(jobs%jobsdone)
+           print *,"crit 4:",.not. all(jobs%dofragopt)
+           print *,"crit 5:",DECinfo%only_one_frag_job
 
           ! Backup if time passed is more than DECinfo%TimeBackup or if all jobs are done
           Backup: if( backup_files )then
