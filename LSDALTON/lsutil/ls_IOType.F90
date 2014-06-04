7683547ddfd26b1f41314d58c6ec1a7e7558d957
Simen Reine <simen.reine@kjemi.uio.no>
2014-06-04 14:49:03 +0200

    LSDALTON: reintroduced memory saving in ls_IO - important for DF/ADMM performance

--- LSDALTON/lsutil/ls_IOType.F90
+++ LSDALTON/lsutil/ls_IOType.F90
@@ -2,16 +2,32 @@
 !> Contain the IOITEM which keeps track of files written to disk
 MODULE io_type
 use precision
+use Matrix_module
 !Integer,parameter :: maxFiles = 30000
 Integer,parameter :: maxRecord = 2**28
 Integer,parameter :: increment = 50
 
+TYPE memMat
+  TYPE(matrix)              :: mat
+  Character(len=80)         :: filename
+END TYPE memMat
+
+TYPE memMatP
+  TYPE(memMat),pointer  :: p
+  TYPE(memMatP),pointer :: next
+  TYPE(memMatP),pointer :: previous
+END TYPE memMatP
+
 TYPE IOITEM
-Integer       :: nallocFiles
-Integer       :: numFiles
+Integer                   :: nallocFiles
+Integer                   :: numFiles
 Character(len=80),pointer :: filename(:)
-Integer,pointer       :: IUNIT(:)
-Logical,pointer       :: isOpen(:)
+Integer,pointer           :: IUNIT(:)
+Logical,pointer           :: isOpen(:)
+Integer                   :: nMemMat
+TYPE(memMatP),pointer     :: first
+TYPE(memMatP),pointer     :: current
+Logical                   :: saveInMem
 END TYPE IOITEM
 
 contains
