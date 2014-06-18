e696ef2e12c905847afee45d8f1c5b0ebcdc3ec4
Maciej Kaminski <mkaminski@chem.uw.edu.pl>
2014-06-18 19:36:05 +0200

    Changed .ECPHOS and .MNFPHO to use DIPVEL instead of DIPLEN. Added .CPPHEC and .CPPHMF to calculate circularly polarized phosphorescence using mean field and effective charge integrals. Added some nice interface for the purpose.
    	modified:   DALTON/include/infhso.h
    	modified:   DALTON/rsp/hypinp.F
    	modified:   DALTON/rsp/rspmai.F
    	modified:   DALTON/rsp/rspvec.F
    	new file:   DALTON/test/rsp_cpph
    	new file:   DALTON/test/trueresult/rsp_cpph.ref

--- DALTON/include/infhso.h
+++ DALTON/include/infhso.h
@@ -1,8 +1,10 @@
       LOGICAL TESTZY, DOSO1, DOSO2, X2MAT, A2MAT, X2GRAD, PHOSPH,
-     &        MNFPHO, ECPHOS, PHOSPV, CPPHOL, CPPHOV                    !MK
+     &        MNFPHO, ECPHOS, PHOSPV, CPPHOL, CPPHOV, CPPHMF, CPPHEC    !MK
       PARAMETER (MXPHOS = 10)
       COMMON /INFHSO/PHOSMAT(3,3,MXPHOS), PHOSMN(3,3,MXPHOS),
      &               PHOSEC(3,3,MXPHOS), PHOTHRD(3,3,MXPHOS),           !MK
-     &               PHOFOUR(3,3,MXPHOS),			        !MK
+     &               PHOFOUR(3,3,MXPHOS), PHOFIFT(3,3,MXPHOS),          !MK
+     &               PHOSIXT(3,3,MXPHOS),                               !MK
      &               IPRHSO, TESTZY, DOSO1, DOSO2, X2MAT, A2MAT, X2GRAD,
-     &               PHOSPH, MNFPHO, ECPHOS, PHOSPV, CPPHOL, CPPHOV     !MK
+     &               PHOSPH, MNFPHO, ECPHOS, PHOSPV, CPPHOL, CPPHOV,    !MK
+     &               CPPHMF, CPPHEC                                     !MK
