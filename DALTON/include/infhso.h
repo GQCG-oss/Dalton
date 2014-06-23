c753469e7458681971b97804ec91ec45fbe75ec4
Maciej Kaminski <mkaminski@chem.uw.edu.pl>
2014-06-23 16:28:06 +0200

    Reverted .ECPHOS and .MNFPHO to use DIPLEN by default.

--- DALTON/include/infhso.h
+++ DALTON/include/infhso.h
@@ -4,7 +4,8 @@
       COMMON /INFHSO/PHOSMAT(3,3,MXPHOS), PHOSMN(3,3,MXPHOS),
      &               PHOSEC(3,3,MXPHOS), PHOTHRD(3,3,MXPHOS),           !MK
      &               PHOFOUR(3,3,MXPHOS), PHOFIFT(3,3,MXPHOS),          !MK
-     &               PHOSIXT(3,3,MXPHOS),                               !MK
+     &               PHOSIXT(3,3,MXPHOS), PHOSMV(3,3,MXPHOS),           !MK
+     &               PHOSEV(3,3,MXPHOS),                                !MK
      &               IPRHSO, TESTZY, DOSO1, DOSO2, X2MAT, A2MAT, X2GRAD,
      &               PHOSPH, MNFPHO, ECPHOS, PHOSPV, CPPHOL, CPPHOV,    !MK
      &               CPPHMF, CPPHEC                                     !MK
