d32ad49d609b10cb2bac79abf0d407db3ff5ce32
Thomas Kjaergaard <tkjaergaardchem@gmail.com>
2014-08-07 16:01:29 +0200

    Modified the XC code. I changed the memory handling related to OpenMP.
    Before each OpenMP thread made its own version of the output
    and added to it through the XC integration.
    This avoided the need for CRITICAL statements. However for big systems the
    added memory required is not desireable. This was therefore reverted and the
    timings are basicly unaffected with the current scheme using the ATOMIC clause.
    
     TK

--- LSDALTON/lsdaltonsrc/LSDALTON.F90
+++ LSDALTON/lsdaltonsrc/LSDALTON.F90
@@ -704,6 +704,7 @@ SUBROUTINE lsinit_all(OnMaster,lupri,luerr,t1,t2)
   use init_lsdalton_mod, only: open_lsdalton_files
   use lstensorMem, only: lstmem_init
   use rsp_util, only: init_rsp_util
+  use dft_memory_handling
   use memory_handling, only: init_globalmemvar
   use lstiming, only: init_timers, lstimer,  print_timers,time_start_phase,PHASE_WORK
   use lspdm_tensor_operations_module,only:init_persistent_array
@@ -733,6 +734,7 @@ SUBROUTINE lsinit_all(OnMaster,lupri,luerr,t1,t2)
   call set_matrix_default !initialize global matrix counters
   call init_rsp_util      !initialize response util module
   call lstmem_init
+  call setPrintDFTmem(.FALSE.)
   call init_IIDF_matrix
 #ifdef VAR_ICHOR
   call InitIchorSaveGabModule()
