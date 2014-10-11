240a771c777c31c16cf6f70f9c38a023ee615ea2
Patrick Ettenhuber <pett@chem.au.dk>
2014-10-11 08:06:55 +0200

    remove printout

--- LSDALTON/deccc/fragment_energy.F90
+++ LSDALTON/deccc/fragment_energy.F90
@@ -997,14 +997,12 @@ contains
 #ifdef MOD_UNRELEASED
        endif
 #endif
-       print *,"CCSD DONE"
 
        ! Extract EOS indices for integrals 
        ! *********************************
        call array_extract_eos_indices(VOVO, PairFragment, Arr_occEOS=VOVOocc, Arr_virtEOS=VOVOvirt)
 !       call array_free(VOVO)
 
-       print *,"free first order"
 #ifdef MOD_UNRELEASED
        if(DECinfo%first_order) then
           !construct RHS for pairs
@@ -1017,8 +1015,6 @@ contains
        ! ********************************************
        ! u(a,i,b,j) = t2(a,i,b,j) + t1(a,i)*t1(b,j) 
        call get_combined_SingleDouble_amplitudes(t1,t2,u)
-
-       print *,"energy adn free crap"
 
        ! Extract EOS indices for amplitudes
        ! **********************************
@@ -1084,7 +1080,6 @@ contains
        call array_free(VOVV)
     end if
 
-    print *,"DOING(T)"
 #ifdef MOD_UNRELEASED
     ! calculate ccsd(t) pair interaction energies
     ! *******************************************
@@ -1119,7 +1114,6 @@ contains
                &PairFragment%noccAOS,PairFragment%noccAOS],4)
 
        endif
-       print *,"DONE(T) -- PRINTING"
 
        ! call ccsd(t) driver and pair fragment evaluation
        call ccsdpt_driver(PairFragment%noccAOS,PairFragment%nunoccAOS,&
@@ -1131,7 +1125,6 @@ contains
        call ccsdpt_energy_e5_pair(PairFragment,t1,ccsdpt_t1)
 
        ! release ccsd(t) singles and doubles amplitudes
-       print *,"freeing ccsdpt crap",ccsdpt_t1%initialized,ccsdpt_t2%initialized
        call array_free(ccsdpt_t1)
        call array_free(ccsdpt_t2)
 
@@ -1140,7 +1133,6 @@ contains
     end if
 #endif
 
-    print *,"test vovo dealloc",VOVO%initialized
     ! free vovo integrals
     if (PairFragment%CCModel == MODEL_RPA  .or. &
        &PairFragment%CCModel == MODEL_CC2  .or. &
@@ -1160,12 +1152,10 @@ contains
     endif
 
     ! Free remaining arrays
-    print *,"test rest"
     call array_free(VOVOocc)
     call array_free(t2occ)
     call array_free(t2virt)
     call array_free(VOVOvirt)
-    print *,"test done"
 
     !print *,"p1",VOVOocc%initialized,associated(VOVOocc%elm1)
     !print *,"p2",VOVOvirt%initialized,associated(VOVOvirt%elm1)
