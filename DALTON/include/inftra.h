8a38884cfb3d4716f49cc9c5da255e9bf05962c5
Hans Joergen Aagaard Jensen <hjj@sdu.dk>
2014-10-16 22:30:47 +0200

    hjaaj: now the energy_symm test works with .HJJTRA
    - when .HJJTRA:
      using new direct access file MO2INT_HJJTRA with symmetry packed (**|CD) integrals,
      for easy and efficient retrieval.
      The record information (which C,D where) is saved on the old sequential MOTWOINT file,
      which does NOT contain any integrals if .HJJTRA, only basis information and record inforamation.
      New NCDTRA is the number of records.
    - save NCDTRA on inftra.h in sirntra.F for sirhjjtra.F
    - moved total timing of integral transformation out in wrapper routine in sirtra.F

--- DALTON/include/inftra.h
+++ DALTON/include/inftra.h
@@ -2,10 +2,10 @@ C --- File: inftra.h ---
 C     Global information for the sirius/sirtra.F 2-el integral transforamtion modules
 C
       REAL*8  THRP,   THRQ
-      INTEGER LSRTAO, IPRTRA, LBUF, NIBUF, NBITS
+      INTEGER LSRTAO, IPRTRA, LBUF, NIBUF, NBITS, NCDTRA
       LOGICAL USEDRC, HJJTRA, NEWTRA, USE_INTSORT
       COMMON /INFTRA/ THRP,   THRQ,
-     &                LSRTAO, IPRTRA, LBUF, NIBUF, NBITS,
+     &                LSRTAO, IPRTRA, LBUF, NIBUF, NBITS, NCDTRA,
      &                USEDRC, HJJTRA, NEWTRA, USE_INTSORT
 C
 C     Name of file with 2-el integrals in AO basis.
