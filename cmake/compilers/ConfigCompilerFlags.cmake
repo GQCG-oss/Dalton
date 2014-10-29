a4d67387cb3a88f7eb7263ac123495cd375ea881
Radovan Bast <rbast@users.noreply.github.com>
2014-10-29 14:29:18 +0100

    Revert "test for -xHost and activate if test program runs"
    This reverts commit 27a38f42ffcc427d7d23c3f5faca3fb904e4ffab.

--- cmake/compilers/ConfigCompilerFlags.cmake
+++ cmake/compilers/ConfigCompilerFlags.cmake
@@ -1,8 +1,5 @@
 include(SaveCompilerFlags)
 
-# check whether we can use -xHost on this machine
-include(ConfigXHostFlag)
-
 if(CMAKE_C_COMPILER_WORKS)
     include(CFlags)
 endif()
--- cmake/compilers/test_xhost_flag.f90
+++
@@ -1,4 +0,0 @@
-! test program to see whether it is possible to use Intel -xHost flag
-program test
-   print *, 'Intel xHost flag is available on this computer.'
-end program
