731684c2592afccb51115fac4320ca7edcf0703c
Radovan Bast <rbast@users.noreply.github.com>
2014-11-17 23:30:51 +0100

    cleanup - should work now

--- cmake/CheckLSDALTON.cmake
+++ cmake/CheckLSDALTON.cmake
@@ -1,15 +1,9 @@
-
-message( "TESTING TESTING TESTING" )
-
 execute_process(
-   COMMAND python ${PROJECT_SOURCE_DIR}/LSDALTON/tools/check_decbcast.py
+   COMMAND python ${CMAKE_SOURCE_DIR}/LSDALTON/tools/check_decbcast.py
    OUTPUT_VARIABLE _output
    OUTPUT_STRIP_TRAILING_WHITESPACE
    )
 
-if( NOT ${_output} MATCHES "TESTSTATUS: GOOD")
+if(NOT ${_output} MATCHES "TESTSTATUS: GOOD")
    message(FATAL_ERROR "LSDALTON BCAST TYPE TEST NOT PASSED, PLEASE RERUN LSDALTON/tools/check_decbcast.py TO GET MORE INFORMATION")
-else()
-   message( "FOO" )
 endif()
-
