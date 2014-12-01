fe9db90c1c53faca126d1dfda7e2bab9c8d2bcf2
Jogvan Magnus Haugaard Olsen <foeroyingur@gmail.com>
2014-12-01 23:27:04 +0100

    Adjust runtest script so that single input file and also add option to include other files (e.g. .pot or .pcm)
        Also added the possibility to not use -noarch option which can be
        useful for restarts.
        All tests are affected because the suffix-filter option is no
        longer the third option and therefore in most of them f= cannot
        be neglected.

--- DALTON/test/runtest_dalton.py
+++ DALTON/test/runtest_dalton.py
@@ -23,7 +23,7 @@ class TestRun(runtest.TestRun):
         runtest.TestRun.__init__(self, _file, argv)
         self.return_code = 0
 
-    def run(self, inp_files, mol_files, f=None, args='', accepted_errors=[]):
+    def run(self, inp_files, mol_files=[], other_files=[], f=None, args='', accepted_errors=[], noarch=True):
 
         launch_script = os.path.normpath(os.path.join(self.binary_dir, 'dalton'))
         if self.skip_run:
@@ -35,47 +35,70 @@ class TestRun(runtest.TestRun):
                 sys.stderr.write('       try also --help\n')
                 sys.exit(-1)
 
-        launcher = '%s -noarch -nobackup %s' % (launch_script, args)
+        if noarch:
+            launcher = '%s -noarch -nobackup %s' % (launch_script, args)
+        else:
+            launcher = '%s -nobackup %s' % (launch_script, args)
 
+        commands = []
+        outputs_no_suffix = []
         for inp in inp_files:
             inp_no_suffix = os.path.splitext(inp)[0]
-            for mol in mol_files:
-                mol_no_suffix = os.path.splitext(mol)[0]
-                if inp_no_suffix == mol_no_suffix:
-                    output_no_suffix = '%s' % (inp_no_suffix,)
-                else:
-                    output_no_suffix = '%s_%s' % (inp_no_suffix, mol_no_suffix)
-                sys.stdout.write('\nrunning test: %s %s\n' % (inp_no_suffix, mol_no_suffix))
-
-                command = launcher + ' %s %s' % (inp_no_suffix, mol_no_suffix)
-                try:
-                    runtest.TestRun.execute(self,
-                                            command=command,
-                                            stdout_file_name = '%s.stdout' % output_no_suffix,
-                                            accepted_errors=accepted_errors)
-                    if f is None:
-                        sys.stdout.write('finished (no reference)\n')
+            if mol_files:
+                for mol in mol_files:
+                    mol_no_suffix = os.path.splitext(mol)[0]
+                    if other_files:
+                        for other in other_files:
+                            other_no_suffix = os.path.splitext(other)[0]
+                            if inp_no_suffix == mol_no_suffix and mol_no_suffix == other_no_suffix:
+                                output_no_suffix = '%s' % (inp_no_suffix,)
+                            else:
+                                output_no_suffix = '%s_%s_%s' % (inp_no_suffix, mol_no_suffix, other_no_suffix)
+                            outputs_no_suffix.append(output_no_suffix)
+                            sys.stdout.write('\nrunning test: %s %s %s\n' % (inp_no_suffix, mol_no_suffix, other_no_suffix))
+                            commands.append(launcher + ' %s %s %s' % (inp_no_suffix, mol_no_suffix, other_no_suffix))
                     else:
-                        try:
-                            # f is a suffix-filter dictionary
-                            for suffix in f:
-                                out = '%s.%s' % (output_no_suffix, suffix)
-                                f[suffix].check(self.work_dir, '%s' % out, 'result/%s' % out, self.verbose)
-                            sys.stdout.write('passed\n')
-                        except IOError, e:
-                            sys.stderr.write('ERROR: could not open file %s\n' % e.filename)
-                            sys.exit(-1)
-                        except runtest.TestFailedError, e:
-                            sys.stderr.write(str(e))
-                            self.return_code += 1
-                        except runtest.BadFilterError, e:
-                            sys.stderr.write(str(e))
-                            sys.exit(-1)
-                        except runtest.FilterKeywordError, e:
-                            sys.stderr.write(str(e))
-                            sys.exit(-1)
-                except runtest.AcceptedError, e:
-                    sys.stdout.write(str(e))
-                except runtest.SubprocessError, e:
-                    sys.stderr.write(str(e))
-                    sys.exit(-1)
+                        if inp_no_suffix == mol_no_suffix:
+                            output_no_suffix = '%s' % (inp_no_suffix,)
+                        else:
+                            output_no_suffix = '%s_%s' % (inp_no_suffix, mol_no_suffix)
+                        outputs_no_suffix.append(output_no_suffix)
+                        sys.stdout.write('\nrunning test: %s %s\n' % (inp_no_suffix, mol_no_suffix))
+                        commands.append(launcher + ' %s %s' % (inp_no_suffix, mol_no_suffix))
+            else:
+                outputs_no_suffix.append('%s' % (inp_no_suffix,))
+                sys.stdout.write('\nrunning test: %s\n' % (inp_no_suffix,))
+                commands.append(launcher + ' %s' % (inp_no_suffix,))
+        for output_no_suffix, command in zip(outputs_no_suffix, commands):
+            try:
+                runtest.TestRun.execute(self,
+                                        command=command,
+                                        stdout_file_name = '%s.stdout' % output_no_suffix,
+                                        accepted_errors=accepted_errors)
+                if f is None:
+                    sys.stdout.write('finished (no reference)\n')
+                else:
+                    try:
+                        # f is a suffix-filter dictionary
+                        for suffix in f:
+                            out = '%s.%s' % (output_no_suffix, suffix)
+                            f[suffix].check(self.work_dir, '%s' % out, 'result/%s' % out, self.verbose)
+                        sys.stdout.write('passed\n')
+                    except IOError, e:
+                        sys.stderr.write('ERROR: could not open file %s\n' % e.filename)
+                        sys.exit(-1)
+                    except runtest.TestFailedError, e:
+                        sys.stderr.write(str(e))
+                        self.return_code += 1
+                    except runtest.BadFilterError, e:
+                        sys.stderr.write(str(e))
+                        sys.exit(-1)
+                    except runtest.FilterKeywordError, e:
+                        sys.stderr.write(str(e))
+                        sys.exit(-1)
+            except runtest.AcceptedError, e:
+                sys.stdout.write(str(e))
+            except runtest.SubprocessError, e:
+                sys.stderr.write(str(e))
+                sys.exit(-1)
+
