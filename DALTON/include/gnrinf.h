eeb6e6376086f17a91f1b2237b15d9dfca95d02b
Radovan Bast <rbast@users.noreply.github.com>
2015-02-03 17:23:13 +0100

    make header files readable by F90 files

--- DALTON/include/gnrinf.h
+++ DALTON/include/gnrinf.h
@@ -1,38 +1,37 @@
-C File : gnrinf.h
-C
-c     -*- mode: fortran; fortran-continuation-string: "&" -*-
-c     File: gnrinf.h -- general information for DALTON
-c
-C
-C     EMBEDDING : QM part is embedded in environment (solvent or e.g. protein)
-C                 May 2011/hjaaj: EMBEDDING = FLAG(16) .or. PCM .or. QM3 .or. QMMM .or. PEQM
-C                 (For now, EMBEDDING is defined in sirius/sirinp.F because this is
-C                 the first instance where all of FLAG(16), PCM, QM3, QMMM, PEQM are set)
-C
-      LOGICAL TESTIN, OPTWLK, RNHERM, RNSIRI, RNABAC, GEOCNV,
-     &        HRINPC, SRINPC, RDINPC, RDMLIN, PARCAL, DIRCAL,
-     &        WRINDX, WLKREJ, WALKIN, RNRESP, USRIPR, SEGBAS,
-     &        DOCCSD, OPTNEW, NEWSYM, NEWBAS, NEWPRP, RELCAL,
-     &        TOTSYM, NMWALK, DKTRAN, GEOALL, WESTA,  SEGAUX,
-     &        ERFEXP, DOSRIN, SRINTS, CHI1ST, DKHINT,
-     &        EMBEDDING, QM3, QMMM,   QMNPMM, PEQM,   QFIT,
+!
+!     -*- mode: fortran; fortran-continuation-string: "&" -*-
+!     File: gnrinf.h -- general information for DALTON
+!
+!
+!     EMBEDDING : QM part is embedded in environment (solvent or e.g. protein)
+!                 May 2011/hjaaj: EMBEDDING = FLAG(16) .or. PCM .or. QM3 .or. QMMM .or. PEQM
+!                 (For now, EMBEDDING is defined in sirius/sirinp.F because this is
+!                 the first instance where all of FLAG(16), PCM, QM3, QMMM, PEQM are set)
+!
+      LOGICAL TESTIN, OPTWLK, RNHERM, RNSIRI, RNABAC, GEOCNV,           &
+     &        HRINPC, SRINPC, RDINPC, RDMLIN, PARCAL, DIRCAL,           &
+     &        WRINDX, WLKREJ, WALKIN, RNRESP, USRIPR, SEGBAS,           &
+     &        DOCCSD, OPTNEW, NEWSYM, NEWBAS, NEWPRP, RELCAL,           &
+     &        TOTSYM, NMWALK, DKTRAN, GEOALL, WESTA,  SEGAUX,           &
+     &        ERFEXP, DOSRIN, SRINTS, CHI1ST, DKHINT,                   &
+     &        EMBEDDING, QM3, QMMM,   QMNPMM, PEQM,   QFIT,             &
      &        USE_LSLIB
       REAL*8  GRADML, PANAS,  CHIVAL, THR_REDFAC
       INTEGER KCHARG, ITERNR, ITERMX, IPRUSR, LENBAS
-C
-      COMMON /GNRINF/
-     &        ! real*8ZZ
-     &        GRADML, PANAS,  CHIVAL, THR_REDFAC,
-     &        ! integer:
-     &        KCHARG, ITERNR, ITERMX, IPRUSR, LENBAS,
-     &        ! logical:
-     &        TESTIN, OPTWLK, RNHERM, RNSIRI, RNABAC, GEOCNV,
-     &        HRINPC, SRINPC, RDINPC, RDMLIN, PARCAL, DIRCAL,
-     &        WRINDX, WLKREJ, WALKIN, RNRESP, USRIPR, SEGBAS,
-     &        DOCCSD, OPTNEW, NEWSYM, NEWBAS, NEWPRP, RELCAL,
-     &        TOTSYM, NMWALK, DKTRAN, GEOALL, WESTA,  SEGAUX,
-     &        ERFEXP, DOSRIN, SRINTS, CHI1ST, DKHINT,
-     &        EMBEDDING, QM3, QMMM,   QMNPMM, PEQM,   QFIT,
+!
+      COMMON /GNRINF/                                                   &
+              ! real*8ZZ
+     &        GRADML, PANAS,  CHIVAL, THR_REDFAC,                       &
+              ! integer
+     &        KCHARG, ITERNR, ITERMX, IPRUSR, LENBAS,                   &
+              ! logical
+     &        TESTIN, OPTWLK, RNHERM, RNSIRI, RNABAC, GEOCNV,           &
+     &        HRINPC, SRINPC, RDINPC, RDMLIN, PARCAL, DIRCAL,           &
+     &        WRINDX, WLKREJ, WALKIN, RNRESP, USRIPR, SEGBAS,           &
+     &        DOCCSD, OPTNEW, NEWSYM, NEWBAS, NEWPRP, RELCAL,           &
+     &        TOTSYM, NMWALK, DKTRAN, GEOALL, WESTA,  SEGAUX,           &
+     &        ERFEXP, DOSRIN, SRINTS, CHI1ST, DKHINT,                   &
+     &        EMBEDDING, QM3, QMMM,   QMNPMM, PEQM,   QFIT,             &
      &        USE_LSLIB
 
       INTEGER LBASDIR
@@ -40,4 +39,3 @@ C
       CHARACTER*(LBASDIR) BASDIR
       CHARACTER*12        WFTYPE
       COMMON /GNRCHR/ BASDIR, WFTYPE
-C --- end of gnrinf.h ---
--- DALTON/include/qmnpmm.h
+++ DALTON/include/qmnpmm.h
@@ -1,43 +1,40 @@
-C FILE: DALTON/include/qmnpmm.h
-C
-      REAL*8  NPCORD,  MMCORD,  NPCHRG,  MMCHRG,
-     &        NPFPOL,  NPFCAP,  NPFOMG1, NPFGAM1,
-     &        NPFOMG2, NPFGAM2, NPFFAC,
-     &        MMFM0,   MMFPOL,
-     &        ENSOLQNP, EESOLQNP, ENSOLMNP, EESOLMNP,
+      REAL*8  NPCORD,  MMCORD,  NPCHRG,  MMCHRG,                        &
+     &        NPFPOL,  NPFCAP,  NPFOMG1, NPFGAM1,                       &
+     &        NPFOMG2, NPFGAM2, NPFFAC,                                 &
+     &        MMFM0,   MMFPOL,                                          &
+     &        ENSOLQNP, EESOLQNP, ENSOLMNP, EESOLMNP,                   &
      &        ENSOLQMM, EESOLQMM, ENSOLMMM, EESOLMMM
-C
-      INTEGER TNPBLK,  TMMBLK,  IPRTLVL,
-     &        TNPATM,  TMMATM,  NPATOM, NPFTYP,
-     &        MMATOM,  MMFTYP,  TNPFF,  TMMFF,
+!
+      INTEGER TNPBLK,  TMMBLK,  IPRTLVL,                                &
+     &        TNPATM,  TMMATM,  NPATOM, NPFTYP,                         &
+     &        MMATOM,  MMFTYP,  TNPFF,  TMMFF,                          &
      &        MMMOL,   TPOLATM, MMSKIP
-C
-      LOGICAL DONPSUB, DOMMSUB, NPMQGAU, MMMQGAU,
-     &        MQITER,  CMXPOL,  DONPCAP, DOMMCAP,
+!
+      LOGICAL DONPSUB, DOMMSUB, NPMQGAU, MMMQGAU,                       &
+     &        MQITER,  CMXPOL,  DONPCAP, DOMMCAP,                       &
      &        DONPPOL, DOMMPOL, NOVDAMP
-C
+!
       INTEGER MAXBLK,  MXNPATM, MXMMATM, MXNPFF, MXMMFF
       PARAMETER (MAXBLK = 5)
       PARAMETER (MXNPATM = 10000)
       PARAMETER (MXMMATM = 90000)
       PARAMETER (MXNPFF = 5)
       PARAMETER (MXMMFF = 20)
-C
-      COMMON /QMNPIN/ NPCORD(3,MXNPATM), MMCORD(3,MXMMATM),     ! real*8
-     &                NPCHRG(MAXBLK),    MMCHRG(MAXBLK),
-     &                NPFPOL(MXNPFF),    NPFCAP(MXNPFF),
-     &                NPFOMG1(MXNPFF),   NPFGAM1(MXNPFF),
-     &                NPFOMG2(MXNPFF),   NPFGAM2(MXNPFF),
-     &                NPFFAC(MXNPFF),
-     &                MMFM0(MXMMFF),     MMFPOL(MXMMFF),
-     &                ENSOLQNP, EESOLQNP, ENSOLMNP, EESOLMNP,
-     &                ENSOLQMM, EESOLQMM, ENSOLMMM, EESOLMMM,
-     &                TNPBLK,  TMMBLK,   IPRTLVL,               ! integer
-     &                TNPATM,  TMMATM,   TNPFF,   TMMFF,
-     &                NPFTYP(MXNPATM),   TPOLATM, MMFTYP(MXMMATM),
-     &                NPATOM(MAXBLK),    MMATOM(MAXBLK),
-     &                MMMOL(MXMMATM),    MMSKIP(MXMMATM),
-     &                DONPSUB, DOMMSUB,  NPMQGAU, MMMQGAU,      ! logical
-     &                MQITER,  CMXPOL,   DONPCAP, DOMMCAP,
+!
+      COMMON /QMNPIN/ NPCORD(3,MXNPATM), MMCORD(3,MXMMATM),             &
+     &                NPCHRG(MAXBLK),    MMCHRG(MAXBLK),                &
+     &                NPFPOL(MXNPFF),    NPFCAP(MXNPFF),                &
+     &                NPFOMG1(MXNPFF),   NPFGAM1(MXNPFF),               &
+     &                NPFOMG2(MXNPFF),   NPFGAM2(MXNPFF),               &
+     &                NPFFAC(MXNPFF),                                   &
+     &                MMFM0(MXMMFF),     MMFPOL(MXMMFF),                &
+     &                ENSOLQNP, EESOLQNP, ENSOLMNP, EESOLMNP,           &
+     &                ENSOLQMM, EESOLQMM, ENSOLMMM, EESOLMMM,           &
+     &                TNPBLK,  TMMBLK,   IPRTLVL,                       &
+     &                TNPATM,  TMMATM,   TNPFF,   TMMFF,                &
+     &                NPFTYP(MXNPATM),   TPOLATM, MMFTYP(MXMMATM),      &
+     &                NPATOM(MAXBLK),    MMATOM(MAXBLK),                &
+     &                MMMOL(MXMMATM),    MMSKIP(MXMMATM),               &
+     &                DONPSUB, DOMMSUB,  NPMQGAU, MMMQGAU,              &
+     &                MQITER,  CMXPOL,   DONPCAP, DOMMCAP,              &
      &                DONPPOL, DOMMPOL,  NOVDAMP
-C -- end of DALTON/include/qmnpmm.h
