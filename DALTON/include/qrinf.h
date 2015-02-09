f6d06bb4bda5c1b467dc8c27f13bd24a349ba4f8
Radovan Bast <rbast@users.noreply.github.com>
2015-02-09 17:48:39 +0100

    convert header files so that they can be included in F90

--- DALTON/include/qrinf.h
+++ DALTON/include/qrinf.h
@@ -3,12 +3,13 @@
 !
 !  Special info for quadratic response
 !
-      INTEGER         MZVAR,  MZCONF, MZWOPT,
-     &                MZYVAR, MZYCON, MZYWOP,
+      INTEGER         MZVAR,  MZCONF, MZWOPT,                           &
+     &                MZYVAR, MZYCON, MZYWOP,                           &
      &                MSYMA, MSYMB, MSYMC, MZYVMX
       LOGICAL         QRREST
-      COMMON /QRINF/  MZVAR(8),  MZCONF(8), MZWOPT(8),
-     &                MZYVAR(8), MZYCON(8), MZYWOP(8),
-     &                MSYMA, MSYMB, MSYMC,  MZYVMX,
+      COMMON /QRINF/  MZVAR(8),  MZCONF(8), MZWOPT(8),                  &
+     &                MZYVAR(8), MZYCON(8), MZYWOP(8),                  &
+     &                MSYMA, MSYMB, MSYMC,  MZYVMX,                     &
      &                QRREST
 ! -- end of qrinf.h --
+
