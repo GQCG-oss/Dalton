d064c4c15cb3d0f22acb4c28341cc76b933638f0
Janus Juul Eriksen (Titan) <januseriksen@gmail.com>
2015-04-18 04:36:20 -0400

    comment out ls_dgemm_acc routine.
    we cannot use assumed size arrays in host_data directives.
    need to come up with a better solution.

--- LSDALTON/cuda/gpu_interfaces.F90
+++ LSDALTON/cuda/gpu_interfaces.F90
@@ -66,93 +66,93 @@ module gpu_interfaces
 
 contains
 
-#ifdef VAR_OPENACC
-  subroutine ls_dgemm_acc(transa,transb,m,n,k,alpha,a,lda,b,ldb,beta,c,ldc,acc_handle,cublas_handle)
-
-       use precision
-       use openacc
-       use iso_c_binding
-
-       character, intent(inout) :: transa,transb
-       integer, intent(inout) :: m,n,k,lda,ldb,ldc
-       real(realk), dimension(lda,*), intent(inout) :: a
-       real(realk), dimension(ldb,*), intent(inout) :: b
-       real(realk), dimension(ldc,*), intent(inout) :: c
-       real(realk), intent(inout) :: alpha,beta
-       integer(kind=acc_handle_kind), intent(inout), optional :: acc_handle
-       type(c_ptr), intent(inout), optional :: cublas_handle
-       logical :: async
-       integer :: transa_2,transb_2
-!#ifdef VAR_PGF90
-!       integer*4, external :: acc_get_cuda_stream
+!#ifdef VAR_OPENACC
+!  subroutine ls_dgemm_acc(transa,transb,m,n,k,alpha,a,lda,b,ldb,beta,c,ldc,acc_handle,cublas_handle)
+!
+!       use precision
+!       use openacc
+!       use iso_c_binding
+!
+!       character, intent(inout) :: transa,transb
+!       integer, intent(inout) :: m,n,k,lda,ldb,ldc
+!       real(realk), dimension(lda,*), intent(inout) :: a
+!       real(realk), dimension(ldb,*), intent(inout) :: b
+!       real(realk), dimension(ldc,*), intent(inout) :: c
+!       real(realk), intent(inout) :: alpha,beta
+!       integer(kind=acc_handle_kind), intent(inout), optional :: acc_handle
+!       type(c_ptr), intent(inout), optional :: cublas_handle
+!       logical :: async
+!       integer :: transa_2,transb_2
+!!#ifdef VAR_PGF90
+!!       integer*4, external :: acc_get_cuda_stream
+!!#endif
+!
+!       async = .false.
+!       if (present(acc_handle)) async = .true.
+!
+!       if ((transa .ne. 'n') .or. (transa .ne. 'N') .or. (transa .ne. 't') .or. (transa .ne. 'T')) then
+!
+!          call lsquit('wrong argument to transa in ls_dgemm_acc',-1)
+!
+!       elseif ((transb .ne. 'n') .or. (transb .ne. 'N') .or. (transb .ne. 't') .or. (transb .ne. 'T')) then
+!
+!          call lsquit('wrong argument to transb in ls_dgemm_acc',-1)
+!
+!       endif
+!
+!#ifdef VAR_CUBLAS
+!
+!       if ((transa .eq. 'n') .or. (transa .eq. 'N')) then
+!
+!          transa_2 = 0 
+!
+!       else
+!
+!          transa_2 = 1
+!
+!       endif
+!       if ((transb .eq. 'n') .or. (transb .eq. 'N')) then
+!
+!          transb_2 = 0
+!
+!       else
+!
+!          transb_2 = 1
+!
+!       endif
+!
 !#endif
-
-       async = .false.
-       if (present(acc_handle)) async = .true.
-
-       if ((transa .ne. 'n') .or. (transa .ne. 'N') .or. (transa .ne. 't') .or. (transa .ne. 'T')) then
-
-          call lsquit('wrong argument to transa in ls_dgemm_acc',-1)
-
-       elseif ((transb .ne. 'n') .or. (transb .ne. 'N') .or. (transb .ne. 't') .or. (transb .ne. 'T')) then
-
-          call lsquit('wrong argument to transb in ls_dgemm_acc',-1)
-
-       endif
-
-#ifdef VAR_CUBLAS
-
-       if ((transa .eq. 'n') .or. (transa .eq. 'N')) then
-
-          transa_2 = 0 
-
-       else
-
-          transa_2 = 1
-
-       endif
-       if ((transb .eq. 'n') .or. (transb .eq. 'N')) then
-
-          transb_2 = 0
-
-       else
-
-          transb_2 = 1
-
-       endif
-
-#endif
-
-#if defined(VAR_CRAY) && !defined(VAR_CUBLAS)
-
-       if (async) then
-
-!$acc host_data use_device(a,b,c)
-          call dgemm_acc_openacc_async(handle,transa,transb,m,n,k,alpha,a,lda,b,ldb,beta,c,ldc)
-!$acc end host_data
-
-       else
-
-!$acc host_data use_device(a,b,c)
-          call dgemm_acc(transa,transb,m,n,k,alpha,a,lda,b,ldb,beta,c,ldc)
-!$acc end host_data
-
-       endif
-
-#elif defined(VAR_CUBLAS)
-
-       if (.not. present(cublas_handle)) call lsquit('defined(VAR_CUBLAS), but no cublas handle.',DECinfo%output) 
-
-!$acc host_data use_device(a,b,c)
-       stat = cublasDgemm_v2(cublas_handle,int(transa_2,kind=4),int(transb_2,kind=4),int(m,kind=4),int(n,kind=4),int(k,kind=4),&
-                             & alpha,c_loc(a),int(lda,kind=4),c_loc(b),int(ldb,kind=4),&
-                             & beta,c_loc(c),int(ldc,kind=4))
-!$acc end host_data
-
-#endif
-
-  end subroutine ls_dgemm_acc
-#endif
+!
+!#if defined(VAR_CRAY) && !defined(VAR_CUBLAS)
+!
+!       if (async) then
+!
+!!$acc host_data use_device(a,b,c)
+!          call dgemm_acc_openacc_async(handle,transa,transb,m,n,k,alpha,a,lda,b,ldb,beta,c,ldc)
+!!$acc end host_data
+!
+!       else
+!
+!!$acc host_data use_device(a,b,c)
+!          call dgemm_acc(transa,transb,m,n,k,alpha,a,lda,b,ldb,beta,c,ldc)
+!!$acc end host_data
+!
+!       endif
+!
+!#elif defined(VAR_CUBLAS)
+!
+!       if (.not. present(cublas_handle)) call lsquit('defined(VAR_CUBLAS), but no cublas handle.',DECinfo%output) 
+!
+!!$acc host_data use_device(a,b,c)
+!       stat = cublasDgemm_v2(cublas_handle,int(transa_2,kind=4),int(transb_2,kind=4),int(m,kind=4),int(n,kind=4),int(k,kind=4),&
+!                             & alpha,c_loc(a),int(lda,kind=4),c_loc(b),int(ldb,kind=4),&
+!                             & beta,c_loc(c),int(ldc,kind=4))
+!!$acc end host_data
+!
+!#endif
+!
+!  end subroutine ls_dgemm_acc
+!#endif
 
   subroutine gpu_interfaces_dummy_routine()
 
