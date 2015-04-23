5c9526dd1e2a45e081dd1bed92bc714785e639d9
Pablo Baudin <baudin.pablo@gmail.com>
2015-04-23 10:45:20 +0200

    clean up fragment_energy.F90 file

--- LSDALTON/deccc/full_rimp2.F90
+++ LSDALTON/deccc/full_rimp2.F90
@@ -28,9 +28,6 @@ module fullrimp2
   use ri_util_module
   !  use orbital_operations
   use full_molecule
-!  use ccintegrals!,only: get_full_AO_integrals,get_AO_hJ,get_AO_K,get_AO_Fock
-!  use ccdriver!,only: ccsolver_justenergy, ccsolver
-  !  use fragment_energy_module,only : Full_DECMP2_calculation
 
   public :: full_canonical_rimp2
 
