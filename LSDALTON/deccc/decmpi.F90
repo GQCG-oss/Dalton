5cb8a431710e10d84a6b5a60a2e6c2466eb3fca3
Kasper Kristensen <kasperk@chem.au.dk>
2015-07-22 09:56:55 +0200

    Introduced PE-EOM-MBPT2 model for excitation energies (only noddy code implementation).

--- LSDALTON/deccc/decmpi.F90
+++ LSDALTON/deccc/decmpi.F90
@@ -2392,6 +2392,7 @@ contains
     call ls_mpi_buffer(DECitem%JacobianPrecond,Master)
     call ls_mpi_buffer(DECitem%HaldApprox,Master)
     call ls_mpi_buffer(DECitem%LW1,Master)
+    call ls_mpi_buffer(DECitem%P_EOM_MBPT2,Master)
     call ls_mpi_buffer(DECitem%doDEC,Master)
     call ls_mpi_buffer(DECitem%DECCO,Master)
     call ls_mpi_buffer(DECitem%DECNP,Master)
