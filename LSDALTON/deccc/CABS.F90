ce1262427df8c065ea2fbb8236500fede410f97d
Kasper Kristensen <kasperk@chem.au.dk>
2015-09-25 15:54:42 +0200

    Finally fixed SNOOP in combination with F12.

--- LSDALTON/deccc/CABS.F90
+++ LSDALTON/deccc/CABS.F90
@@ -20,6 +20,8 @@ CONTAINS
     nbast_cabs = getNbasis(AOdfCABS,ContractedintType,SETTING%MOLECULE(1)%p,LUPRI)
   end subroutine determine_CABS_nbast
 
+
+
   subroutine build_CABS_MO(nbast_cabs,lupri,setting,CMO_std,CMO_cabs)
     implicit none
     !> Number of CABS AOs (CABS+)
@@ -34,115 +36,64 @@ CONTAINS
     TYPE(MATRIX),intent(inout)    :: CMO_cabs
     !
     real(realk)     :: TIMSTR,TIMEND
-    type(matrix) :: S,Smix,S_cabs,tmp,S_minus_sqrt,S_minus_sqrt_cabs
-    type(matrix) :: tmp_cabs,Vnull,tmp2,SVDmat
+    type(matrix) :: SAOCABS,S_cabs,tmp,S_minus_sqrt_cabs
+    type(matrix) :: tmp_cabs,Vnull,tmp2,SMOCABS
     real(realk),pointer :: SV(:),optwrk(:),Sfull(:,:),S_cabsfull(:,:)
     integer,pointer :: IWORK(:)
-    integer     :: lwork,nbast,nnull,luerr,IERR,INFO,I,nbastCABO
-    logical  :: ODSCREEN,Failed,VerifyCabs,doMPI
+    integer     :: lwork,nnull,luerr,IERR,INFO,I,nbastCABO,nov,nAO
+    logical  :: ODSCREEN,Failed,doMPI
 
-    VerifyCabs = DECinfo%F12debug
-#ifdef VAR_LSDEBUG
-    VerifyCabs = .TRUE.
-#endif
+    CALL LSTIMER('START ',TIMSTR,TIMEND,lupri)
+
     IERR=0
     ODSCREEN = SETTING%SCHEME%OD_SCREEN
     SETTING%SCHEME%OD_SCREEN = .FALSE.
     luerr = 6
-    CALL LSTIMER('START ',TIMSTR,TIMEND,lupri)
-    nbast = getNbasis(AORegular,ContractedintType,SETTING%MOLECULE(1)%p,LUPRI)
-    CALL mat_init(S,nbast,nbast)
-    call mat_init(S_minus_sqrt,nbast,nbast)
-
-    call mat_init(tmp,nbast,nbast)
-    call II_get_mixed_overlap(LUPRI,LUERR,SETTING,S,AORegular,AORegular,.FALSE.,.FALSE.)
-    call lowdin_diag(nbast, S%elms,tmp%elms, S_minus_sqrt%elms, lupri)
-    call mat_free(tmp)
-    call mat_free(S)
+    nov = CMO_std%ncol  ! number of occupied + virtual MOs
+    nAO = CMO_std%nrow  ! number of AO basis functions
 
     CALL mat_init(S_cabs,nbast_cabs,nbast_cabs)
-    call mat_init(S_minus_sqrt_cabs,nbast_cabs,nbast_cabs)
-
     call mat_init(tmp_cabs,nbast_cabs,nbast_cabs)
+    call mat_init(S_minus_sqrt_cabs,nbast_cabs,nbast_cabs)
     doMPI = SETTING%SCHEME%doMPI
     SETTING%SCHEME%doMPI = .FALSE.
-    call II_get_mixed_overlap(LUPRI,LUERR,SETTING,S_cabs,AOdfCABS,AOdfCABS,.FALSE.,.FALSE.)
-    If(VerifyCabs) then
-       !Test that S_cabs have the structure 
-       !  S_cabs =  (  S   |     *      )
-       !            (  *   |  cabs_only )
-       CALL mat_init(S,nbast,nbast)
-       call II_get_mixed_overlap(LUPRI,LUERR,SETTING,S,AORegular,AORegular,.FALSE.,.FALSE.)
-       call mem_alloc(Sfull,nbast,nbast)
-       CALL mat_to_full(S,1.0E0_realk,Sfull)
-       CALL mat_free(S)
+    ! CABSAO,CABSAO overlap matrix
 
-       call mem_alloc(S_cabsfull,nbast_cabs,nbast_cabs)
-       CALL mat_to_full(S_cabs,1.0E0_realk,S_cabsfull)
-       call verifyCABS_HaveRegularFirst(Sfull,nbast,S_cabsfull,nbast_cabs,Failed)
-       call mem_dealloc(Sfull)
-       IF(Failed)THEN
-          CALL LSQUIT('Something Wrong CABSAO do not have regular AO first',-1)
-       ELSE
-          !check CABSonly is last 
-          nbastCABO = nbast_cabs - nbast
-          CALL mat_init(S,nbastCABO,nbastCABO)
-          call II_get_mixed_overlap(LUPRI,LUERR,SETTING,S,AOdfCABO,AOdfCABO,.FALSE.,.FALSE.)
-          call mem_alloc(Sfull,nbastCABO,nbastCABO)
-          CALL mat_to_full(S,1.0E0_realk,Sfull)
-          CALL mat_free(S)
-          call verifyCABS_HaveCABOLast(Sfull,nbastCABO,nbast,S_cabsfull,nbast_cabs,Failed)
-          call mem_dealloc(Sfull)
-          IF(Failed)THEN
-             CALL LSQUIT('Something Wrong CABSAO do not have CABS only last',-1)
-          ELSE
-             !check last block
-             CALL mat_init(S,nbastCABO,nbast)
-             call II_get_mixed_overlap(LUPRI,LUERR,SETTING,S,AOdfCABO,AORegular,.FALSE.,.FALSE.)
-             call mem_alloc(Sfull,nbastCABO,nbast)
-             CALL mat_to_full(S,1.0E0_realk,Sfull)
-             CALL mat_free(S)
-             call verifyCABS3(Sfull,nbastCABO,nbast,S_cabsfull,nbast_cabs,Failed)
-             call mem_dealloc(Sfull)
-             IF(Failed)THEN
-                CALL LSQUIT('Something Wrong CABS3',-1)
-             ENDIF
-          ENDIF
-       ENDIF
-       call mem_dealloc(S_cabsfull)
-       WRITE(lupri,*)'CABS basis test is successful'
-    endif
+    call II_get_mixed_overlap(LUPRI,LUERR,SETTING,S_cabs,AOdfCABS,AOdfCABS,.FALSE.,.FALSE.)
+    ! CABS,CABS overlap matrix in orthogonalized basis (S^-1/2)
 
     call lowdin_diag(nbast_cabs, S_cabs%elms,tmp_cabs%elms, S_minus_sqrt_cabs%elms, lupri)
+
     call mat_free(tmp_cabs)
-    CALL mat_free(S_cabs)       
-    CALL mat_init(Smix,nbast,nbast_cabs)
-    call II_get_mixed_overlap(LUPRI,LUERR,setting,Smix,AORegular,AOdfCABS,.FALSE.,.FALSE.)
+    CALL mat_free(S_cabs)
+    CALL mat_init(SAOCABS,nAO,nbast_cabs)
+
+    ! AO,CABSAO overlap matrix
+    call II_get_mixed_overlap(LUPRI,LUERR,setting,SAOCABS,AORegular,AOdfCABS,.FALSE.,.FALSE.)
     SETTING%SCHEME%doMPI=doMPI
 
-    call mat_init(tmp,nbast,nbast_cabs)
-    call mat_mul(S_minus_sqrt,Smix,'N','N',1.0E0_realk,0.0E0_realk,tmp)
-    call mat_mul(tmp,S_minus_sqrt_cabs,'N','N',1.0E0_realk,0.0E0_realk,Smix)
-
+    ! Calculate (MO, ortogonalized CABSAO) overlap: C^T S(AO,CABS) SCABS^(-1/2)
+    call mat_init(tmp,nov,nbast_cabs)
+    call mat_mul(CMO_std,SAOCABS,'T','N',1.0E0_realk,0.0E0_realk,tmp)
+    call mat_init(SMOCABS,nov,nbast_cabs)
+    call mat_mul(tmp,S_minus_sqrt_cabs,'N','N',1.0E0_realk,0.0E0_realk,SMOCABS)
     call mat_free(tmp)
+    call mat_free(SAOCABS)
 
-    call mat_free(S_minus_sqrt)
-
-    call mat_init(tmp,Smix%nrow,Smix%nrow)
-    call mat_init(tmp_cabs,Smix%ncol,Smix%ncol)
-
+    call mat_init(tmp,SMOCABS%nrow,SMOCABS%nrow)
+    call mat_init(tmp_cabs,SMOCABS%ncol,SMOCABS%ncol)
     call mat_zero(tmp)
     call mat_zero(tmp_cabs)
 
-    call mem_alloc(SV,MIN(Smix%nrow,Smix%ncol))
+    call mem_alloc(SV,MIN(SMOCABS%nrow,SMOCABS%ncol))
     SV=0.0E0_realk
     call mem_alloc(optwrk,5) 
-    call dgesvd('A','A',Smix%nrow,Smix%ncol,Smix%elms,Smix%nrow, &
+    call dgesvd('A','A',SMOCABS%nrow,SMOCABS%ncol,SMOCABS%elms,SMOCABS%nrow, &
          & SV,tmp%elms,tmp%nrow,tmp_cabs%elms,tmp_cabs%nrow,optwrk,-1,INFO)
     lwork = INT(optwrk(1))
     call mem_dealloc(optwrk) 
     call mem_alloc(optwrk,lwork) 
-    call dgesvd('A','A',Smix%nrow,Smix%ncol,Smix%elms,Smix%nrow, &
+    call dgesvd('A','A',SMOCABS%nrow,SMOCABS%ncol,SMOCABS%elms,SMOCABS%nrow, &
          & SV,tmp%elms,tmp%nrow,tmp_cabs%elms,tmp_cabs%nrow,optwrk,lwork,INFO)
     IF( INFO.GT.0 ) THEN
        WRITE(*,*)'The algorithm computing SVD failed to converge.'
@@ -156,24 +107,19 @@ CONTAINS
        ENDIF
     ENDDO
 
-    print *, 'nbast_cabs ', nbast_cabs
-    print *, 'nnull', nnull
-    print * ,'sv ', sv
-
-    IF(nnull.NE.nbast_cabs-MIN(Smix%nrow,Smix%ncol))THEN       
+    IF(nnull.NE.nbast_cabs-MIN(SMOCABS%nrow,SMOCABS%ncol))THEN       
        print*,'nnull',nnull
-       print*,'nbast_cabs-MIN(Smix%nrow,Smix%ncol)',nbast_cabs-MIN(Smix%nrow,Smix%ncol)
-       print*,'nbast_cabs,MIN(Smix%nrow,Smix%ncol)',nbast_cabs,MIN(Smix%nrow,Smix%ncol)
+       print*,'nbast_cabs-MIN(SMOCABS%nrow,SMOCABS%ncol)',nbast_cabs-MIN(SMOCABS%nrow,SMOCABS%ncol)
+       print*,'nbast_cabs,MIN(SMOCABS%nrow,SMOCABS%ncol)',nbast_cabs,MIN(SMOCABS%nrow,SMOCABS%ncol)
        CALL LSQUIT('error in build_CABS_MO',-1)
     ENDIF
-    !    nnull = nbast_cabs-MIN(Smix%nrow,Smix%ncol)
     call mem_dealloc(SV)
     call mat_free(tmp)
-    call mat_free(Smix)
+    call mat_free(SMOCABS)
 
     !Construct CABS MO from V (tmp_cabs)
     call mat_init(tmp,nnull,nbast_cabs)
-    call mat_retrieve_block(tmp_cabs,tmp%elms,nnull,nbast_cabs,nbast+1,1)
+    call mat_retrieve_block(tmp_cabs,tmp%elms,nnull,nbast_cabs,nov+1,1)
     call mat_free(tmp_cabs)
 
     call mat_init(Vnull,nbast_cabs,nnull)
@@ -192,11 +138,11 @@ CONTAINS
     call mat_free(S_minus_sqrt_cabs)
     call mat_free(Vnull)       
 
-
     CALL LSTIMER('build_CABS_MO',TIMSTR,TIMEND,lupri)       
     SETTING%SCHEME%OD_SCREEN = ODSCREEN
 
   end subroutine build_CABS_MO
+
 
   subroutine verifyCABS_HaveRegularFirst(Sfull,nbast,S_cabsfull,nbast_cabs,Failed)
     implicit none
