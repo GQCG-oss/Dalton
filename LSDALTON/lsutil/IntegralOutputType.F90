56d31d295f7205bce694b10f4df17245b90c1ef3
Thomas Kjaergaard <tkjaergaardchem@gmail.com>
2015-09-25 14:27:19 +0200

    Hopefully fixed dashboard problem on master branch. TK

--- LSDALTON/lsutil/IntegralOutputType.F90
+++ LSDALTON/lsutil/IntegralOutputType.F90
@@ -9,8 +9,13 @@ integer,pointer :: BATCH(:)
 end type intbatch
 
 TYPE INTEGRALOUTPUT
+#ifdef VAR_PTR_RESHAPE
+REAL(REALK),contiguous,pointer    :: ResultMat(:,:,:,:,:) 
+REAL(REALK),contiguous,pointer    :: Result3D(:,:,:) 
+#else
 REAL(REALK),pointer    :: ResultMat(:,:,:,:,:) 
 REAL(REALK),pointer    :: Result3D(:,:,:) 
+#endif
 type(lstensor),pointer :: screenTensor
 type(lstensor),pointer :: resultTensor
 type(lstensor)         :: RHScont
