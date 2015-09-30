9922fe86971b74684462a4b0604a285612997b36
Pablo Baudin <baudin.pablo@gmail.com>
2015-09-30 21:17:35 +0200

    update personal contributions

--- LSDALTON/lsdaltonsrc/init_lsdalton.F90
+++ LSDALTON/lsdaltonsrc/init_lsdalton.F90
@@ -266,7 +266,7 @@ SUBROUTINE print_intro(lupri)
        &  ' ----------------------------------------------------------------------------',&
        &  ' Vebjoern Bakken,         University of Oslo,        Norway    (Geometry optimizer)',&
        &  ' Radovan Bast,            KTH Stockholm,             Sweden    (CMake, Testing)',&
-       &  ' Pablo Baudin,            Aarhus University,         Denmark   (DEC)',&
+       &  ' Pablo Baudin,            Aarhus University,         Denmark   (DEC,CCSD)',&
        &  ' Sonia Coriani,           University of Trieste,     Italy     (Response)',&
        &  ' Patrick Ettenhuber,      Aarhus University,         Denmark   (CCSD)',&
        &  ' Trygve Helgaker,         University of Oslo,        Norway    (Supervision)',&
