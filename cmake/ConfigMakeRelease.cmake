114284c52a2ca1b5ad14a05c51d8a1b425595a0f
Radovan Bast <bast@users.noreply.github.com>
2016-03-25 11:29:32 +0100

    restore make relase after rename of CHANGELOG.md

--- cmake/ConfigMakeRelease.cmake
+++ cmake/ConfigMakeRelease.cmake
@@ -15,7 +15,7 @@ set(CPACK_SOURCE_INSTALLED_DIRECTORIES "${EXPORT_DIR};/")
 
 include(CPack)
 
-set(DIRS_TO_RELEASE basis cmake CMakeLists.txt COPYING CTestConfig.cmake DALTON external setup VERSION .gitignore CHANGELOG.rst dalton.in)
+set(DIRS_TO_RELEASE basis cmake CMakeLists.txt COPYING CTestConfig.cmake DALTON external setup VERSION .gitignore CHANGELOG.md dalton.in)
 
 add_custom_target(
     release
