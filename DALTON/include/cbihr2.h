3f356ba3663a8b9ef083ab0a761e61672ab59ee2
Hans Jørgen Aagaard Jensen <hjj@sdu.dk>
2017-10-18 23:15:46 +0200

    hjaaj: eleminated THRQ; MAKE_MOTWOINT option to abaset.F(SETSIR)
    - eliminated THRQ, which was used in old sirtra.F(SORTA)
      Instead use THRP as elsewhere in sirtra.F
    - MAKE_MOTWOINT false when abaset.F(SETSIR) called from RESPONS, because
      needed integral transformation level is not known in RESPONS until
      **RESPONS input has been read. This way is clearer than the previous
      one based on FLAG(34) manipulation.
    - removed unused allocated CMO in sirfcktra.F
    - some polish

--- DALTON/include/cbihr2.h
+++ DALTON/include/cbihr2.h
@@ -1,12 +1,11 @@
-! FILE: cbihr2.h
-
+! file: cbihr2.h
 ! Control variables for her2* : AO 2-electron integrals from Hermit
-
-      REAL*8          THRFAC,    THRTWO
-      COMMON /CBRHR2/ THRFAC(2), THRTWO
+      REAL*8          THRTWO, THRFAC
       INTEGER         IPRTWO, IPRNTA, IPRNTB, IPRNTC, IPRNTD,
      &                ICEDIF, IFTHRS
-      COMMON /CBIHR2/ IPRTWO, IPRNTA, IPRNTB, IPRNTC, IPRNTD,
-     &                ICEDIF, IFTHRS
       LOGICAL         RUNTWO, RTNTWO, TKTIME, SOFOCK, USRSCR
-      COMMON /CBIHR2/ RUNTWO, RTNTWO, TKTIME, SOFOCK, USRSCR
+      COMMON /CBIHR2/ THRTWO, THRFAC(2),
+     &                IPRTWO, IPRNTA, IPRNTB, IPRNTC, IPRNTD,
+     &                ICEDIF, IFTHRS,
+     &                RUNTWO, RTNTWO, TKTIME, SOFOCK, USRSCR
+! -- end of cbihr2.h --
