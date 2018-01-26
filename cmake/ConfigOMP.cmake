827db82d3efa9fadd6adf21a4f14e956ff110c28
Radovan Bast <bast@users.noreply.github.com>
2018-01-26 12:17:43 +0100

    mv setting of -DVAR_OMP and ENABLE_THREADED_MKL out of ConfigOMP
    this is in preparation of using autocmake's ConfigOMP

--- cmake/ConfigOMP.cmake
+++ cmake/ConfigOMP.cmake
@@ -1,8 +1,1 @@
 option(ENABLE_OPENMP "Enable OpenMP parallelization" OFF)
-
-if(ENABLE_OPENMP)
-    add_definitions(-DVAR_OMP)
-    set(ENABLE_THREADED_MKL TRUE)
-else()
-    set(ENABLE_THREADED_MKL FALSE)
-endif()
