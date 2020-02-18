3e3ebe4b981b7970581122ef38b071c47750e93f
Rasmus Faber <rfaber@kemi.dtu.dk>
2020-02-18 16:10:30 +0100

    AOSOPPA: MPI work
    Ensure that more arguments in calls to mpi are explicitly typed,
    i.e. MPI_INTEGER_KIND.

--- DALTON/soppa/so_parutils.F90
+++ DALTON/soppa/so_parutils.F90
@@ -56,6 +56,7 @@ module so_parutils
    ! Actually every call to MPI functions should be explicitly typed,
    ! but it is a pain to write 1_mpi_integer_kind everywhere...
    integer(mpi_integer_kind), parameter :: one_mpi = 1, zero_mpi = 0
+   integer(mpi_integer_kind), parameter :: sop_master = 0
 #undef my_MPI_INTEGER
 
    private soppa_update_common, stupid_isao_bcast_routine
@@ -121,136 +122,137 @@ contains
 #include "ccpack.h"
 #include "ccinftap.h"
 
-      integer(mpi_integer_kind) :: bytesize, ierr
+      integer(mpi_integer_kind) :: bytesize, ierr, count_mpi 
          !
          !The infinite list of getbytespan -- mpi_bcast starts here
          !
       call getbytespan(lbuf, eribufLAST, bytesize)
-      call mpi_bcast(lbuf, bytesize, mpi_byte, 0, mpi_comm_world, ierr)
+      call mpi_bcast(lbuf, bytesize, mpi_byte, sop_master, mpi_comm_world, ierr)
 
       call getbytespan(luaorc, eritapLAST, bytesize)
-      call mpi_bcast(luaorc, bytesize, mpi_byte,0, mpi_comm_world, ierr)
+      call mpi_bcast(luaorc, bytesize, mpi_byte, sop_master, mpi_comm_world, ierr)
 
       call getbytespan(nsym, ccorbLAST, bytesize)
-      call mpi_bcast(nsym, bytesize, mpi_byte, 0, mpi_comm_world, ierr)
+      call mpi_bcast(nsym, bytesize, mpi_byte, sop_master, mpi_comm_world, ierr)
 
       call getbytespan(irow, infindLAST, bytesize)
-      call mpi_bcast(irow, bytesize, mpi_byte, 0, mpi_comm_world, ierr)
+      call mpi_bcast(irow, bytesize, mpi_byte, sop_master, mpi_comm_world, ierr)
 
       call getbytespan(centsh, blocksLAST, bytesize)
-      call mpi_bcast(centsh, bytesize, mpi_byte,0, mpi_comm_world, ierr)
+      call mpi_bcast(centsh, bytesize, mpi_byte,sop_master, mpi_comm_world, ierr)
 
       call getbytespan(skip, ccsdgninpLAST, bytesize)
-      call mpi_bcast(skip, bytesize, mpi_byte, 0, mpi_comm_world, ierr)
+      call mpi_bcast(skip, bytesize, mpi_byte, sop_master, mpi_comm_world, ierr)
 
       call getbytespan(ccs, ccmodelsLAST, bytesize)
-      call mpi_bcast(ccs, bytesize, mpi_byte, 0, mpi_comm_world, ierr)
+      call mpi_bcast(ccs, bytesize, mpi_byte, sop_master, mpi_comm_world, ierr)
 
       call getbytespan(etmp, etmpLAST, bytesize)
-      call mpi_bcast(etmp, bytesize, mpi_byte, 0, mpi_comm_world, ierr)
+      call mpi_bcast(etmp, bytesize, mpi_byte, sop_master, mpi_comm_world, ierr)
 
       call getbytespan(nckijmax, ccsdmaxLAST, bytesize)
-      call mpi_bcast(nckijmax, bytesize, mpi_byte,0,mpi_comm_world,ierr)
+      call mpi_bcast(nckijmax, bytesize, mpi_byte,sop_master,mpi_comm_world,ierr)
 
       call getbytespan(nt1amx, ccsdsymLAST, bytesize)
-      call mpi_bcast(nt1amx, bytesize, mpi_byte, 0, mpi_comm_world,ierr)
+      call mpi_bcast(nt1amx, bytesize, mpi_byte, sop_master, mpi_comm_world,ierr)
 
       call getbytespan(it2del, ccsdioLAST, bytesize)
-      call mpi_bcast(it2del, bytesize, mpi_byte, 0, mpi_comm_world,ierr)
+      call mpi_bcast(it2del, bytesize, mpi_byte, sop_master, mpi_comm_world,ierr)
 
       call getbytespan(mxcall, distclLAST, bytesize)
-      call mpi_bcast(mxcall, bytesize, mpi_byte, 0, mpi_comm_world,ierr)
+      call mpi_bcast(mxcall, bytesize, mpi_byte, sop_master, mpi_comm_world,ierr)
 
       call getbytespan(runeri, cbieriLAST, bytesize)
-      call mpi_bcast(runeri, bytesize, mpi_byte, 0, mpi_comm_world,ierr)
+      call mpi_bcast(runeri, bytesize, mpi_byte, sop_master, mpi_comm_world,ierr)
 
       call getbytespan(sotime, soppinfLAST, bytesize)
-      call mpi_bcast(sotime, bytesize, mpi_byte, 0, mpi_comm_world,ierr)
+      call mpi_bcast(sotime, bytesize, mpi_byte, sop_master, mpi_comm_world,ierr)
 
       call getbytespan(nexci2, soppexcLAST, bytesize)
-      call mpi_bcast(nexci2, bytesize, mpi_byte, 0, mpi_comm_world,ierr)
+      call mpi_bcast(nexci2, bytesize, mpi_byte, sop_master, mpi_comm_world,ierr)
 
       call getbytespan(soorwc, rwinfLAST, bytesize)
-      call mpi_bcast(soorwc, bytesize, mpi_byte, 0, mpi_comm_world,ierr)
+      call mpi_bcast(soorwc, bytesize, mpi_byte, sop_master, mpi_comm_world,ierr)
 
       call getbytespan(expbt, aobtchLAST, bytesize)
-      call mpi_bcast(expbt, bytesize, mpi_byte, 0, mpi_comm_world, ierr)
+      call mpi_bcast(expbt, bytesize, mpi_byte, sop_master, mpi_comm_world, ierr)
 
       call getbytespan(nitcl, odclssLAST, bytesize)
-      call mpi_bcast(nitcl, bytesize, mpi_byte, 0, mpi_comm_world, ierr)
+      call mpi_bcast(nitcl, bytesize, mpi_byte, sop_master, mpi_comm_world, ierr)
 
       call getbytespan(thrs, ccomLAST, bytesize)
-      call mpi_bcast(thrs, bytesize, mpi_byte, 0, mpi_comm_world, ierr)
+      call mpi_bcast(thrs, bytesize, mpi_byte, sop_master, mpi_comm_world, ierr)
 
       call getbytespan(gtotyp, ccomcLAST, bytesize)
-      call mpi_bcast(gtotyp, bytesize, mpi_byte, 0, mpi_comm_world,ierr)
+      call mpi_bcast(gtotyp, bytesize, mpi_byte, sop_master, mpi_comm_world,ierr)
 
       call getbytespan(scrmab, ericomLAST, bytesize)
-      call mpi_bcast(scrmab, bytesize, mpi_byte, 0, mpi_comm_world,ierr)
+      call mpi_bcast(scrmab, bytesize, mpi_byte, sop_master, mpi_comm_world,ierr)
 
       call getbytespan(thrsh, erithrLAST, bytesize)
-      call mpi_bcast(thrsh, bytesize, mpi_byte, 0, mpi_comm_world, ierr)
+      call mpi_bcast(thrsh, bytesize, mpi_byte, sop_master, mpi_comm_world, ierr)
 
       call getbytespan(memadd, erimemLAST, bytesize)
-      call mpi_bcast(memadd, bytesize, mpi_byte, 0, mpi_comm_world,ierr)
+      call mpi_bcast(memadd, bytesize, mpi_byte, sop_master, mpi_comm_world,ierr)
 
       call getbytespan(nodbch, odbtchLAST, bytesize)
-      call mpi_bcast(nodbch, bytesize, mpi_byte, 0, mpi_comm_world,ierr)
+      call mpi_bcast(nodbch, bytesize, mpi_byte, sop_master, mpi_comm_world,ierr)
 
       call getbytespan(charge, nucleiLAST, bytesize)
-      call mpi_bcast(charge, bytesize, mpi_byte, 0, mpi_comm_world,ierr)
+      call mpi_bcast(charge, bytesize, mpi_byte, sop_master, mpi_comm_world,ierr)
 
       call getbytespan(ndistr, eridstLAST, bytesize)
-      call mpi_bcast(ndistr, bytesize, mpi_byte, 0, mpi_comm_world,ierr)
+      call mpi_bcast(ndistr, bytesize, mpi_byte, sop_master, mpi_comm_world,ierr)
 
       call getbytespan(namn, nuclecLAST, bytesize)
-      call mpi_bcast(namn, bytesize, mpi_byte, 0, mpi_comm_world, ierr)
+      call mpi_bcast(namn, bytesize, mpi_byte, sop_master, mpi_comm_world, ierr)
 
       call getbytespan(maxrep, symmtiLAST, bytesize)
-      call mpi_bcast(maxrep, bytesize, mpi_byte, 0, mpi_comm_world,ierr)
+      call mpi_bcast(maxrep, bytesize, mpi_byte, sop_master, mpi_comm_world,ierr)
 
       call getbytespan(fmult, symmtrLAST, bytesize)
-      call mpi_bcast(fmult, bytesize, mpi_byte, 0, mpi_comm_world, ierr)
+      call mpi_bcast(fmult, bytesize, mpi_byte, sop_master, mpi_comm_world, ierr)
 
       call getbytespan(gamac, comr12LAST, bytesize)
-      call mpi_bcast(gamac, bytesize, mpi_byte, 0, mpi_comm_world, ierr)
+      call mpi_bcast(gamac, bytesize, mpi_byte, sop_master, mpi_comm_world, ierr)
 
       call getbytespan(mbas1, cmmmulLAST, bytesize)
-      call mpi_bcast(mbas1, bytesize, mpi_byte, 0, mpi_comm_world, ierr)
+      call mpi_bcast(mbas1, bytesize, mpi_byte, sop_master, mpi_comm_world, ierr)
 
       call getbytespan(jtop, hertopLAST, bytesize)
-      call mpi_bcast(jtop, bytesize, mpi_byte, 0, mpi_comm_world, ierr)
+      call mpi_bcast(jtop, bytesize, mpi_byte, sop_master, mpi_comm_world, ierr)
 
       call getbytespan(zcmval, cbireaLAST, bytesize)
-      call mpi_bcast(zcmval, bytesize, mpi_byte, 0, mpi_comm_world,ierr)
+      call mpi_bcast(zcmval, bytesize, mpi_byte, sop_master, mpi_comm_world,ierr)
 
       call getbytespan(mulnam, cbirea_cLAST, bytesize)
-      call mpi_bcast(mulnam, bytesize, mpi_byte, 0, mpi_comm_world,ierr)
+      call mpi_bcast(mulnam, bytesize, mpi_byte, sop_master, mpi_comm_world,ierr)
 
       call getbytespan(nmulbs, cmmbasLAST, bytesize)
-      call mpi_bcast(nmulbs, bytesize, mpi_byte, 0, mpi_comm_world,ierr)
+      call mpi_bcast(nmulbs, bytesize, mpi_byte, sop_master, mpi_comm_world,ierr)
 
       call getbytespan(i2bst, symsqLAST, bytesize)
-      call mpi_bcast(i2bst, bytesize, mpi_byte, 0, mpi_comm_world,ierr)
+      call mpi_bcast(i2bst, bytesize, mpi_byte, sop_master, mpi_comm_world,ierr)
 
       call getbytespan(thrpckint, ccpackLAST, bytesize)
-      call mpi_bcast(thrpckint,bytesize,mpi_byte,0,mpi_comm_world, ierr)
+      call mpi_bcast(thrpckint,bytesize,mpi_byte,sop_master,mpi_comm_world, ierr)
 
       call getbytespan(luiajb, cc_tapLAST, bytesize)
-      call mpi_bcast(luiajb,bytesize,mpi_byte,0,mpi_comm_world, ierr)
+      call mpi_bcast(luiajb,bytesize,mpi_byte,sop_master,mpi_comm_world, ierr)
 
       call getbytespan(gradml, gnrinfLAST, bytesize)
-      call mpi_bcast(gradml, bytesize, mpi_byte, 0, mpi_comm_world,ierr)
+      call mpi_bcast(gradml, bytesize, mpi_byte, sop_master, mpi_comm_world,ierr)
 
       call stupid_isao_bcast_routine()
 
       ! Common blocks with explicitly set sizes, no need to calculate bytespan
       ! Though we should probably still insert the actual parameter governing
       ! their length, eg. lbasdir+12 = 612
-      call mpi_bcast(basdir,lbasdir+12, mpi_character, 0,  &
+      count_mpi = lbasdir+12
+      call mpi_bcast(basdir, count_mpi, mpi_character, sop_master,  &
      &               mpi_comm_world, ierr)
-      call mpi_bcast(fnvajkl, 10, mpi_character, 0, mpi_comm_world,ierr)
-      call mpi_bcast(vclthr, 24, mpi_byte, 0, mpi_comm_world, ierr)
+      call mpi_bcast(fnvajkl, 10_mpi_integer_kind, mpi_character, sop_master, mpi_comm_world,ierr)
+      call mpi_bcast(vclthr, 24_mpi_integer_kind, mpi_byte, sop_master, mpi_comm_world, ierr)
 
       ! The slaves need to create the iadrpk array by
       ! calling the module functions rather than with a bcast
@@ -304,7 +306,7 @@ contains
 ! Use proper integerkind for mpi-arguments, irrespective of
 ! what flags are use for compilation of MPI and Dalton
 ! Not that it will change things right away, but perhaps one day
-      integer(mpi_integer_kind) :: ierr, numprocs, mycolor
+      integer(mpi_integer_kind) :: ierr, numprocs, mycolor, count_mpi, mynum_mpi
 ! Lengths of arrays
       integer :: lt2am, lfockd, ldensij, ldensab, ldensai, lworkf, &
                  LAssignedIndices
@@ -327,13 +329,13 @@ contains
       !
       ! Recieve the method on which to work
       !
-      call mpi_bcast( imod, 1, my_mpi_integer, 0, mpi_comm_world, &
+      call mpi_bcast( imod, one_mpi, my_mpi_integer, sop_master, mpi_comm_world, &
                       ierr )
       ! Do we need to do an allreduce to check that all
       ! Processes agree not to update the common-blocks?
       ! For now just let master tell the slaves
-      call mpi_bcast(update_common_blocks, 1, my_mpi_logical,     &
-                     0, mpi_comm_world, ierr)
+      call mpi_bcast(update_common_blocks, one_mpi, my_mpi_logical,     &
+                     sop_master, mpi_comm_world, ierr)
 
       if ( update_common_blocks ) then
          ! Recieve all the system and symmetry information
@@ -349,7 +351,8 @@ contains
       ! If there are more processes than integral distributions,
       ! we don't include the rest in the soppa communicator.
       !
-      numprocs = nodtot + 1 ! nodtot from infpar.h
+      !numprocs = nodtot + 1 ! nodtot from infpar.h
+      call mpi_comm_size( mpi_comm_world, numprocs, ierr)
       if ( numprocs .le. soppa_nint ) then
          !
          ! Usual case:
@@ -366,7 +369,8 @@ contains
          else
             mycolor = 0
          endif
-         call mpi_comm_split( mpi_comm_world, mycolor, mynum, &
+         mynum_mpi = mynum
+         call mpi_comm_split( mpi_comm_world, mycolor, mynum_mpi, &
                               soppa_comm_active, ierr)
       endif
 
@@ -418,7 +422,6 @@ contains
 !
 !     Allocation of of space for load-balancing
 !
-      call mpi_comm_size( mpi_comm_world, numprocs, ierr)
       maxnumjobs = soppa_nint - min(soppa_nint, numprocs) + 1
       lAssignedIndices = (maxnumjobs + 1) / irat
       kAssignedIndices = kend
@@ -439,7 +442,7 @@ contains
 !
       do
             ! Recieve work from master
-         call mpi_bcast ( soppa_work_kind, 1, my_mpi_integer, 0,       &
+         call mpi_bcast ( soppa_work_kind, one_mpi, my_mpi_integer, sop_master,       &
      &               mpi_comm_world, ierr )
 
             ! Act according to the job type recieved
@@ -468,8 +471,8 @@ contains
             ! These are joined together in the info_array.
             ! MODEL has allready have been communicated
             !
-            call mpi_bcast( info_array(1), 6, my_mpi_integer, 0,     &
-                            mpi_comm_world, ierr)
+            call mpi_bcast( info_array(1), 6_mpi_integer_kind, my_mpi_integer, &
+                            sop_master, mpi_comm_world, ierr)
 
             ! Inactive processes do nothing
             if ( soppa_comm_active .eq. MPI_COMM_NULL ) cycle
@@ -496,7 +499,8 @@ contains
             if (imod.eq.0) call quit('Error in AOSOPPA nodedriver:'// &
                ' Slave have not reserved memory for amplitudes')
 
-            call mpi_bcast( work(kt2am), lt2am, mpi_real8, 0,        &
+            count_mpi = lt2am
+            call mpi_bcast( work(kt2am), count_mpi, mpi_real8, sop_master,        &
      &                      mpi_comm_world, ierr )
 
          case default
@@ -519,6 +523,7 @@ contains
 !     dalton_nodedriver to enter the soppa node-driver and sends
 !     information, with doesn't change between soppa iterations.
 !    -----------------------------------------------------------
+      implicit none
 #include "iprtyp.h"
 #include "distcl.h"
 !
@@ -545,12 +550,12 @@ contains
          ! Here the slaves enter soppa_nodedriver
          !
          ! Set the method
-      call mpi_bcast( imodel, 1, my_mpi_integer, 0, mpi_comm_world, &
+      call mpi_bcast( imodel, one_mpi, my_mpi_integer, sop_master, mpi_comm_world, &
                       ierr )
          !
          ! Send the various common blocks if needed
-      call mpi_bcast( update_common_blocks, 1, my_mpi_logical,    &
-     &                   0, mpi_comm_world, ierr )
+      call mpi_bcast( update_common_blocks, one_mpi, my_mpi_logical,    &
+     &                   sop_master, mpi_comm_world, ierr )
 !
       if ( update_common_blocks ) then
 
@@ -582,13 +587,14 @@ contains
       integer, intent(in) :: lt2mp
       real(sop_dp), intent(in) :: t2mp(lt2mp)
 !
-      integer(mpi_integer_kind) :: ierr
+      integer(mpi_integer_kind) :: ierr, count_mpi
 !
 ! Tell slaves that the amplitudes will be send
       call mpixbcast(parsoppa_update_amplitudes, 1, 'INTEGE', 0)
 !
 ! Send the actual amplitudes
-      call mpi_bcast(t2mp, lt2mp, mpi_real8, 0, mpi_comm_world, ierr)
+      count_mpi = lt2mp
+      call mpi_bcast(t2mp, count_mpi, mpi_real8, sop_master, mpi_comm_world, ierr)
       return
    end subroutine soppa_update_amplitudes
 
@@ -646,7 +652,7 @@ contains
 !    This size SHOULD be MXCORB x "integer size"
 !    But since this is a cc block, someone may change that to MXCORB_CC
       call getbytespan(isao, ccisaolast, bytesize )
-      call mpi_bcast(isao, bytesize, mpi_byte, master, mpi_comm_world,   &
+      call mpi_bcast(isao, bytesize, mpi_byte, sop_master, mpi_comm_world,   &
      &               ierr)
 
    end subroutine stupid_isao_bcast_routine
@@ -691,6 +697,7 @@ subroutine getbytespan(firstvar, lastvar, bytespan)
 !      call getbytespan(lbuf, eribufLAST, bytesize)
 !      call mpi_bcast(lbuf, bytesize, mpi_byte, 0, mpi_comm_world, ierr)
 
+      implicit none
 #include "mpif.h"
 #include "priunit.h"
 
@@ -698,6 +705,7 @@ subroutine getbytespan(firstvar, lastvar, bytespan)
       integer,intent(in)   :: firstvar, lastvar
 !RF This should be mpi_address_kind
       integer(mpi_address_kind) :: firstmem, lastmem
+      integer(mpi_integer_kind) :: ierr
       integer              :: totaltransfer = 0
       integer, parameter   :: approxeager = 4096 ! Approximate upper limit for eager protocol transfers. Implementation dependent.
       integer              :: numeagersends=0, numrendezsends=0
