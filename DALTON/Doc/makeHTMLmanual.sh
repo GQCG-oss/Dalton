530e696d4d901015c33accfae80e2f61f1eec22c
Hans Joergen Aagard Jensen <hjj@sdu.dk>
2020-06-29 13:13:18 +0200

    hjaaj. Updated and generalized script for creating the pdf manual.
    - the script for the html manual is also updated in the same way,
      but it does not produce the desired output.

--- DALTON/Doc/makeHTMLmanual.sh
+++ DALTON/Doc/makeHTMLmanual.sh
@@ -1,23 +1,51 @@
 #!/bin/bash
 #
-DALTON_VERSION=${DALTON_VERSION:-../../VERSION}
-DOC_DIRECTORY=${DOC_DIRECTORY:-./}
-# Declare variables
-#
-RELEASE_VERSION=`cat $DALTON_VERSION`
-HTMLDIR="${DOC_DIRECTORY}/../Dalton_HTMLmanual"
-export HTMLDIR
-if [ -d "$HTMLDIR" ]; then
-   echo "directory $HTMLDIR exists"
+if [ $# -gt 0 ]; then
+   echo "--> Using user specified html directory: $1"
+   HTMLDIR="$1"
 else
-   echo "directory $HTMLDIR does not exist - creating it ..."
+   HTMLDIR="${DOC_DIRECTORY}/Dalton_HTMLmanual"
+if [ -d "$HTMLDIR" ]; then
+   echo "--> html output directory $HTMLDIR exists"
+else
+   echo "--> html directory $HTMLDIR does not exist - creating it ..."
    mkdir $HTMLDIR
 fi
 
-cd $DOC_DIRECTORY
+fi
+if [ -d "$HTMLDIR" ]; then
+   echo "--> html output directory $HTMLDIR exists"
+else
+   echo "--> html directory $HTMLDIR does not exist - creating it ..."
+   mkdir $HTMLDIR
+fi
+
+DOC_DIRECTORY=${DOC_DIRECTORY:-.}
+# Declare variables
+#
+if [ -n "$DALTON_VERSION" ]; then
+   RELEASE_VERSION=$DALTON_VERSION
+   echo "--> User specified release version: $RELEASE_VERSION"
+elif [ -n "$RELEASE_VERSION" ]; then
+   echo "--> User specified release version: $RELEASE_VERSION"
+else
+   DALTON_VERSION=${DALTON_VERSION:-../../VERSION}
+   RELEASE_VERSION=`cut -d ' ' -f 1 ${DALTON_VERSION}`
+fi
+
+if [ ! -d $DOC_DIRECTORY ]; then
+   echo '--> ERROR, Dalton document directory "' $DOC_DIRECTORY '" does not exist!'
+   exit 1
+fi
+
+cp -t $HTMLDIR $DOC_DIRECTORY/*.tex
+cp -t $HTMLDIR $DOC_DIRECTORY/*.bib
+
+cd $HTMLDIR
+HTMLDIR=`pwd`
 
 if [ -z Master.tex ]; then
-   echo 'ERROR, Dalton Master.tex does not exist in "' $DOC_DIRECTORY '".'
+   echo 'ERROR, Dalton Master.tex does not exist in "' $HTMLDIR '".'
    exit 2
 fi
 
@@ -27,15 +55,24 @@ fi
 #echo "RELEASE_VERSION is " $RELEASE_VERSION
 #echo "HTMLDIR is " $HTMLDIR
 
+echo "--> Generation of .aux files with latex is needed for latex2html"
+
 #
 # Run latex2html
 #
-echo "Output from latex2html will be in dalton_latex2html.log"
+(latex Master; bibtex Master; latex Master; latex Master; makeindex Master; latex Master) \
+>& dalton_latex.log
+
+echo "--> Output from latex2html will be in dalton_latex2html.log"
 
 latex2html -transparent -font_size 11pt -no_antialias -local_icons \
     -address "Dalton Manual - Release ${RELEASE_VERSION}" -dir $HTMLDIR Master.tex >& dalton_latex2html.log
+mv Master.html Dalton_Master.html
 
-echo "Dalton html manual is now created in $HTMLDIR"
+# clean-up
+rm *.aux *.ind *.blg *.bbl *.toc *.tex *.bib
+
+echo "--> Dalton html manual is now created in $HTMLDIR; html root file is Dalton_Master.html"
 #
 # Post-processing from release 1.2.1
 #
