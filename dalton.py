e4f9fa5665b661cfcb15653189fca35fdcd5c633
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-11 05:09:20 +0000

    Adding tarfile member sanitization to extractall()

--- dalton.py
+++ dalton.py
@@ -52,7 +52,26 @@ def upload_files(args, tmp):
     dal, mol, pot, out = process_args(args.inputs)
     if args.f:
         with tarfile.open(args.f + '.tar.gz', mode='r:gz') as tgz:
-            tgz.extractall(path=tmp)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tgz, path=tmp)
 
     shutil.copy(dal + '.dal', os.path.join(tmp, 'DALTON.INP'))
     try:
